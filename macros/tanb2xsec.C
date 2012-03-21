5cd1166e6dd47dfb672024e7d319f4e3f9d69af2
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-03-21 14:41:27 +0000

    translates tanb limits into model dependent xsec limits fro comparison with the model independent xsec limits on ggH and bbH only

---
+++ macros/tanb2xsec.C
@@ -0,0 +1,27 @@
+#include "mssm_xs_tools.h"
+#include "mssm_xs_tools.C"
+
+#include "TString.h"
+#include "TGraph.h"
+
+void tanb2xsec(const char* filename, const char* directory="HIG-11-029", const char* graph="observed", bool verbose=true)
+{
+  mssm_xs_tools mssm;
+  mssm.SetInput("HiggsAnalysis/HiggsToTauTau/data/out.mhmax-7-nnlo.root"); //mssm.help();
+
+  /// open input file  
+  TFile* inputFile = new TFile(filename, "update");
+  TGraph* limit = (TGraph*)inputFile->Get(TString::Format("%s/%s", directory, graph));
+  for(int idx=0; idx<limit->GetN(); ++idx){
+    float mA = limit->GetX()[idx]; float tanb = limit->GetY()[idx];
+    float xsec_limit = mssm.Give_Xsec_ggFA(mA, tanb)*mssm.Give_BR_A_tautau(mA, tanb)/1000. + mssm.GiveXsec_Santander_A(mA, tanb)*mssm.Give_BR_A_tautau(mA, tanb)/1000.;
+    if(verbose){
+      std::cout << "Limit on cross section [" << mA << "]: " << xsec_limit << " pb" << std::endl;
+    }
+    limit->SetPoint(idx, limit->GetX()[idx], xsec_limit);
+  }
+  inputFile->cd(directory);
+  limit->Write(graph);
+  inputFile->Close();
+  return;
+}
