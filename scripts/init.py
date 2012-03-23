e8f3958829db0ff8fedc96fbbfdd856fa83d4bfb
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-03-23 15:52:27 +0000

    only make sure the thing is executable...

--- scripts/init.py
+++ scripts/init.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
 import os
-
 os.system("python {CMSSW_BASE}/src/data/init-data.py".format(CMSSW_BASE=os.environ.get("CMSSW_BASE")))
 os.system("python {CMSSW_BASE}/src/data/init-setup.py".format(CMSSW_BASE=os.environ.get("CMSSW_BASE")))
