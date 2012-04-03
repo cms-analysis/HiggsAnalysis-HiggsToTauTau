4d0b630364aa7485c0a6e1e7d59af3669d1792af
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-04-03 14:19:12 +0000

    adapting the init.py script

--- scripts/init.py
+++ scripts/init.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
 import os
-os.system("python {CMSSW_BASE}/src/data/init-data.py".format(CMSSW_BASE=os.environ.get("CMSSW_BASE")))
-os.system("python {CMSSW_BASE}/src/data/init-setup.py".format(CMSSW_BASE=os.environ.get("CMSSW_BASE")))
+
+print " -------------------------------------------------------------------------------------------- "
+print " Completing package HiggsAnalysis/HiggsToTauTau with all input files, which are necessary to  "
+print " make maximal use of it. These input files are expected to be present in an input directory   "
+print " datacard. You can check this directory out from the UserCode cvs using the following command "
+print
+print " cvs co -r V00-01-01 -d datacards UserCode/RWolf/MitLimits/Higgs2Tau/data"
+print " -------------------------------------------------------------------------------------------- "
+
+os.chdir("{CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/data".format(CMSSW_BASE=os.environ.get("CMSSW_BASE")))
+os.system("python init-data.py" )
+os.chdir("{CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/setup".format(CMSSW_BASE=os.environ.get("CMSSW_BASE")))
+os.system("python init-setup.py")
