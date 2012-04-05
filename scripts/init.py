4715aa17703227020546b154ebe449eb1c2ff05b
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-04-05 16:57:19 +0000

    update of datacards tag announcement and finally make the script executable...

--- scripts/init.py
+++ scripts/init.py
@@ -7,7 +7,7 @@ print " Completing package HiggsAnalysis/HiggsToTauTau with all input files, whi
 print " make maximal use of it. These input files are expected to be present in an input directory   "
 print " datacard. You can check this directory out from the UserCode cvs using the following command "
 print
-print " cvs co -r V00-01-01 -d datacards UserCode/RWolf/MitLimits/Higgs2Tau/data"
+print " cvs co -r V00-01-02 -d datacards UserCode/RWolf/MitLimits/Higgs2Tau/data"
 print " -------------------------------------------------------------------------------------------- "
 
 os.chdir("{CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/data".format(CMSSW_BASE=os.environ.get("CMSSW_BASE")))
