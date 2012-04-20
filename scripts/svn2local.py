4c5da9cd56586df8d3369ff8edb3b6f209d05926
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-04-20 17:06:03 +0000

    updates for SM4 combination

--- scripts/svn2local.py
+++ scripts/svn2local.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os
+
 from optparse import OptionParser, OptionGroup
 
 ## set up the option parser
@@ -9,38 +9,19 @@ parser.add_option("-i", "--input", dest="input", default="lp11/", type="string",
 parser.add_option("-o", "--out", dest="out", default="cmb", type="string", help="Name of the output directory [Default: cmb]")
 parser.add_option("-c", "--channel", dest="channel", default="all", type="choice", help="Channel to be copied [Default: all]", choices=["all", "htt", "hgg", "hbb", "hww", "hzz4l", "hzz2l2q", "hzz2l2nu", "hzz2l2t"])
 parser.add_option("--SM4", dest="sm4", default=False, action="store_true", help="Copy SM4 datacards [Default: False]")
-
-
 ## check number of arguments; in case print usage
 (options, args) = parser.parse_args()
+
 if len(args) < 1 :
     parser.print_usage()
     exit(1)
 
+import os
+from HiggsAnalysis.HiggsToTauTau.utils import parseArgs
+
 prefix = ""
 if options.sm4 :
     prefix = "SM4_"
-
-## parse a list of arguments which can be intergers or of type intA-intB
-## where intA<=intB and fill this list of arguments into a list if ints
-def parseArgs(args) :
-    list = []
-    for elem in args :
-        if elem.find("-") > -1 :
-            if elem.find(":") > -1 :
-                step = int(elem[elem.find(":")+1:])
-                min = int(elem[:elem.find("-")  ])
-                max = int(elem[elem.find("-")+1:elem.find(":")])
-            else :
-                step = 1
-                min = int(elem[:elem.find("-")  ])
-                max = int(elem[elem.find("-")+1:])
-            while min <= max :
-                list.append(min)
-                min=min+step
-        else :
-            list.append(elem)
-    return list
 
 for mass in parseArgs(args) :
     print "{output}/{mass}".format(output=options.out, mass=mass)
@@ -53,13 +34,15 @@ for mass in parseArgs(args) :
         os.system("cp {input}/{mass}/{pre}htt_mt* {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
         os.system("cp {input}/{mass}/{pre}htt_em* {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
         os.system("cp {input}/{mass}/{pre}htt_mm* {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
-        os.system("cp {input}/{mass}/{pre}vhtt*.txt   {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
-        os.system("cp {input}/{mass}/vhtt_shapes.root {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
+        #os.system("cp {input}/{mass}/{pre}vhtt*.txt   {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
+        #os.system("cp {input}/{mass}/vhtt_shapes.root {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
         os.system("cp {input}/common/{pre}htt_*.root {output}/common/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
     if options.channel == "hgg" or options.channel == "all" :
         ## for hgg copy the input file which is common for all masspoints from common to common
-        os.system("cp {input}/common/{pre}hggmva.txt {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
-        os.system("cp {input}/common/{pre}hgg*.root {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
+        #os.system("cp {input}/common/{pre}hggmva.txt {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
+        #os.system("cp {input}/common/{pre}hgg*.root {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
+        os.system("cp {input}/{mass}/{pre}hgg{SM4}_*.txt {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix, SM4="NOVBF" if options.sm4 else ""))
+        os.system("cp {input}/common/{pre}hgg*.root {output}/common/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
     if options.channel == "hbb" or options.channel == "all" :
         os.system("cp {input}/{mass}/{pre}vhbb_* {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
     if options.channel == "hww" or options.channel == "all" :
@@ -71,7 +54,8 @@ for mass in parseArgs(args) :
     if options.channel == "hzz2l2nu" or options.channel == "all" :
         os.system("cp {input}/{mass}/{pre}hzz2l2nu_* {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
     if options.channel == "hzz2l2t" or options.channel == "all" :
-        os.system("cp {input}/{mass}/{pre}HZZ2l2t_* {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
+        os.system("cp {input}/{mass}/{pre}hzz2l2t_* {output}/{mass}/".format(input=options.input, output=options.out, mass=mass, pre=prefix))
+        os.system("cp {input}/common/{pre}hzz2l2t_* {output}/common/".format(input=options.input, output=options.out, pre=prefix))
     ## check if directory is empty -- if yes remove it from the list of masspoints to be submited
     if len(os.listdir("{output}/{mass}".format(output=options.out, mass=mass))) == 0 :
         os.system("rm -r {output}/{mass}".format(output=options.out, mass=mass))
