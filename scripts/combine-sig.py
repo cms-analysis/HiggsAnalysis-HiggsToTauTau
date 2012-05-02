3e6b108bc58a5bdf667d7260bc4a0099eb60b59e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-05-02 11:43:58 +0000

    adding PL significance

---
+++ scripts/combine-sig.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+from optparse import OptionParser, OptionGroup
+
+## set up the option parser
+parser = OptionParser(usage="usage: %prog [options] ARG1",
+                      description="Script to create a consistent set of a submission script and a crab configuration file for batch job submission at lxbatch, condor or via the grid. The name of the submission script and configuration file can be changed via command line. Note that batch job submission to condor is not yet tested. The script uses combine with option -M ProfileLikelihood to calculate expected significances. ARG1 corresponds to the workspace in form of roofit workspace.")
+## direct options
+parser.add_option("-o", "--out", dest="out", default="batch", type="string", help="Name of the output files (.sh and .cfg). [Default: batch]")
+parser.add_option("-s", "--seed", dest="seed", default="-1", type="string", help="Random seeds to be used for the jobs (if -1 the seed will be chosen randomly). [Default: \"-1\"]")
+parser.add_option("-v", "--verbose", dest="v", default=0, type="int", help="Verbosity level of combine [Default: 0]")
+## combine options
+egroup = OptionGroup(parser, "COMBINE (ProfileLikelihood) COMMAND OPTIONS", "Command options for the use of combine with method -M ProfileLikelihood.")
+egroup.add_option("-m", "--mass", dest="mass", default=120, type="int", help="Masspoint (needed for hgg datacards(from submit_.py this parameter is passed on internally). [Default: 120]")
+egroup.add_option("--signal-strength", dest="signal_strength", default="1", type="string", help="Set signal strength for expected significance calculation. [Default: \"1\"]")
+parser.add_option_group(egroup)
+## crab cfg parameters
+cgroup = OptionGroup(parser, "CRAB CONFIGURATION OPTIONS", "Options for the configuration of the crab configuration file. Note that all of these parameters can be changed in the batch.cfg file later on.")
+cgroup.add_option("-t", "--toys", dest="t", default=50, type="int", help="Total number of toys (can be changed in .cfg file). [Default: 50]")
+cgroup.add_option("-j", "--jobs", dest="j", default=10, type="int", help="Total number of jobs (can be changed in .cfg file). [Default: 10]")
+cgroup.add_option("--server", dest="server", default=False, action="store_true", help="Use crab server. [Default: False]")
+cgroup.add_option("--lsf", dest="lsf", default=False, action="store_true", help="Run on LSF instead of GRID (can be changed in .cfg file). [Default: True]")
+cgroup.add_option("--condor", dest="condor", default=False, action="store_true", help="Run on condor_g instead of GRID (can be changed in .cfg file). [Default: False]")
+cgroup.add_option("--glidein", dest="glide", default=False, action="store_true", help="Use glide-in scheduler instead of glite. [Default: False]")
+cgroup.add_option("-q", "--queue", dest="queue", default="8nh", type="string", help="LSF queue to use (can be changed in .cfg file). [Default: 8nh]")
+cgroup.add_option("-P", "--priority", dest="prio", default=False, action="store_true", help="Use priority user role")
+parser.add_option_group(cgroup)
+
+## check number of arguments; in case print usage
+(options, args) = parser.parse_args()
+if len(args) != 1:
+    parser.print_usage()
+    exit(1)
+
+## get the workspace
+workspace = args[0]
+
+from math import *
+import os
+
+## No fork in batch jobs for now
+options.fork = 1 
+
+## create job submission script
+print "> creating executable script ",options.out+".sh"
+script = open(options.out+".sh", "w")
+script.write("""
+#!/bin/bash
+
+i="$1"
+if [ "$i" = "" ]; then
+  echo "Error: missing job index"
+  exit 1;
+fi
+echo "max events from CRAB: $MaxEvents"
+n="$MaxEvents"
+if [ "$n" = "" ]; then
+  n="$2"
+fi
+if [ "$n" = "" ]; then
+  echo "Error: missing number of experiments"
+  exit 2;
+fi
+
+echo "## Starting at $(date)"
+""")
+
+toys = "$n"
+## prepare optional options
+script.write("./combine {wsp} -M ProfileLikelihood -t {toys} --significance --signalForSignificance={sig} -m {mass} -v {v} -n {out} -s {seed}\n".format(
+wsp=workspace, toys=toys, sig=options.signal_strength, mass=options.mass, v=options.v, out=options.out, seed=options.seed))
+    
+script.write("\n");
+script.write("hadd %s_grid.root higgsCombine*.root\n" % options.out)
+script.write('echo "## Done at $(date)"\n');
+script.close()
+os.system("chmod +x %s.sh" % options.out)
+
+## create a link for combine
+if not os.path.exists("combine"):
+    print "Creating a symlink to the combine binary"
+    os.system("cp -s $(which combine) .")
+
+## define scheduler
+sched = "glite"
+if options.lsf: sched = "lsf"
+if options.condor: sched = "condor"
+if options.glide: sched = "glidein"
+
+## create crab configuration file for job submission
+print "> creating crab cfg ",options.out+".cfg"
+cfg = open(options.out+".cfg", "w")
+cfg.write("""
+[CRAB]
+jobtype = cmssw
+scheduler = {sched}
+use_server = {srv}
+
+[LSF]
+queue = {queue}
+
+[CMSSW]
+datasetpath = None
+pset = None
+output_file = {out}_grid.root
+total_number_of_events = {total}
+number_of_jobs = {jobs}
+
+[USER]
+script_exe = {out}.sh
+additional_input_files = combine,{wsp}
+return_data = 1
+""".format(wsp=workspace, out=options.out, sched=sched, srv=(1 if options.server else 0), queue=options.queue, jobs=options.j, total=options.t))
+
+if options.prio: cfg.write("""
+[GRID]
+rb               = CERN
+proxy_server     = myproxy.cern.ch
+role             = priorityuser
+retry_count      = 0
+maxtarballsize   = 250
+
+ce_white_list    = T2, T3_US_UCD
+ce_black_list    = T2_CN_Beijing, T2_RU, T2_SU, T2_UA, T2_PK, T2_PL_Cracow, T2_UK_SGrid_Bristol
+""")
+cfg.close()
