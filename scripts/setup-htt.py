21de636f34dcf1e7688447e7dba234d163007fd1
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-05-04 12:39:30 +0000

    fixing setup of sm event-categories with all channels

--- scripts/setup-htt.py
+++ scripts/setup-htt.py
@@ -9,7 +9,7 @@ parser.add_option("-n", "--name", dest="name", default="xxxx-test", type="string
 parser.add_option("--sm-masses", dest="sm_masses", default="110 115 120 125 130 135 140 145", type="string", help="Available SM mass points [Default: 110 115 120 125 130 135 140 145]")
 parser.add_option("--mssm-masses", dest="mssm_masses", default="90 100 120 130 140 160 180 200 250 300 350 400 450 500", type="string", help="Available MSSM mass points [Default: 90 100 120 130 140 160 180 200 250 300 350 400 450 500]")
 parser.add_option("--analysis", dest="analysis", default="all", type="choice", help="Choose between sm or mssm analysis [Default: all]", choices=["sm", "mssm", "all"])
-parser.add_option("--category", dest="category", default="all", type="choice", help="Choose between event categories (depending on analysis) [Default: all]", choices=["cmb", "incl", "btag", "nobtag", "vbf", "boost", "all"])
+parser.add_option("--category", dest="category", default="all", type="choice", help="Choose between event categories (depending on analysis) [Default: all]", choices=["cmb", "incl", "btag", "nobtag", "novbf", "vbf", "boost", "all"])
 parser.add_option("--channel", dest="channel", default="all", type="choice", help="Choose between ditau decay channels [Default: all]", choices=["cmb", "emu", "etau", "mutau", "all"])
 parser.add_option("--drop-channels", dest="drop",default="", type="string", help="Scale the given channels to 0. in the datacards. (This action will be applied to all channels that match the given string patterns, which may be on as a whitespace or ',' separated list). [Default: \"\"]")
 
@@ -87,15 +87,15 @@ if options.analysis == "sm" or options.analysis == "all":
             os.system("setup-batch.py -n cmb    -c mt  -o htt_mt_0.txt -e SM-00    %s %s" % (optionals, options.sm_masses  ))
             os.system("setup-batch.py -n cmb    -c mt  -o htt_mt_1.txt -e SM-01    %s %s" % (optionals, options.sm_masses  ))
             os.system("setup-batch.py -n cmb    -c mt  -o htt_mt_2.txt -e SM-02    %s %s" % (optionals, options.sm_masses  ))
-        if options.category == "novbf":
+        if options.category == "novbf" or options.category == "all":
             os.system("setup-batch.py -n novbf  -c em  -o htt_em_0.txt -e SM-00    %s %s" % (optionals, options.sm_masses  ))
             os.system("setup-batch.py -n novbf  -c et  -o htt_et_0.txt -e SM-00    %s %s" % (optionals, options.sm_masses  ))
             os.system("setup-batch.py -n novbf  -c mt  -o htt_mt_0.txt -e SM-00    %s %s" % (optionals, options.sm_masses  ))
-        if options.category == "boost":
+        if options.category == "boost" or options.category == "all":
             os.system("setup-batch.py -n boost  -c em  -o htt_em_1.txt -e SM-01    %s %s" % (optionals, options.sm_masses  ))
             os.system("setup-batch.py -n boost  -c et  -o htt_et_1.txt -e SM-01    %s %s" % (optionals, options.sm_masses  ))
-            os.system("setup-batch.py -n bosst  -c mt  -o htt_mt_1.txt -e SM-01    %s %s" % (optionals, options.sm_masses  ))
-        if options.category == "vbf":
+            os.system("setup-batch.py -n boost  -c mt  -o htt_mt_1.txt -e SM-01    %s %s" % (optionals, options.sm_masses  ))
+        if options.category == "vbf" or options.category == "all":
             os.system("setup-batch.py -n vbf    -c em  -o htt_em_2.txt -e SM-02    %s %s" % (optionals, options.sm_masses  ))
             os.system("setup-batch.py -n vbf    -c et  -o htt_et_2.txt -e SM-02    %s %s" % (optionals, options.sm_masses  ))
             os.system("setup-batch.py -n vbf    -c mt  -o htt_mt_2.txt -e SM-02    %s %s" % (optionals, options.sm_masses  ))
