b63c33c67ae11e577ec31509143a66621b67b47e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-05-25 13:55:14 +0000

    macro to blind data for limit calculation

---
+++ macros/blindData.C
@@ -0,0 +1,84 @@
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include <TKey.h>
+#include <TH1F.h>
+#include <TFile.h>
+#include <TROOT.h>
+#include <TCollection.h>
+
+#include "HiggsAnalysis/HiggsToTauTau/macros/Utils.h"
+
+
+/**
+   \class   blindData blindData.C "HiggsAnalysis/HiggsToTauTau/macros/blindData.C"
+
+   \brief   macro to replace data_obs by the exact expectation from all indicated background and potentially signal histograms.
+
+   Macro to replace the data_obs histogram by the exact expectation from all indicated 
+   background and potantially signal histograms. The list of background and signal 
+   histograms and the input file have to be given as agruments. The macro will survery
+   the input file for one directory layer according to the conventions for htt limit 
+   combinations and in this directory structire search for the data_obs histogram and 
+   the indicated background (and signal) historams. The data_obs histogram will be 
+   reset and refilled by the sum of all indicated background (and signal) histograms.
+   
+   The macro can be used to inject signal for a given mass into the data_obs. To obtain 
+   the expected signal only from background just pass an empty string for the signal 
+   samples as argument. In case you intend to inject signal keep in mind that you should 
+   have scaled the signal of you input file by the corresponding cross section to obtain 
+   a proper yield estimate for the signal. 
+
+   Function arguments are:
+   
+   filename               : input file that is supposed to keep the input histograms
+   background_patterns    : list of background samples (should be separated by comma 
+                            or whitespaces)
+   signal_patterns        : list of signal samples (should be separated by comma or 
+                            whitespaces)
+   armed                  : write modified data_obs histograms back to file
+*/
+
+void blindData(const char* filename, const char* background_patterns="Fakes, EWK, ttbar, Ztt", const char* signal_patterns="ggH125, qqH125, VH125", bool armed=false, unsigned int debug=1)
+{
+  /// prepare input parameters
+  std::vector<std::string> signals;
+  string2Vector(cleanupWhitespaces(signal_patterns), signals);
+  std::vector<std::string> samples;
+  string2Vector(cleanupWhitespaces(background_patterns), samples);
+  samples.insert(samples.end(), signals.begin(), signals.end());
+
+  TKey* idir;
+  TH1F* buffer = 0;
+  TH1F* blind_data_obs = 0;
+  TFile* file = new TFile(filename, "update");
+  TIter nextDirectory(file->GetListOfKeys());
+  //std::vector<TH1F*> hists;
+  while((idir = (TKey*)nextDirectory())){
+    if( idir->IsFolder() ){
+      file->cd(); // make sure to start in directory head 
+      if( debug>0 ){ std::cout << "Found directory: " << idir->GetName() << std::endl; }
+      if( file->GetDirectory(idir->GetName()) ){
+	file->cd(idir->GetName()); // change to sub-directory
+	buffer = (TH1F*)file->Get((std::string(idir->GetName())+"/data_obs").c_str()); blind_data_obs = (TH1F*)buffer->Clone("data_obs");
+	//if( debug>0 ){ std::cout << "Old scale of blinded data_obs: " << blind_data_obs->Integral() << std::endl; }
+	blind_data_obs->Reset();
+	for(std::vector<std::string>::const_iterator sample = samples.begin(); sample!=samples.end(); ++sample){
+	  if( debug>0 ){ std::cout << "Looking for histogram: " << (std::string(idir->GetName())+"/"+(*sample)) << std::endl; }
+	  buffer = (TH1F*)file->Get((std::string(idir->GetName())+"/"+(*sample)).c_str()); blind_data_obs->Add(buffer);
+	}
+	blind_data_obs->Scale((int)blind_data_obs->Integral()/blind_data_obs->Integral());
+	//if( debug>0 ){ std::cout << "New scale of blinded data_obs: " << blind_data_obs->Integral() << std::endl; }
+	if(armed){
+	  std::cout << "Writing to file: " << blind_data_obs->GetName() << std::endl;
+	  blind_data_obs->Write("data_obs", TObject::kOverwrite); 
+	}
+      }
+    }
+  }
+  file->Close();
+  return;
+}
+
