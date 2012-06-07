8f3f619b76a5b9f3ed84d56396efda6a349fd18c
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-06-07 13:27:42 +0000

    script to create datacards from input files

---
+++ scripts/setup-datacards.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+from optparse import OptionParser, OptionGroup
+
+## set up the option parser
+parser = OptionParser(usage="usage: %prog [options] ARGS",
+                      description="Script to create datacards to be used locally or to be uploaded to the cvs. The output directory to copy the datacards to is expected to have a dedicated structure. Directories that do not exist are created on the fly. The default configuration is to upload all currently available datacards to the common cvs repository for the HiggsToTauTau analysis subgroup. ARGS corresponds to a list of integer values resembling the mass points for which you want to create the datacards. Ranges can be indicated e.g. by: 110-145'. That only any x-th mass point should be taken into account can be indicated e.g. by: 110-145:5. The latter example will pick up the masses 110 115 120 130 135 140 145. Any combination of this syntax is possible.")
+parser.add_option("-o", "--out", dest="out", default="datacards", type="string", help="Name of the output directory to which the datacards should be copied. [Default: datacards]")
+parser.add_option("-p", "--periods", dest="periods", default="7TeV 8TeV", type="string", help="Choose between run periods [Default: \"7TeV 8TeV\"]")
+parser.add_option("-a", "--analysis", dest="analysis", default="sm", type="choice", help="Type of analysis (sm or mssm). Lower case is required. [Default: sm]", choices=["sm", "mssm"])
+parser.add_option("-c", "--channels", dest="channels", default="mm em mt et", type="string", help="List of channels, for which datacards should be created. The list should be embraced by call-ons and separeted by whitespace or comma. Available channels are mm, em, mt, et, tt, vhtt. [Default: \"mm em mt et\"]")
+cats1 = OptionGroup(parser, "SM EVENT CATEGORIES", "Event categories to be picked up for the SM analysis.")
+cats1.add_option("--sm-categories-mm", dest="mm_sm_categories", default="0 1 2 3 4 5", type="string", help="List mm of event categories. [Default: \"0 1 2 3 4 5\"]")
+cats1.add_option("--sm-categories-em", dest="em_sm_categories", default="0 1 2 3 4 5", type="string", help="List em of event categories. [Default: \"0 1 2 3 4 5\"]")
+cats1.add_option("--sm-categories-mt", dest="mt_sm_categories", default="0 1 2 3 4 5", type="string", help="List mt of event categories. [Default: \"0 1 2 3 4 5\"]")
+cats1.add_option("--sm-categories-et", dest="et_sm_categories", default="0 1 2 3 4 5", type="string", help="List et of event categories. [Default: \"0 1 2 3 4 5\"]")
+cats1.add_option("--sm-categories-tt", dest="tt_sm_categories", default="0 1", type="string", help="List of tt event categories. [Default: \"0 1\"]")
+cats1.add_option("--sm-categories-vhtt", dest="vhtt_sm_categories", default="2", type="string", help="List of vhtt event categories. [Default: \"2\"]")
+parser.add_option_group(cats1)
+cats2 = OptionGroup(parser, "MSSM EVENT CATEGORIES", "Event categories to be used for the MSSM analysis.")
+cats2.add_option("--mssm-categories-mm", dest="mm_mssm_categories", default="0 1 2 3 6 7", type="string", help="List mm of event categories. [Default: \"0 1 2 3 6 7\"]")
+cats2.add_option("--mssm-categories-em", dest="em_mssm_categories", default="0 1 2 3 6 7", type="string", help="List em of event categories. [Default: \"0 1 2 3 6 7\"]")
+cats2.add_option("--mssm-categories-mt", dest="mt_mssm_categories", default="0 1 2 3 6 7", type="string", help="List mt of event categories. [Default: \"0 1 2 3 6 7\"]")
+cats2.add_option("--mssm-categories-et", dest="et_mssm_categories", default="0 1 2 3 6 7", type="string", help="List et of event categories. [Default: \"0 1 2 3 6 7\"]")
+cats2.add_option("--mssm-categories-tt", dest="tt_mssm_categories", default="0 1", type="string", help="List of tt event categories. [Default: \"0 1\"]")
+cats2.add_option("--mssm-categories-hmm", dest="hmm_mssm_categories", default="0 1", type="string", help="List of hmm event categories. [Default: \"0 1\"]")
+parser.add_option_group(cats2)
+parser.add_option("--SM4", dest="SM4", default=False, action="store_true", help="Re-scale signal samples in input file according to SM4 cross section*BR before datacard creation. [Default: False]")
+
+## check number of arguments; in case print usage
+(options, args) = parser.parse_args()
+
+if len(args) < 1 :
+    parser.print_usage()
+    exit(1)
+    
+import os
+from HiggsAnalysis.HiggsToTauTau.utils import parseArgs
+
+## periods
+periods = options.periods.split()
+for idx in range(len(periods)) : periods[idx] = periods[idx].rstrip(',')
+## channels
+channels = options.channels.split()
+for idx in range(len(channels)) : channels[idx] = channels[idx].rstrip(',')
+
+## setup directory structure in case it does not exist, yet 
+if not os.path.exists(options.out) :
+    os.system("mkdir {OUTPUT}/".format(OUTPUT=options.out))
+if options.analysis == "sm" : 
+    if not os.path.exists("{OUTPUT}/sm".format(OUTPUT=options.out)) :
+        os.system("mkdir {OUTPUT}/sm".format(OUTPUT=options.out))
+        for channel in channels :
+            prefix = "" if channel == "vhtt" else "htt_"
+            if not os.path.exists("{OUTPUT}/sm/{PRE}{CHN}".format(OUTPUT=options.out, PRE=prefix, CHN=channel)) :
+                os.system("mkdir {OUTPUT}/sm/{PRE}{CHN}".format(OUTPUT=options.out, PRE=prefix, CHN=channel))
+os.chdir(options.out)
+
+## switch to sm event categories
+if options.analysis == "sm" :
+    os.chdir("sm")
+    ## event categories
+    categories = {
+        "mm"   : options.mm_sm_categories.split(),
+        "em"   : options.em_sm_categories.split(),
+        "mt"   : options.mt_sm_categories.split(),
+        "et"   : options.et_sm_categories.split(),
+        "tt"   : options.tt_sm_categories.split(),
+        "vhtt" : options.vhtt_sm_categories.split(),
+        }
+    
+## switch to mssm event categories
+if options.analysis == "mssm" :
+    os.chdir("mssm")
+    categories = {
+        "mm"   : options.mm_mssm_categories.split(),
+        "em"   : options.em_mssm_categories.split(),
+        "mt"   : options.mt_mssm_categories.split(),
+        "et"   : options.et_mssm_categories.split(),
+        "tt"   : options.tt_mssm_categories.split(),
+        "hmm"  : options.hmm_mssm_categories.split(),
+        }
+
+## start the process here
+base = os.getcwd()
+for channel in channels :
+    for period in periods :
+        for cat in categories[channel] :
+            ## special treatment for incomplete 8TeV samples:
+            ## here only mH=115 is considered for mm/em/mt/et
+            ## and mH=125 for tt, vhtt misses completely.
+            single_mass = "125" if channel == "tt" else "115"
+            if channel == "vhtt" and period == "8TeV" :
+                continue
+            ## here the normal workflow continues
+            prefix = "" if channel == "vhtt" else "htt_"
+            os.chdir("{PWD}/{CHN}".format(CHN=prefix+channel, PWD=base))
+            os.system("datacard-project.py -c {CHN} -e {PER}-0{CAT} {PER}-0{CAT}".format(CHN=channel, PER=period, CAT=cat))
+            os.chdir("{PWD}/{CHN}/{PER}-0{CAT}".format(CHN=prefix+channel, PER=period, PWD=base, CAT=cat))
+            for mass in parseArgs(args) :
+                ## patch for incomplete 8TeV samples (see above)
+                if period == "8TeV" :
+                    if not int(mass) == int(single_mass) :
+                        print "drop out"
+                        continue
+                if options.SM4 :
+                    print "rescaling signal cross sections accoring to SM4 cross sections"
+                    os.system(r"root -q -l -b {CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/macros/rescale2SM4.C+\(true,\"{CHN}.inputs-{per}.root\"\)".format(
+                        CMSSW_BASE=os.environ['CMSSW_BASE'], CHN=channel, per=period.lowe()))
+                print "creating datacard for:", period, channel, cat, mass
+                os.system("create-datacard.py -i {CHN}.inputs-{per}.root -o {CHN}_{CAT}_{PER}-{MASS}.txt {MASS}".format(
+                    CHN=prefix+channel, per=period.lower(), CAT=cat, PER=period, MASS=mass))
+            os.system("mv *.* ../")
+            os.chdir("{PWD}/{CHN}".format(CHN=prefix+channel, PWD=base))
+            os.system("rm -r {PER}-0{CAT}".format(PER=period, CAT=cat))
+            os.system("rm -r cgs.* unc.*")
