42598ead98bc0c53fc77ff9139df1be26be164b8
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2012-06-21 21:57:30 +0000

    Allow specification of bsub options

--- scripts/lxb_limit.py
+++ scripts/lxb_limit.py
@@ -6,7 +6,7 @@ Parellelize calls to limit.py using LXBATCH.
 
 Usage:
 
-    lxb_limit.py submit_name "ARG_GLOB" [other options]
+    lxb_limit.py submit_name "bsub opts" "ARG_GLOB" "limit.py options"
 
 ARG_GLOB must expand to a list of directories.  Each entry in this list will be
 a separate lxbatch job.  NB the use of quotes about ARG_GLOB.
@@ -17,7 +17,7 @@ Note also you may need to escape the quotes around sub-arguments.  For example,
 
 would be
 
-    lxb_limit.py my_limits "cmb/*"  --tanb+ --userOpt \'--minosAlgo stepping\'
+    lxb_limit.py my_limits "-q 1nh -n 6" "cmb/*"  --tanb+ --tanbparallel 6 --userOpt \'--minosAlgo stepping\'
 
 will produce
 
@@ -39,13 +39,14 @@ import sys
 log = logging.getLogger("lxb_limit")
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
-if len(sys.argv) < 4:
-    print "Usage: lxb_limit.py submit_name \"ARG_GLOB\" \"[other options]\""
+if len(sys.argv) < 5:
+    print "Usage: lxb_limit.py submit_name \"bsub args\" \"ARG_GLOB\" \"[other options]\""
     sys.exit(1)
 
 name = sys.argv[1]
-dirglob = sys.argv[2]
-option_str = sys.argv[3:]
+bsubargs = sys.argv[2]
+dirglob = sys.argv[3]
+option_str = sys.argv[4:]
 
 script_template = '''
 #!/usr/bin/bash
@@ -69,5 +70,5 @@ with open(submit_name, 'w') as submit_script:
                 directory = dir
             ))
         os.system('chmod 755 %s' % script_file_name)
-        submit_script.write('bsub -q 1nh %s\n' % script_file_name)
+        submit_script.write('bsub %s %s\n' % (bsubargs, script_file_name))
 os.system('chmod 755 %s' % submit_name)
