22f3507643cbbababaa7583906efd8ba8f91da92
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2012-08-13 11:33:56 +0000

    Update Abdollah result snagger

--- setup/vhtt/snag_zh_result.sh
+++ setup/vhtt/snag_zh_result.sh
@@ -2,12 +2,15 @@
 
 # Copy over Abdollah's results.
 
-cp /afs/cern.ch/user/a/abdollah/public/For_Evan/Final_7TeV_1_Evan/cgs-sm-7TeV-01.conf .
-cp /afs/cern.ch/user/a/abdollah/public/For_Evan/Final_7TeV_1_Evan/unc-sm-7TeV-01.conf .
-cp /afs/cern.ch/user/a/abdollah/public/For_Evan/Final_7TeV_1_Evan/unc-sm-7TeV-01.vals .
-hadd -f vhtt_4l.inputs-sm-7TeV.root /afs/cern.ch/user/a/abdollah/public/For_Evan/Final_7TeV_1_Evan/shapes_*.root
+basedir7=/afs/cern.ch/user/a/abdollah/public/For_Evan/Final_7TeV_2_Evan_CutCount
+basedir8=/afs/cern.ch/user/a/abdollah/public/For_Evan/Final_8TeV_2_Evan_CutCount
 
-cp /afs/cern.ch/user/a/abdollah/public/For_Evan/Final_8TeV_1_Evan/cgs-sm-8TeV-01.conf .
-cp /afs/cern.ch/user/a/abdollah/public/For_Evan/Final_8TeV_1_Evan/unc-sm-8TeV-01.conf .
-cp /afs/cern.ch/user/a/abdollah/public/For_Evan/Final_8TeV_1_Evan/unc-sm-8TeV-01.vals .
-hadd -f vhtt_4l.inputs-sm-8TeV.root /afs/cern.ch/user/a/abdollah/public/For_Evan/Final_8TeV_1_Evan/shapes_*.root
+cp $basedir7/cgs-sm-7TeV-01.conf .
+cp $basedir7/unc-sm-7TeV-01.conf .
+cp $basedir7/unc-sm-7TeV-01.vals .
+hadd -f vhtt_4l.inputs-sm-7TeV.root $basedir7/shapes_*.root
+
+cp $basedir8/cgs-sm-8TeV-01.conf .
+cp $basedir8/unc-sm-8TeV-01.conf .
+cp $basedir8/unc-sm-8TeV-01.vals .
+hadd -f vhtt_4l.inputs-sm-8TeV.root $basedir8/shapes_*.root
