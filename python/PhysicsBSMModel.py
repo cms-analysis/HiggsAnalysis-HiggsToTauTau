021e692fad48ce148b219e11b206e0592f83a2a9
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-09-28 11:16:18 +0000

    some cleanup

--- python/PhysicsBSMModel.py
+++ python/PhysicsBSMModel.py
@@ -1,14 +1,16 @@
 ## --------------------------------------------------------------------------------------------------------------------------------------------------
 ##
-## This is a first toy version of a physics model t play around with bbH-ggH fits. The best way to use it at the moment would be to run it in the
-## points of the observed limits (in the current implementation), of to leave any further multiplication by MSSM cross section out and to run it
-## resulting in a model independent cross section limit. This would be even simpler, no multiplication by MSSM xsec were be necessary. Also tanb
-## could be left floating in principle, but this is only manageable in a degenerate mass scheme.
+## This is a first version of a trivial physics model to play around with combined bbH-ggH fits. The best way to use it at the moment is to define
+## r_ggH and r_bbH as free floating POI's and to pass on the yields normalized to 1/pb. This will result in best fit values for the cross section
+## on ggH and bbH from a simultaneous fit. The datacards can be used just as is. In principle further manipulations of the POI's and other defined
+## root variables are possible, but at the moment it seems that the simplest version of the fit is also the most sensible path to follow. Therefore
+## only comments are shown, how to deal with variables manipulations in comments in the implementation below. 
 ##
 ## How to use this little toy:
 ##
-## text2workspace.py htt_cmb_7TeV-400.txt -m 400 -P HiggsAnalysis.HiggsToTauTau.PhysicsBSMModel:floatingMSSMXSHiggs --PO modes=ggH,bbH --PO tanb=30
-## combine -M MultiDimFit htt_cmb_7TeV-400.root --robustFit 1 --algo=singles --cl=0.68
+## text2workspace.py htt_cmb_7TeV-$MASS.txt -m $MASS -P HiggsAnalysis.HiggsToTauTau.PhysicsBSMModel:floatingMSSMXSHiggs --PO modes=ggH,bbH \
+## [--PO tanb=30]
+## combine -M MultiDimFit htt_cmb_7TeV-$MASS.root --robustFit 1 --algo=[singles]/[2dcontour]/[grid] [--points 50] --cl=[0.68][0.95][0.99]
 ##
 ## --------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -18,11 +20,16 @@ from HiggsAnalysis.CombinedLimit.PhysicsModel import PhysicsModel
 from HiggsAnalysis.HiggsToTauTau.tools.mssm_xsec_tools import mssm_xsec_tools
 
 class MSSMLikeHiggsModel(PhysicsModel):
+    """
+    Base class for all further derivatives. At the moment this allows for ggH and bbH for production and hbb, htt, hmm for
+    decay. Supported run periods are 7Tev and 8TeV. The decay channels and run period has to be part of the datacards name.
+    The production channel is taken from the samples name following the CMSHCG convention for production channels.
+    """
     def getHiggsSignalYieldScale(self, production, decay, energy):
             raise RuntimeError, "Not implemented"
     def getYieldScale(self,bin,process):
         """
-        split in production and decay channels; call getHiggsSignalYieldScale; return 1 for backgrounds
+        Split in production and decay channels. Call getHiggsSignalYieldScale. Return 1 for backgrounds.
         """
         if not self.DC.isSignal[process]: return 1
         processSource = process
@@ -48,19 +55,21 @@ class MSSMLikeHiggsModel(PhysicsModel):
 
 class FloatingMSSMXSHiggs(MSSMLikeHiggsModel):
     """
-    float ggH and bbH independently
+    Trivial model to float ggH and bbH independently. At the moment only ggH and bbh are supported. Extensions to the other
+    production channels channels are given in comments. Also the principle how to deal with manipulations of the POI's and
+    other defined roofit variables are shown in comments. 
     """
     def __init__(self):
         MSSMLikeHiggsModel.__init__(self) # not using 'super(x,self).__init__' since I don't understand it
-        self.tanb = None
-        self.modes = [ "ggH", "bbH" ]
+        #self.tanb   = None
         self.mARange = []
+        self.modes   = [ "ggH", "bbH" ]
     def setPhysicsOptions(self,physOptions):
         """
-        options are: tanb, modes, mARange
+        Options are: modes. Examples for options like mARange and tanb are given in comments. 
         """
         for po in physOptions:
-            if po.startswith("tanb="): self.tanb = po.replace("tanb=", "")
+            #if po.startswith("tanb="): self.tanb = po.replace("tanb=", "")
             if po.startswith("modes="): self.modes = po.replace("modes=","").split(",")
             if po.startswith("mARange="):
                 self.mARange = po.replace("mARange=","").split(":")
@@ -70,18 +79,23 @@ class FloatingMSSMXSHiggs(MSSMLikeHiggsModel):
                     raise RuntimeError, "Extrema for mA range defined with inverterd order. Second element must be larger than first element"
     def doParametersOfInterest(self):
         """
-        create POI and other parameters, and define the POI set. Evaluate cross section for given values of mA and tanb
+        Create POI and other parameters, and define the POI set. E.g. Evaluate cross section for given values of mA and tanb
         """
-        # --- Evaluate Cross Sections for given values of tanb and mA ---
-        mssm_scan = mssm_xsec_tools("{CMSSW_BASE}/src/{PATH}".format(CMSSW_BASE=os.environ['CMSSW_BASE'], PATH="HiggsAnalysis/HiggsToTauTau/data/out.mhmax-mu+200-7TeV-nnlo.root"))
-        mssm_xsec = mssm_scan.query(self.options.mass, float(self.tanb))
-        self.modelBuilder.doVar("bbH_xsec[%g]" % (mssm_xsec['higgses']['A']['xsec']['santander']*mssm_xsec['higgses']['A']['BR']))
-        self.modelBuilder.doVar("ggH_xsec[%g]" % (mssm_xsec['higgses']['A']['xsec']['ggF'      ]*mssm_xsec['higgses']['A']['BR']))
-        # --- Signal Strength as only POI --- 
-        if "ggH" in self.modes: self.modelBuilder.doVar("r_ggH[1,0,20]");
-        if "bbH" in self.modes: self.modelBuilder.doVar("r_bbH[1,0,20]");
+        ## Example: evaluate cross sections for given values of tanb and mA, the names of the variables are ggH_xsec and bbH_xsec,
+        ## the values are given by the value in brakets. In principle three values can be passed on: value, lower bound and upper
+        ## bound.
+        #
+        #mssm_scan = mssm_xsec_tools("{CMSSW_BASE}/src/{PATH}".format(CMSSW_BASE=os.environ['CMSSW_BASE'], PATH="HiggsAnalysis/HiggsToTauTau/data/out.mhmax-mu+200-7TeV-nnlo.root"))
+        #mssm_xsec = mssm_scan.query(self.options.mass, float(self.tanb))
+        #self.modelBuilder.doVar("bbH_xsec[%g]" % (mssm_xsec['higgses']['A']['xsec']['santander']*mssm_xsec['higgses']['A']['BR']))
+        #self.modelBuilder.doVar("ggH_xsec[%g]" % (mssm_xsec['higgses']['A']['xsec']['ggF'      ]*mssm_xsec['higgses']['A']['BR']))
+        #
+        ## Define signal strengths on ggH and bbH as POI, NOTE: the range of the POIs is defined here
+        if "ggH" in self.modes: self.modelBuilder.doVar("r_ggH[10,0,20]");
+        if "bbH" in self.modes: self.modelBuilder.doVar("r_bbH[10,0,20]");
         poi = ",".join(["r_"+m for m in self.modes])
-        # --- Higgs Mass as other parameter ----
+        ## Define Higgs boson mass as another parameter. It will be floating if mARange is set otherwise it will be treated
+        ## as fixed. NOTE: this is only left here as an extended example. It's not useful to have mA floating at the moment.
         if self.modelBuilder.out.var("MH"):
             if len(self.mHRange):
                 print 'MH will be left floating within', self.mARange[0], 'and', self.mARange[1]
@@ -104,12 +118,23 @@ class FloatingMSSMXSHiggs(MSSMLikeHiggsModel):
         self.modelBuilder.doSet("POI",poi)
     def getHiggsSignalYieldScale(self,production,decay, energy):
         if production == "ggH" or production == "bbH":
-            self.modelBuilder.factory_('expr::%s_yield("@0*@1", r_%s, %s_xsec)' % (production, production, production))
+            ## This is an example how to multiply the yields r_ggH and r_bbH with the roofit variables ggH_xsec and bbH_xsec
+            ## that have been defined above, with the help of a roofit expression.
+            #
+            #self.modelBuilder.factory_('expr::%s_yield("@0*@1", r_%s, %s_xsec)' % (production, production, production))
+            #
+            ## This is the trivial model that we follow now. We just pass on the values themselves. Yes this is just a
+            ## trivial renaming, but we leave it in as an example. Instead also r_ggH and r_bbH could be passed on directly
+            ## in the return function instead of the newly defined variables ggH_yields or bbH_yield.
+            self.modelBuilder.factory_('expr::%s_yield("@0", r_%s)' % (production, production))
             return "%s_yield" % production
-        #if production == "bbH": return ("r_bbH" if "bbH" in self.modes else 1)
+        ## This just corresponds to entry points to extend the model to other production channels like qqH, ttH, VH. 
+        #
         #if production == "qqH": return ("r_qqH" if "qqH" in self.modes else 1)
         #if production == "ttH": return ("r_ttH" if "ttH" in self.modes else 1)
         #if production in [ "WH", "ZH", "VH" ]: return ("r_VH" if "VH" in self.modes else 1)
+        #
         raise RuntimeError, "Unknown production mode '%s'" % production
 
+## this is the entry point for text2workspace.py
 floatingMSSMXSHiggs = FloatingMSSMXSHiggs()
