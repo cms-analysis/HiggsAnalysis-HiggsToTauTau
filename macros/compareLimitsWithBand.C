65922514328ec46be3b36e9b50933bf39b350f7a
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-10-04 10:37:21 +0000

    currently this macro carries legends for HCP projections

--- macros/compareLimitsWithBand.C
+++ macros/compareLimitsWithBand.C
@@ -160,10 +160,10 @@ void compareLimitsWithBand(const char* filename, const char* channelstr, double 
   leg->SetFillColor (kWhite);
   //leg->SetHeader( "95% CL Limits" );
   if(observed.size()>1 && addObserved){ leg->AddEntry( observed[1] , "simulation signal inj. (m_{H}=125 GeV)",  "L"  );}
-  if(observed.size()>0){ leg->AddEntry( observed[0] , "observed",  "PL" );}
+  if(observed.size()>0){ leg->AddEntry( observed[0] , "observed (HIG-12-032)",  "PL" );}
   //if(observed.size()>1){ leg->AddEntry( observed[1] , "single toy (inj. signal)",  "PL" );}
-  if(expected.size()>0){ leg->AddEntry( expected[0] , "expected"             ,  "L" );}
-  if(expected.size()>1 && addExpected){ leg->AddEntry( expected[1] , "projection to 30/fb",  "L" );}
+  if(expected.size()>0){ leg->AddEntry( expected[0] , "expected (HIG-12-032)"             ,  "L" );}
+  if(expected.size()>1 && addExpected){ leg->AddEntry( expected[1] , "projection to 5+12/fb",  "L" );}
   leg->AddEntry( innerBand[0], "#pm 1#sigma expected" ,  "F" );
   leg->AddEntry( outerBand[0], "#pm 2#sigma expected" ,  "F" );
   leg->Draw("same");
