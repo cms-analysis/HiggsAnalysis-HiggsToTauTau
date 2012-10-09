268fa6b198f495019eb6ed7b169e28d7df31d466
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2012-10-09 12:59:17 +0000

    update

--- scripts/lxq_inject.py
+++ scripts/lxq_inject.py
@@ -12,6 +12,7 @@ parser.add_option("--qsub", dest="qsub", default="-l site=hh -j y -o /dev/null -
 parser.add_option("--njob", dest="njob", default="100", type="string", help="Number of jobs for which to inject signal. [Default: \"100\"]")
 parser.add_option("--masses", dest="masses", default="110-145:5", type="string", help="Masses argument for signal injection. [Default: \"100-145:5\"]")
 parser.add_option("--options", dest="opts", default="--observedOnly", type="string", help="Options for limit calculation with limit.py. [Default: \"--observedOnly\"]")
+parser.add_option("--collect", dest="collect", default=False,  action="store_true", help="Collect the individual jobs of a single batch submission. [Default: False)")
 ## check number of arguments; in case print usage
 (options, args) = parser.parse_args()
 
@@ -52,22 +53,41 @@ inject-signal.py -i {input}_{jobID} -r {rnd} {masses}
 limit.py --asymptotic {options} {input}_{jobID}/*
 '''
 
-submit_name = '%s_submit.sh' % name
-with open(submit_name, 'w') as submit_script:
-    submit_script.write('export cmssw_base=$CMSSW_BASE\n')
-    submit_script.write('export scram_arch=$SCRAM_ARCH\n')
-    for idx in range(int(njob)):
-        log.info("Generating script for limit.py with injected signal for job %g", idx)
-        script_file_name = '%s_%i.sh' % (name, idx)
-        with open(script_file_name, 'w') as script:
-            script.write(script_template.format(
-                working_dir = os.getcwd(),
-                options = opts,
-                rnd = rnd,
-                input = input,
-                jobID = idx,
-                masses = masses
-            ))
-        os.system('chmod a+x %s' % script_file_name)
-        submit_script.write('qsub %s %s\n' % (qsubargs, script_file_name))
-os.system('chmod a+x %s' % submit_name)
+if options.collect :
+    for mass in parseArgs(args) :
+        ## to allow for more files to be combined distinguish by first digit in a first
+        ## iteration, than cpomine the resulting 10 files to the final output file.
+        for idx in range(10) :
+            os.system("hadd {INPUT}/{MASS}/batch_collected_{IDX}.root {INPUT}_*{IDX}/{MASS}/higgsCombine-exp.Asymptotic.mH{MASS}.root".format(
+                INPUT=input,
+                MASS=mass,
+                IDX=idx
+                ))
+        for idx in range(10) :
+            os.system("hadd {INPUT}/{MASS}/batch_collected.root {INPUT}/{MASS}/batch_collected_{IDX}.root".format(
+                INPUT=input,
+                MASS=mass,
+                IDX=idx
+                ))
+        os.system("rm -r {INPUT}/{MASS}/batch_collected_*.root")
+else:
+    submit_name = '%s_submit.sh' % name
+    with open(submit_name, 'w') as submit_script:
+        submit_script.write('export cmssw_base=$CMSSW_BASE\n')
+        submit_script.write('export scram_arch=$SCRAM_ARCH\n')
+        for idx in range(int(njob)):
+            log.info("Generating script for limit.py with injected signal for job %g", idx)
+            script_file_name = '%s_%i.sh' % (name, idx)
+            with open(script_file_name, 'w') as script:
+                script.write(script_template.format(
+                    working_dir = os.getcwd(),
+                    options = opts,
+                    rnd = rnd,
+                    input = input,
+                    jobID = idx,
+                    masses = masses
+                    ))
+            os.system('chmod a+x %s' % script_file_name)
+            submit_script.write('qsub %s %s\n' % (qsubargs, script_file_name))
+    os.system('chmod a+x %s' % submit_name)
+                
