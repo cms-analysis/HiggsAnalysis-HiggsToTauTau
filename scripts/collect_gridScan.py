2642e8b9e882a327143c134268e961489fbc143d
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2012-10-11 12:19:49 +0000

    collects the rootfiles from each job of lxq/b_gridScan.py and merges them

---
+++ scripts/collect_gridScan.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser, OptionGroup
+
+## set up the option parser
+parser = OptionParser(usage="usage: %prog [options]",
+                      description="Script to collect and hadd the rootfiles of the output of lxq/b_gridScan.py. Also the temp files are deleted!")
+parser.add_option("-d", "--directory", dest="directory", default="test", type="string", help="Name of the directory where the gridScan points are . [Default: test-injected]")
+## check number of arguments; in case print usage
+(options, args) = parser.parse_args()
+
+import os
+import sys
+
+mass = options.directory[options.directory.rfind("/")+1:]
+os.system("hadd {DIRECTORY}/higgsCombine.MultiDimFit.mH{MASS}.root {DIRECTORY}/higgsCombine_*.MultiDimFit.mH{MASS}.root".format(
+    DIRECTORY=options.directory,
+    MASS=mass
+    ))
+os.system("rm {DIRECTORY}/tmp*".format(
+    DIRECTORY=options.directory
+    ))
+os.system("rm {DIRECTORY}/higgsCombine_*.MultiDimFit.mH{MASS}.root".format(
+    DIRECTORY=options.directory,
+    MASS=mass
+    ))
+        
+            
