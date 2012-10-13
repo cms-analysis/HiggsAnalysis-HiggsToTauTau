24c4034546cdaa830934fef9adcc6d0050d5fc3b
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-10-13 16:14:52 +0000

    removed obsolete commented stuff and extended to work with MSSM

--- macros/postfit.C
+++ macros/postfit.C
@@ -25,15 +25,30 @@
    This is a macro to create di-tau masses for all classic htt channels combined
 */
 
+float maximum(TH1F* h, bool LOG=false){
+  if(LOG){
+    if(h->GetMaximum()>1000){ return 1000.*TMath::Nint(500*h->GetMaximum()/1000.); }
+    if(h->GetMaximum()>  10){ return   10.*TMath::Nint( 50*h->GetMaximum()/  10.); }
+    return 50*h->GetMaximum(); 
+  }
+  else{
+    if(h->GetMaximum()>  12){ return 10.*TMath::Nint((1.3*h->GetMaximum()/10.)); }
+    if(h->GetMaximum()> 1.2){ return TMath::Nint((1.3*h->GetMaximum())); }
+    return 1.3*h->GetMaximum(); 
+  }
+}
+
 void 
-postfit(const char* inputfile, const char* dataset = "2011+2012", const char* extra="", float min=0.1, float max=500., bool log=true)
+postfit(const char* inputfile, const char* analysis = "SM", const char* dataset = "2011+2012", const char* extra="", float min=0.1, float max=-1., bool log=true)
 {
   // defining the common canvas, axes pad styles
   SetStyle(); gStyle->SetLineStyleString(11,"20 10");
-
+  // switch for MSSM/SM
+  bool MSSM = std::string(analysis) == std::string("MSSM");
+ 
   if (std::string(dataset) == std::string("2011"     )){ dataset = "2011, #sqrt{s} = 7 TeV, L = 4.9 fb^{-1}"; }
   if (std::string(dataset) == std::string("2012"     )){ dataset = "2012, #sqrt{s} = 8 TeV, L = 12.0 fb^{-1}"; }
-  if (std::string(dataset) == std::string("2011+2012")){ dataset = "Preliminary, #sqrt{s} = 7-8 TeV, L = 17 fb^{-1}"; }
+  if (std::string(dataset) == std::string("2011+2012")){ dataset = ", #sqrt{s} = 7-8 TeV, L = 17 fb^{-1}"; }
 
   TFile* input = new TFile(inputfile);
   TH1F* Fakes  = (TH1F*)input->Get("Fakes"   ); 
@@ -58,45 +73,8 @@ postfit(const char* inputfile, const char* dataset = "2011+2012", const char* ex
   //data->GetXaxis()->SetRange(0, 28);
   data->SetNdivisions(505);
   data->SetMinimum(min);
-  data->SetMaximum(max);
+  data->SetMaximum(max>0 ? max : maximum(Ztt, log));
   data->Draw("e");
-
-  /* 
-     For combination paper HIG-12-028
-
-  data->GetYaxis()->SetTitle("Event/10GeV");
-  data->GetXaxis()->SetTitle("m_{#tau#tau} (GeV)");
-
-  TPaveText* label1  = new TPaveText(0.27, 0.84, 0.35, 0.93, "NDC");
-  label1->SetBorderSize(   0 );
-  label1->SetFillStyle(    0 );
-  label1->SetTextAlign(   12 );
-  label1->SetTextSize ( 0.032 );
-  label1->SetTextColor(    1 );
-  label1->SetTextFont (   42 );
-  label1->AddText("CMS H#rightarrow#tau#tau");
-  label1->Draw();
-
-  TPaveText* label2  = new TPaveText(0.21, 0.82, 0.35, 0.86, "NDC");
-  label2->SetBorderSize(   0 );
-  label2->SetFillStyle(    0 );
-  label2->SetTextAlign(   12 );
-  label2->SetTextSize ( 0.032 );
-  label2->SetTextColor(    1 );
-  label2->SetTextFont (   42 );
-  label2->AddText("#sqrt{s} = 7 TeV, L = 4.9 fb^{-1}");
-  label2->Draw();
-
-  TPaveText* label3  = new TPaveText(0.21, 0.80, 0.35, 0.79, "NDC");
-  label3->SetBorderSize(   0 );
-  label3->SetFillStyle(    0 );
-  label3->SetTextAlign(   12 );
-  label3->SetTextSize ( 0.032 );
-  label3->SetTextColor(    1 );
-  label3->SetTextFont (   42 );
-  label3->AddText("#sqrt{s} = 8 TeV, L = 5.1 fb^{-1}");
-  label3->Draw();
-  */
 
   if(log){
     Ztt  ->Draw("same");
@@ -124,9 +102,14 @@ postfit(const char* inputfile, const char* dataset = "2011+2012", const char* ex
   CMSPrelim(dataset, extra, 0.17, 0.835);
 
   float lower_bound = EWK1 ? 0.60 : 0.65;
-  TLegend* leg = new TLegend(0.53, lower_bound, 0.93, 0.90);
+  TLegend* leg = new TLegend(MSSM ? 0.45 : 0.50, lower_bound, 0.93, 0.90);
   SetLegendStyle(leg);
-  leg->AddEntry(ggH  , "(5#times) H#rightarrow#tau#tau  m_{H}=125" , "L" );
+  if(MSSM){
+    leg->AddEntry(ggH  , "#phi(160 GeV)#rightarrow#tau#tau, tan#beta=8" , "L" );
+  }
+  else{
+    leg->AddEntry(ggH  , "5#timesH(125)#rightarrow#tau#tau" , "L" );
+  }
   leg->AddEntry(data , "observed"                       , "LP");
   leg->AddEntry(Ztt  , "Z#rightarrow#tau#tau"           , "F" );
   if(EWK1){
@@ -139,98 +122,6 @@ postfit(const char* inputfile, const char* dataset = "2011+2012", const char* ex
   leg->AddEntry(ttbar, "t#bar{t}"                       , "F" );
   leg->AddEntry(Fakes, "QCD"                            , "F" );
   leg->Draw();
-
-//  TPaveText* mssm  = new TPaveText(0.83, 0.85, 0.95, 0.90, "NDC");
-//  mssm->SetBorderSize(   0 );
-//  mssm->SetFillStyle(    0 );
-//  mssm->SetTextAlign(   12 );
-//  mssm->SetTextSize ( 0.03 );
-//  mssm->SetTextColor(    1 );
-//  mssm->SetTextFont (   62 );
-//  mssm->AddText("m_{H}=125");
-//  mssm->Draw();
-
-//
-//  TPaveText* cat;
-//  if(std::string(category) == std::string("b") || std::string(category) == std::string("nob") || std::string(category) == std::string("nob_LOG")){
-//    //cat = new TPaveText(0.52, 0.60, 0.90, 0.65, "NDC");
-//    cat = new TPaveText(0.57, 0.58, 0.90, 0.65, "NDC");
-//  }
-//  else{
-//    //cat = new TPaveText(0.57, 0.60, 0.90, 0.65, "NDC");
-//    cat = new TPaveText(0.60, 0.58, 0.90, 0.65, "NDC");
-//  }
-//  cat->SetBorderSize(   0 );
-//  cat->SetFillStyle(    0 );
-//  cat->SetTextAlign(   12 );
-//  cat->SetTextSize ( 0.03 );
-//  cat->SetTextColor(    1 );
-//  cat->SetTextFont (   62 );
-//  if(category_string == std::string("b")          ){cat->AddText("b-Tag Category");    }
-//  if(category_string == std::string("nob")        ){cat->AddText("Non b-Tag Category");}
-//  if(category_string == std::string("nob_LOG")    ){cat->AddText("Non b-Tag Category");}
-//  if(category_string == std::string("vbf")        ){cat->AddText("VBF Category");      }
-//  if(category_string == std::string("boost")      ){cat->AddText("Boosted Category");  }
-//  if(category_string == std::string("novbf")      ){cat->AddText("0/1-Jet Category");  }
-//  if(category_string == std::string("novbf_LOG")  ){cat->AddText("0/1-Jet Category");  }
-//  cat->Draw();
-//
-//  TPaveText* channels  = new TPaveText(0.78, 0.895, 0.90, 0.995, "NDC");
-//  channels->SetBorderSize(   0 );
-//  channels->SetFillStyle(    0 );
-//  channels->SetTextAlign(   12 );
-//  channels->SetTextSize ( 0.03 );
-//  channels->SetTextColor(    1 );
-//  channels->SetTextFont (   62 );
-//  channels->AddText("All channels");
-//  channels->Draw();
-//
-//  TLegend* leg;
-//  TPaveText* mssm;
-//  if(std::string(category) == std::string("b") || std::string(category) == std::string("nob") || std::string(category) == std::string("nob_LOG")){
-//    leg = new TLegend(0.52, 0.65, 0.9, 0.90);
-//    SetLegendStyle(leg);
-//    leg->AddEntry(Higgs, "#phi#rightarrow#tau#tau" , "L" );
-//    leg->AddEntry(data , "observed"                , "LP");
-//    leg->AddEntry(Ztt  , "Z#rightarrow#tau#tau"    , "F" );
-//    leg->AddEntry(ttbar, "t#bar{t}"                , "F" );
-//    leg->AddEntry(EWK  , "electroweak"             , "F" );
-//    leg->AddEntry(Fakes, "multijets"               , "F" );
-//    leg->Draw();
-//    
-//    mssm  = new TPaveText(0.69, 0.85, 0.90, 0.90, "NDC");
-//    mssm->SetBorderSize(   0 );
-//    mssm->SetFillStyle(    0 );
-//    mssm->SetTextAlign(   12 );
-//    mssm->SetTextSize ( 0.03 );
-//    mssm->SetTextColor(    1 );
-//    mssm->SetTextFont (   62 );
-//    mssm->AddText("(m_{A}=120, tan#beta=20)");
-//  }
-//  else{
-//    leg = new TLegend(0.57, 0.65, 0.95, 0.90);
-//    SetLegendStyle(leg);
-//    SetLegendStyle(leg);
-//    leg->AddEntry(Higgs, "(5#times) H#rightarrow#tau#tau" , "L" );
-//    leg->AddEntry(data , "observed"                , "LP");
-//    leg->AddEntry(Ztt  , "Z#rightarrow#tau#tau"    , "F" );
-//    leg->AddEntry(ttbar, "t#bar{t}"                , "F" );
-//    leg->AddEntry(EWK  , "electroweak"             , "F" );
-//    leg->AddEntry(Fakes, "multijets"               , "F" );
-//
-//    
-//    mssm  = new TPaveText(0.83, 0.85, 0.95, 0.90, "NDC");
-//    mssm->SetBorderSize(   0 );
-//    mssm->SetFillStyle(    0 );
-//    mssm->SetTextAlign(   12 );
-//    mssm->SetTextSize ( 0.03 );
-//    mssm->SetTextColor(    1 );
-//    mssm->SetTextFont (   62 );
-//    mssm->AddText("m_{H}=120");
-//    mssm->Draw();
-//  }
-//  leg->Draw();
-//  mssm->Draw();
 
   /*
     prepare output
