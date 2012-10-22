f9c8dd0ee371bfd47accd4ff72e64fc4c5a89af0
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2012-10-22 12:03:41 +0000

    Use current conventions for interpolated mass points in VH

--- setup/vhtt/merge_shape_files.sh
+++ setup/vhtt/merge_shape_files.sh
@@ -6,24 +6,34 @@ lltCategories="emt,mmt"
 zhCategories="mmmt_zh,mmet_zh,mmme_zh,mmtt_zh,eemt_zh,eeet_zh,eeem_zh,eett_zh" 
 lttCategories="ett_sm,mtt_sm" 
 
+domorph() 
+{
+  echo "Morphing file $1, samples $2, categories $3" 
+  echo "110->130, in 0.5"
+  horizontal-morphing.py \
+    --categories="$3"\
+    --samples="$2" \
+    --uncerts="" --masses="110,120,130" --step-size=0.5 \
+    -i $1 -v
+  echo "130->145, in 1.0"
+  horizontal-morphing.py \
+    --categories="$3"\
+    --samples="$2" \
+    --uncerts="" --masses="130,140" --step-size=1 \
+    -i $1 -v --extrapolate="141,142,143,144,145"
+  echo "124.5->126.5, in 0.1"
+  horizontal-morphing.py \
+    --categories="$3"\
+    --samples="$2" \
+    --uncerts="" --masses="124,127" --step-size=0.1 \
+    -i $1 -v 
+}
+
 morph() 
 {
   echo "LLT channels"
-  horizontal-morphing.py \
-    --categories="${lltCategories}"\
-    --samples="VH{MASS},VH_hww{MASS}" \
-    --uncerts="" --masses="110,120,130,140" --step-size=1 \
-    --extrapolate="145" -i $1 -v
-  echo "ZH channels"
-  horizontal-morphing.py \
-    --categories="${zhCategories}"\
-    --samples="VH{MASS},VH_hww{MASS}" \
-    --uncerts="" --masses="110,115,120,125,130,135,140,150" --step-size=1 -i $1
-  #echo "LTT channels"
-  #horizontal-morphing.py \
-    #--categories="${lttCategories}"\
-    #--samples="VH{MASS}" \
-    #--uncerts="" --masses="110,120,130,140" --step-size=1 -i $1
+  domorph $1 "WH{MASS},WH_hww{MASS}"  "${lltCategories}"
+  domorph $1 "VH{MASS},VH_hww{MASS}"  "${zhCategories}"
 }
 
 echo "Combining 7TeV"
