06c4d45a5c78330d9e22f52cba5bcd766dd228f7
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2012-10-22 15:28:53 +0000

    make sure to use svfit rootfiles for mm - still not robust and pretty much hardcoded :/

--- test/mlfit_and_copy.py
+++ test/mlfit_and_copy.py
@@ -4,29 +4,45 @@ from optparse import OptionParser, OptionGroup
 # ML-fit a directory of datcards and copy here for making plots
 # Usage: ./mlfit_and_copy.py <path>MY-LIMITS/125
 
-parser = OptionParser(usage="usage: %prog [options] datacatd.txt",
+parser = OptionParser(usage="usage: %prog [options] input_path",
                       description="Script to build up the necessart enviroment for postfit plots - including maxlikelihood calculation.")
+parser.add_option("--rMin", dest="rMin", default="-5", type="string", help="Minimum value of signal strenth. [Default: -2]")
+parser.add_option("--rMax", dest="rMax", default="5", type="string", help="Maximum value of signal strenth. [Default: 2]")
+parser.add_option("-c", "--channels", dest="channels", default="mm em mt et", type="string", help="List of channels, for which datacards should be created. The list should be embraced by call-ons and separeted by whitespace or comma. Available channels are mm, em, mt, et, tt, vhtt. [Default: \"mm em mt et\"]")
+parser.add_option("-a", "--analysis", dest="analysis", default="sm", type="string", help="Type of analysis (sm or mssm). Lower case is required. [Default: \"sm\"]")
 (options, args) = parser.parse_args()
+
+if len(args) < 1 :
+    parser.print_usage()
+    exit(1)
+
 
 import os
 import sys
 
-analysis = args[0]
-dir = args[1]
+dir = args[0]
 
-print "Fitting %s : %s" % (analysis, dir)
+print "Fitting %s : %s" % (options.analysis, dir)
 
 os.system("mkdir -p datacards")
 os.system("mkdir -p root")
 os.system("mkdir -p fitresults")
-os.system("limit.py --max-likelihood --stable --rMin -10 --rMax 10 %s" % dir)
+os.system("limit.py --max-likelihood --stable --rMin %s --rMax %s %s" % (options.rMin, options.rMax, dir))
 
-if analysis == "SM" :
+if options.analysis == "sm" :
     os.system("cp -v %s/out/mlfit.txt ./fitresults/mlfit_sm.txt" % dir)
     os.system("cp -v %s/*.txt ./datacards" % dir)
-    os.system("cp -v %s/../common/*.root ./root" % dir)
+    os.system("cp -v %s/../common/*.root ./root" % dir)	
+    if options.channels.find("mm")>-1 :
+	os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-sm-8TeV_postfit.root ./root/htt_mm.input_8TeV.root")
+	os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-sm-7TeV_postfit.root ./root/htt_mm.input_7TeV.root")
 
-if analysis == "MSSM" :
+if options.analysis == "mssm" :
     os.system("cp -v %s/out/mlfit.txt ./fitresults/mlfit_mssm.txt" % dir)
     os.system("cp -v %s/*.txt ./datacards" % dir)
-    os.system("cp -v %s/../common/*TeV.root ./root" % dir)
+    os.system("cp -v %s/../common/*.root ./root" % dir)	
+    if options.channels.find("mm")>-1 :
+	os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-mssm-8TeV_postfit.root ./root/htt_mm.input_8TeV.root")
+	os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-mssm-7TeV_postfit.root ./root/htt_mm.input_7TeV.root")
+
+
