8c0f6632935974e382dcda51a9c5495d6b738b1e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-10-27 19:23:28 +0000

    layout for HCP

--- python/layouts/sm_htt_layout.py
+++ python/layouts/sm_htt_layout.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 layout = cms.PSet(
     ## dataset
-    #dataset = cms.string(" 2012, #sqrt{s} = 8 TeV, H #rightarrow #tau #tau, L = 5.0 fb^{-1}"),
+    #dataset = cms.string(" 2012, #sqrt{s} = 8 TeV, H #rightarrow #tau #tau, L = 12.0 fb^{-1}"),
     #dataset = cms.string(" 2011, #sqrt{s} = 7 TeV, H #rightarrow #tau #tau, L = 4.9 fb^{-1}"),
-    dataset = cms.string(" pre-approval for HCP, #sqrt{s} = 7-8 TeV, H #rightarrow #tau #tau, L = 17 fb^{-1}"),
+    dataset = cms.string(", for HCP, H #rightarrow #tau #tau, L = 17 fb^{-1}"),
     ## x-axis title
     xaxis = cms.string("m_{H} [GeV]"),
     ## x-axis title
@@ -24,7 +24,7 @@ layout = cms.PSet(
     ## min for plotting
     min = cms.double(0.),
     ## max for plotting
-    max = cms.double(8.), ##12
+    max = cms.double(10.), ##3.
     ## min for plotting
     log = cms.int32(0),
     ## define verbosity level
