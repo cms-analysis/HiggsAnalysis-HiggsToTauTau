b4d976dc9d863b883dac794ab12889e7864bfad4
Philip Coleman Harris <sha1-dd4ce72ed302de3d017f8bf6c5b43ad5d0f6400d@cern.ch>
2012-10-29 20:18:40 +0000

    quick script to change fit axes

---
+++ scripts/changeAxis.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from optparse import OptionParser, OptionGroup
+
+## set up the option parser
+parser = OptionParser(usage="usage: %prog [options]",
+                      description="Script to scale signal in inputs files by the cross section for SM Higgs boson. This will transform expectations normalized to 1/pb to yield predictions. The signal samples have to be given as options. ARGS corresponds to the given masses, for which the scaling should be performed.")
+parser.add_option("-i"  ,"--input", dest="input", default="test.root", type="string", help="Input file where to find the signal histograms (or workspaces). [Default: test.root]")
+parser.add_option("-k"  ,"--key", dest="key", default="CMS", type="string",         help="Key to change range")
+parser.add_option("-m"  ,"--min", dest="min", default="50" , type="int",help="Min [Default:50]")
+parser.add_option("-v"  ,"--verbose", dest="verbose", default=False, action="store_true", help="increase verbosity. [Default: False]")
+
+# check number of arguments; in case print usage
+(options, args) = parser.parse_args()
+#if len(args) < 1 :
+#    parser.print_usage()
+#    exit(1)
+
+import os
+import re
+
+print " You are running with configuration: "
+print "-------------------------------------"
+print " input   : ", options.input
+print " key     : ", options.key
+print " min     : ", options.min
+
+from HiggsAnalysis.HiggsToTauTau.utils import parseArgs 
+
+## do the rescaling
+os.system(r"root -l -b -q {CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/macros/changeAxis.C+\(true,\"{OLD}\",{NEW},\"{INPUTFILE}\",\"\",0\)".format(
+    CMSSW_BASE=os.environ.get("CMSSW_BASE"), OLD=options.key,NEW=options.min, INPUTFILE=options.input))
+
