7fe338d4e136fcf25eb0f9df1fff453c5a3af8ba
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-11-01 00:06:32 +0000

    add some more specifics to cp

--- test/mlfit_and_copy.py
+++ test/mlfit_and_copy.py
@@ -33,7 +33,7 @@ if not options.skip :
 if options.analysis == "sm" :
     os.system("cp -v %s/out/mlfit.txt ./fitresults/mlfit_sm.txt" % dir)
     os.system("cp -v %s/*.txt ./datacards" % dir)
-    os.system("cp -v %s/../common/*.root ./root" % dir)	
+    os.system("cp -v %s/../common/*TeV.root ./root" % dir)	
     ## for mm override the histograms as used for the limit calculation in favour of something more human readible
     os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-sm-8TeV_postfit.root ./root/htt_mm.input_8TeV.root")
     os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-sm-7TeV_postfit.root ./root/htt_mm.input_7TeV.root")
