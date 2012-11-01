42b9833ab33e5e88ade678b0c3f75b2b91808f57
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-11-01 00:08:12 +0000

    fixing include path

--- macros/postfit.C
+++ macros/postfit.C
@@ -14,7 +14,7 @@
 #include <TAttLine.h>
 #include <TPaveText.h>
 
-#include "$CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/interface/HttStyles.h"
+#include "HiggsAnalysis/HiggsToTauTau/interface/HttStyles.h"
 
 
 /**
