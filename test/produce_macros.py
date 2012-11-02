6462d345ac9cc1f1786afdcdddab4be9c491b887
Andreas Hinzmann <sha1-5d7a3aa3734fdea6829a5f92e42cf5075d63a16b@cern.ch>
2012-11-02 10:17:46 +0000

    revert last uncertainty bands again, since they were not bugged

--- test/produce_macros.py
+++ test/produce_macros.py
@@ -180,33 +180,23 @@ class Analysis:
                        for bin in range(1,hist.GetNbinsX()+1):
 		         shift = self.process_shape_weight[curr_name][shape_name]
                          out_line = ''
-			 value = 1
-                         upper = hist_up.GetBinContent(bin)
-                         lower = hist_down.GetBinContent(bin) if hist_down.GetBinContent(bin)>0.01 else 0.
-                         central = hist.GetBinContent(bin)
-		         if shift>0 and central>0 and upper!=central:
-                             if central :
-                                 value = shift*(upper/central-1)+1
-		         elif shift<0 and central>0 and lower!=central:
-                             if lower :
-                                 value = shift*(central/lower-1)+1
-			 if value!=1:
-		             print_me  = '''std::cout << "scaling bin %(bin)i by %(value)f" << std::endl;''' % {"bin":bin, "value":value}
-		             out_line  = print_me+"hin->SetBinContent(%(bin)i,hin->GetBinContent(%(bin)i)*%(value)f); \n" % {"bin":bin, "value":value}
+			 value = 0
+		         if shift>0:
+                             value = (hist_up.GetBinContent(bin)-hist.GetBinContent(bin))/hist.GetBinWidth(bin)
+		         elif shift<0:
+                             value = (hist.GetBinContent(bin)-hist_down.GetBinContent(bin))/hist.GetBinWidth(bin)
+			 if value!=0:
+		             print_me  = '''std::cout << "scaling bin %(bin)i by %(shift)f %(name)s" << std::endl;''' % {"bin":bin, "shift":shift, "name":shape_name}
+		             out_line  = print_me+"hin->SetBinContent(%(bin)i,hin->GetBinContent(%(bin)i)+%(value)f); \n" % {"bin":bin, "value":value*shift}
 			 if options.uncertainties:
-			   uncertainty=1
-			   if hist_down.GetBinContent(bin) and hist.GetBinContent(bin):
-			     uncertainty=max(uncertainty,hist.GetBinContent(bin)/hist_down.GetBinContent(bin),hist_down.GetBinContent(bin)/hist.GetBinContent(bin))
-			   if hist_up.GetBinContent(bin) and hist.GetBinContent(bin):
-			     uncertainty=max(uncertainty,hist.GetBinContent(bin)/hist_up.GetBinContent(bin),hist_up.GetBinContent(bin)/hist.GetBinContent(bin))
-			   uncertainty = self.process_shape_uncertainties[curr_name][shape_name]*min(2,uncertainty-1)
-			   if options.verbose and uncertainty>1:
+			   uncertainty = self.process_shape_uncertainties[curr_name][shape_name]*abs(value)
+			   if options.verbose and uncertainty>max(hist_down.GetBinContent(bin)/hist.GetBinWidth(bin),hist.GetBinContent(bin)/hist.GetBinWidth(bin),hist_up.GetBinContent(bin)/hist.GetBinWidth(bin)):
 			       print "WARNING: There is a bin-by-bin uncertainty larger than 100%. Make sure there is no problem with the bin-by-bin uncertainties in the root file",histfile,"in",self.analysis,self.category,". Please check:",shape_name,"bin-down:",hist_down.GetBinContent(bin),"bin-center:",hist.GetBinContent(bin),"bin-up:",hist_up.GetBinContent(bin)
 		           if not process_name+str(bin) in uncertainties_set:
    		               uncertainties_set+=[process_name+str(bin)]
-                               out_line  += "hin->SetBinError(%(bin)i,hin->GetBinContent(%(bin)i)*%(uncertainty)f); \n" % {"bin":bin, "uncertainty":uncertainty}
+                               out_line  += "hin->SetBinError(%(bin)i,%(uncertainty)f); \n" % {"bin":bin, "uncertainty":uncertainty}
 			   elif uncertainty!=0:
-                               out_line  += "hin->SetBinError(%(bin)i,sqrt(pow(hin->GetBinError(%(bin)i),2)+pow(hin->GetBinContent(%(bin)i)*%(uncertainty)f,2))); \n" % {"bin":bin, "uncertainty":uncertainty}
+                               out_line  += "hin->SetBinError(%(bin)i,sqrt(pow(hin->GetBinError(%(bin)i),2)+pow(%(uncertainty)f,2))); \n" % {"bin":bin, "uncertainty":uncertainty}
                          output_file.write(out_line)
                          if options.verbose :
                              if out_line :
