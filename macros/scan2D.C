57ad05445b163144482ada1e5f1cc4323b4587a0
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-11-03 18:34:48 +0000

    update to what has been used to request green light for HCP -- not clear yet, why the PaveTaxt is not displayed???

--- macros/scan2D.C
+++ macros/scan2D.C
@@ -29,11 +29,11 @@ void scan2D(const char* path, std::string xval="r_ggH", int xbin=10, float xmin=
   }
 
   std::map<std::pair<std::string, bool>, const char*> axis_titles;
-  axis_titles[std::make_pair<std::string, bool>(std::string("r_bbH"), true )] = "#sigma(gg#rightarrowbb#phi)#timesBR (pb)";
+  axis_titles[std::make_pair<std::string, bool>(std::string("r_bbH"), true )] = "#bf{#sigma(gg#rightarrowbb#phi)#timesBR (pb)}";
   axis_titles[std::make_pair<std::string, bool>(std::string("r_ggH"), true )] = "#sigma(gg#rightarrow#phi)#timesBR (pb)";
   axis_titles[std::make_pair<std::string, bool>(std::string("r_ggH"), false)] = "#sigma(gg#rightarrowH)/#sigma(SM)";
-  axis_titles[std::make_pair<std::string, bool>(std::string("r_qqH"), false)] = "#sigma(qq#rightarrowqqH)/#sigma(SM)";
-  axis_titles[std::make_pair<std::string, bool>(std::string("CF"   ), false)] = "c_{F}";
+  axis_titles[std::make_pair<std::string, bool>(std::string("r_qqH"), false)] = "#bf{#sigma(qq#rightarrowqqH)/#sigma(SM)}";
+  axis_titles[std::make_pair<std::string, bool>(std::string("CF"   ), false)] = "#bf{c_{F}}";
   axis_titles[std::make_pair<std::string, bool>(std::string("CV"   ), false)] = "c_{V}";
 
   TCanvas* canv = new TCanvas("canv", "canv", 600, 600);
@@ -43,6 +43,16 @@ void scan2D(const char* path, std::string xval="r_ggH", int xbin=10, float xmin=
   canv->SetGridy();
   canv->SetRightMargin(0.14);
 
+  TPaveText * label = new TPaveText(0.50, 0.80, 1.0, 0.90, "NDC");
+  label->SetBorderSize(   0 );
+  label->SetFillStyle(    0 );
+  label->SetTextAlign(   12 );
+  label->SetTextSize ( 0.05 );
+  label->SetTextColor(    1 );
+  label->SetTextFont (   62 );
+  label->AddText("m_{H}=125 GeV");
+  label->Draw();
+
   scan->SetXTitle(axis_titles[std::pair<std::string, bool>(xval, mssm)]);
   scan->GetXaxis()->SetLabelFont(62);
   scan->GetXaxis()->SetTitleFont(62);
@@ -51,18 +61,19 @@ void scan2D(const char* path, std::string xval="r_ggH", int xbin=10, float xmin=
   scan->SetYTitle(axis_titles[std::pair<std::string, bool>(yval, mssm)]);
   scan->GetYaxis()->SetLabelFont(62);
   scan->GetYaxis()->SetTitleSize(0.05);
-  scan->GetYaxis()->SetTitleOffset(1.12);
+  scan->GetYaxis()->SetTitleOffset(1.4);
   scan->GetYaxis()->SetLabelSize(0.05);
   scan->SetZTitle("#bf{#Delta NLL}");
   scan->GetZaxis()->SetLabelFont(62);
   scan->GetZaxis()->SetTitleSize(0.04);
   scan->GetZaxis()->SetTitleOffset(1.03);
-  scan->GetZaxis()->SetLabelSize(0.05);
+  scan->GetZaxis()->SetLabelSize(0.04);
 
+  scan->SetMinimum(0.);
   //scan->SetMaximum(2);
   scan->Draw("colz");
 
-  canv->Print("scan.png");
-  canv->Print("scan.eps");
-  canv->Print("scan.pdf");
+  canv->Print("scan-ggH-qqH-125.png");
+  canv->Print("scan-ggH-qqH-125.eps");
+  canv->Print("scan-ggH-qqH-125.pdf");
 }
