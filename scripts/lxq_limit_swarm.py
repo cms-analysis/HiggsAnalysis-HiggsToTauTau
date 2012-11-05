fa3a3a0bbc822ca809bc69e74820f826ca5bbce7
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2012-11-05 09:34:13 +0000

    like the lxb_<..> just for NAF batch

---
+++ scripts/lxq_limit_swarm.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser, OptionGroup
+
+## set up the option parser
+parser = OptionParser(usage="usage: %prog [options] ARGS",
+                      description="Script send all limit calculations of in a structure as created by the setup-htt.py script to NAF batch. A use example is e.g. lxq_limit_swarm.py MY-LIMIT/*, where the folder MY-LIMIT containes subfolders of type cmb, em et mt mm tt, ... which in themselves contain the masses directories. The script will split these jobs into single limit calculations per mass point in each subfolder.")
+parser.add_option("-q", "--qsub", dest="qsub", default="-j y -o /dev/null", type="string", help="Arguments for qsub. [Default: \"-j y -o /dev/null\"]")
+parser.add_option("-m", "--method", dest="method", default="--asymptotic", type="string", help="limit calculation method. [Default: \"--asymptotic\"]")
+parser.add_option("--options", dest="options", default="", type="string", help="Options that should be passed on to combine. [Default: \"\"]")
+## check number of arguments; in case print usage
+(options, args) = parser.parse_args()
+
+import os
+
+for dir in args:
+    ana = dir[:dir.rfind('/')]
+    limit = dir[len(ana)+1:]
+    jobname = ana[ana.rfind('/')+1:]+'-'+limit
+    ## create submission scripts
+    os.system("lxq_limit.py {JOBNAME} \"-q {QSUB}\" \"{DIR}/*\" {METHOD} {OPTS}".format(
+        JOBNAME=jobname,
+        DIR=dir,
+        QSUB=options.qsub,
+        METHOD = options.method,
+        OPTS = options.options
+        ))
+    ## execute
+    os.system("./{JOBNAME}_submit.sh".format(JOBNAME=jobname))
+    ## shelve
+    os.system("mv {JOBNAME}_submit.sh {JOBNAME}".format(JOBNAME=jobname))
