d24d8ef690a4d94d479322ce878e5ed78b7d819f
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2012-11-19 15:26:55 +0000

    build in new parameter

--- python/layouts/mssm_xsec_layout.py
+++ python/layouts/mssm_xsec_layout.py
@@ -2,14 +2,16 @@ import FWCore.ParameterSet.Config as cms
 
 layout = cms.PSet(
     ## dataset
-    dataset = cms.string("4.6 fb^{-1}  #sqrt{s}=7 TeV"),
-    #dataset = cms.string("Preliminary 1.6 fb^{-1}  #sqrt{s}=7 TeV"),
+    #dataset = cms.string("12.2 fb^{-1}  #sqrt{s}=8 TeV"),
+    dataset = cms.string("4.9 fb^{-1}  #sqrt{s}=7 TeV"),
     ## x-axis title
     xaxis = cms.string("m_{A} [GeV]"),
     ## x-axis title
-    yaxis = cms.string("#sigma(#phi#rightarrow#tau#tau)_{95% CL} [pb]"),
+    yaxis = cms.string("#sigma(ggH/bbH#rightarrow#phi#rightarrow#tau#tau)_{95% CL} [pb]"),
     ## is this mssm?
     mssm = cms.bool(True),
+    ## center of mass energy (7, 8, 78 for combined)
+    ecms = cms.int32(7),
     ## print to png
     png  = cms.bool(True),
     ## print to pdf
@@ -19,9 +21,9 @@ layout = cms.PSet(
     ## print to root
     root = cms.bool(True),
     ## min for plotting
-    min = cms.double(0.05),
+    min = cms.double(0.0005),
     ## max for plotting
-    max = cms.double(500.),
+    max = cms.double(200.),
     ## min for plotting
     log = cms.int32(1),
     ## define verbosity level
@@ -44,5 +46,10 @@ layout = cms.PSet(
    ,400.
    ,450.
    ,500.
+   ,600.
+   ,700.
+   ,800.
+   ,900.
+   ,1000.
   ),
 )
