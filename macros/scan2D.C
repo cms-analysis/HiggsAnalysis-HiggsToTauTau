c4c76b62d852b0b6c843c15f253db8a4c6c897a5
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2012-11-26 11:53:18 +0000

    now between two different kind of plot styles could be chosen - 2d ggh vs bbh with likelihood scan (lxb/q-multidim-fit.py) with contours

--- macros/scan2D.C
+++ macros/scan2D.C
@@ -10,7 +10,7 @@
 
 #include "HiggsAnalysis/HiggsToTauTau/interface/HttStyles.h"
 
-void scan2D(const char* path, std::string xval="r_ggH", int xbin=10, float xmin=0., float xmax=20., std::string yval="r_bbH", int ybin=10, float ymin=0., float ymax=20., bool mssm=true, bool log=true)
+void scan2D(const char* path, std::string xval="r_ggH", int xbin=10, float xmin=0., float xmax=20., std::string yval="r_bbH", int ybin=10, float ymin=0., float ymax=20., bool mssm=true, bool log=true, bool temp=true)
 {
   SetStyle();
   TFile* file_ = TFile::Open(path);
@@ -27,12 +27,23 @@ void scan2D(const char* path, std::string xval="r_ggH", int xbin=10, float xmin=
     limit->GetEvent(i);
     if(scan->GetBinContent(scan->FindBin(x,y))==0){scan->Fill(x, y, nll);}
   }
-  float best_fit, best_fit_help, x_save, y_save;
+  float best_fit=50, best_fit_help, x_save, y_save;
   for(int i=0; i<nevent; ++i){
     limit->GetEvent(i);
     best_fit_help=scan->GetBinContent(scan->FindBin(x,y));
-    if (best_fit>=best_fit_help) {best_fit=best_fit_help; x_save=x; y_save=y;}
+    //std::cout <<scan->GetBinContent(scan->FindBin(x,y)) <<std::endl;
+    if (best_fit>=best_fit_help) {best_fit=best_fit_help; x_save=x; y_save=y;
+     //  std::cout << "best fit point: " << std::endl;
+//       std::cout << "r_ggH: " << x_save << std::endl; 
+//       std::cout << "r_bbH: " << y_save << std::endl; 
+//       std::cout << "nll: " << best_fit << std::endl; 
+//       std::cout << std::endl;
     }
+  }  
+  std::cout << "best fit point: " << std::endl;
+  std::cout << "r_ggH: " << x_save << std::endl; 
+  std::cout << "r_bbH: " << y_save << std::endl; 
+  std::cout << "nll: " << best_fit << std::endl; 
 
   std::map<std::pair<std::string, bool>, const char*> axis_titles;
   axis_titles[std::make_pair<std::string, bool>(std::string("r_bbH"), true )] = "#bf{#sigma(gg#rightarrowbb#phi)#timesBR (pb)}";
@@ -110,10 +121,12 @@ void scan2D(const char* path, std::string xval="r_ggH", int xbin=10, float xmin=
 //   label2->Draw();
 
   TH2F* scan2 = new TH2F("scan2", "", xbin, xmin, xmax, ybin, ymin, ymax);
-  int nevent2 = limit->GetEntries();
-  for(int i=0; i<nevent2; ++i){
-    limit->GetEvent(i);
-    if(scan2->GetBinContent(scan2->FindBin(x,y))==0){scan2->Fill(x, y, nll);}
+  if(temp){
+    int nevent2 = limit->GetEntries();
+    for(int i=0; i<nevent2; ++i){
+      limit->GetEvent(i);
+      if(scan2->GetBinContent(scan2->FindBin(x,y))==0){scan2->Fill(x, y, nll);}
+    }
   }
   scan2->SetXTitle(axis_titles[std::pair<std::string, bool>(xval, mssm)]);
   scan2->GetXaxis()->SetLabelFont(62);
@@ -133,55 +146,96 @@ void scan2D(const char* path, std::string xval="r_ggH", int xbin=10, float xmin=
   scan2->SetMinimum(0.);
   scan2->Draw("colz");
 
+  TLegend* leg = new TLegend(0.42, 0.70, 0.80, 0.90);
+  leg->SetBorderSize( 0 );
+  leg->SetFillStyle ( 0 );
+  leg->SetFillColor (kWhite);
+
+  if(temp){
+  //get and draw contours 
+  //std::cout << "conts " << conts->GetEntries() << std::endl;
+    for (int ic=0; ic<conts->GetEntries(); ic++) {   
+      TList* graphlist = (TList*)conts->At(ic);  
+      //std::cout << "graphlist " << graphlist->GetEntries() << std::endl;
+      for (int ig=0; ig<graphlist->GetEntries(); ig++) {
+	TGraph* gr = (TGraph*)graphlist->At(ig);
+	if (ic==0) {
+	  //std::cout << "ic " <<  ic << "  ig " << ig << std::endl;
+	  gr->SetLineColor(kBlack);
+	  gr->SetLineStyle(1);
+	  if( ig==graphlist->GetEntries()-1 ) leg->AddEntry(gr, "68% CL contour"  ,  "L" );
+	}
+	if (ic==1) {
+	  //std::cout << "ic " <<  ic << "  ig " << ig << std::endl;
+	  gr->SetLineColor(kBlack);
+	  gr->SetLineStyle(2);
+	  if( ig==graphlist->GetEntries()-1 ) leg->AddEntry(gr, "95% CL contour"  ,  "L" );
+	}
+	gr->SetLineWidth(3);
+	gr->Draw("contsame");
+      }
+    }
+  }
+  else{  
+    TList* graphlist1 = (TList*)conts->At(1);  
+    TGraph* gr1 = (TGraph*)graphlist1->At(0);
+    int m=gr1->GetN(); //get ploted array dimention
+    double bx[m];
+    double by[m];
+    //std::cout<<m<<std::endl;
+    for(Int_t i=0; i<m; i++) {
+      gr1->GetPoint(i,bx[i],by[i]);
+      //cout<<i<<"th element of X array: "<<bx[i]<<endl;
+      //cout<<i<<"th element of Y array: "<<by[i]<<endl;
+    }
+    TGraph *newgr1 = new TGraph();
+    newgr1->SetPoint(0,0,0);
+    for(int i=0; i<m; i++){
+      newgr1->SetPoint(i+1, bx[i], by[i]);
+    }
+    gr1->SetLineStyle(2);
+    gr1->SetLineWidth(3);
+    gr1->SetLineColor(kBlack);
+    newgr1->SetFillColor(kCyan);
+    newgr1->Draw("cfsame");
+    gr1->Draw("contsame");
+    leg->AddEntry(gr1, "95% CL contour"  ,  "L" );
+    
+    
+    TList* graphlist0 = (TList*)conts->At(0);
+    TGraph* gr0 = (TGraph*)graphlist0->At(0);
+    int n=gr0->GetN(); //get ploted array dimention
+    double ax[n];
+    double ay[n];
+    //std::cout<<n<<std::endl;
+    for(Int_t i=0; i<n; i++) {
+      gr0->GetPoint(i,ax[i],ay[i]);
+      //cout<<i<<"th element of X array: "<<ax[i]<<endl;
+      //cout<<i<<"th element of Y array: "<<ay[i]<<endl;
+    }
+    TGraph *newgr0 = new TGraph();
+    newgr0->SetPoint(0,0,0);
+    for(int i=0; i<n; i++){
+      newgr0->SetPoint(i+1, ax[i], ay[i]);
+    }
+    gr0->SetLineStyle(1);
+    gr0->SetLineWidth(3);
+    gr0->SetLineColor(kBlack);
+    newgr0->SetFillColor(kBlue);
+    newgr0->Draw("cfsame");
+    gr0->Draw("contsame");
+    leg->AddEntry(gr0, "68% CL contour"  ,  "L" );
+  }
+
   TGraph *bestfit = new TGraph();
   bestfit->SetPoint(0, x_save, y_save);
   bestfit->SetMarkerStyle(34);
   bestfit->SetMarkerSize(3.0);
   bestfit->SetMarkerColor(kBlack);
   bestfit->Draw("Psame");
-
-  TLegend* leg = new TLegend(0.42, 0.70, 0.80, 0.90);
-  leg->SetBorderSize( 0 );
-  leg->SetFillStyle ( 0 );
-  leg->SetFillColor (kWhite);
-
   leg->AddEntry(bestfit, "best fit point"  ,  "P" );
 
-  // get and draw contours 
-  //std::cout << "conts " << conts->GetEntries() << std::endl;
-  for (int ic=0; ic<conts->GetEntries(); ic++) {   
-    TList* graphlist = (TList*)conts->At(ic);  
-    //std::cout << "graphlist " << graphlist->GetEntries() << std::endl;
-    for (int ig=0; ig<graphlist->GetEntries(); ig++) {
-      TGraph* gr = (TGraph*)graphlist->At(ig);
-      if (ic==0) {
-	//std::cout << "ic " <<  ic << "  ig " << ig << std::endl;
-	gr->SetLineColor(kBlack);
-	//gr->SetFillColor(kCyan);
-	//gr->SetLineWidth(-12002);
-	//gr->SetFillStyle(3004);
-	gr->SetLineStyle(1);
-	if( ig==graphlist->GetEntries()-1 ) leg->AddEntry(gr, "68% CL contour"  ,  "L" );
-      }
-      if (ic==1) {
-	//std::cout << "ic " <<  ic << "  ig " << ig << std::endl;
-	gr->SetLineColor(kBlack);
-	//gr->SetFillColor(kBlue);
-	//gr->SetLineWidth(-12002);
-        //gr->SetFillStyle(3004);  
-	gr->SetLineStyle(2);
-	if( ig==graphlist->GetEntries()-1 ) leg->AddEntry(gr, "95% CL contour"  ,  "L" );
-      }
-      gr->SetLineWidth(3);
-      gr->Draw("contsame");
-    }
-  }
-  
   leg->Draw("same");
-
-
-
-  //scan->Draw("colz");
 
   canv2->Print("scan-ggH-qqH-125.png");
   canv2->Print("scan-ggH-qqH-125.eps");
