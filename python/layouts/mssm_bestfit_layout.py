d6dbf5dd4b6e7b7bfe7be972de8ae30ab455306e
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2012-11-28 13:42:52 +0000

    editing

--- python/layouts/mssm_bestfit_layout.py
+++ python/layouts/mssm_bestfit_layout.py
@@ -2,14 +2,16 @@ import FWCore.ParameterSet.Config as cms
 
 layout = cms.PSet(
     ## dataset
-    dataset = cms.string("Projection #Phi #rightarrow #tau #tau, 17 fb^{-1}  #sqrt{s}=7+8 TeV"),
+    dataset = cms.string("Preliminary #Phi #rightarrow #tau #tau, 17 fb^{-1}  #sqrt{s}=7+8 TeV"),
     #dataset = cms.string("Preliminary 1.6 fb^{-1}  #sqrt{s}=7 TeV"),
     ## x-axis title
     xaxis = cms.string("m_{A} [GeV]"),
     ## x-axis title
-    yaxis = cms.string("#sigma(#phi#rightarrow#tau#tau) [pb]"),
+    yaxis = cms.string("#sigma(pp#rightarrow#phi#rightarrow#tau#tau) [pb]"),
     ## is this mssm?
     mssm = cms.bool(True),
+    ## center of mass energy (7, 8, 78 for combination)
+    ecms = cms.int32(78),
     ## print to png
     png  = cms.bool(True),
     ## print to pdf
@@ -25,7 +27,7 @@ layout = cms.PSet(
     ## min for plotting
     log = cms.int32(0),
     ## process of interest - r_ggH or r_bbH
-    POI = cms.string("r_ggH"),
+    POI = cms.string("r_bbH"),
     ## define verbosity level
     verbosity   = cms.uint32(2),
     ## Set true if we want to skip observed limits
@@ -34,8 +36,8 @@ layout = cms.PSet(
     outputLabel = cms.string("mssm"), 
     ## define masspoints for limit plot
     masspoints = cms.vdouble(
-    #90.
-    100.
+    90.
+   ,100.
    ,120.
    ,130.
    ,140.
@@ -48,5 +50,10 @@ layout = cms.PSet(
    ,400.
    ,450.
    ,500.
+   ,600.
+   ,700.
+   ,800.
+   ,900.
+   ,1000.
   ),
 )
