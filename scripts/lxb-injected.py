91d77524277ea587ed86ef027641821ff058844c
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-12-21 11:01:49 +0000

    lxq absorbed into lxb as option

--- scripts/lxb-injected.py
+++ scripts/lxb-injected.py
@@ -3,13 +3,22 @@ from optparse import OptionParser, OptionGroup
 ## set up the option parser
 parser = OptionParser(usage="usage: %prog [options] ARGS",
                       description="Script to set up a set of scripts for statistical signal injection. The source directory for individual signal injections can be given by the option --inputs. This directory will be cloned in N subdirectories. N can be changed by --njob. in each subdirectory a signal is injected statistically. Afterwards the limit calculation is run for each mass that is found in the subdirectory. ARGS corresponds to the masses which are to be analysed.")
-parser.add_option("-n", "--name", dest="name", default="TEST", type="string", help="Name of the batch submission scripts. [Default: \"TEST\"]")
-parser.add_option("-i", "--input", dest="input", default="TEST/INJECT", type="string", help="Name of the input directory that should be used as starting point for signal injection. [Default: \"TEST/INJECT\"]")
-parser.add_option("--bsub", dest="bsub", default="-q 1nd", type="string", help="Submission arguments for the batch queue. [Default: \"-q 1nd\"]")
-
-parser.add_option("--njob", dest="njob", default="100", type="string", help="Number of toys for which to inject signal. (One toy per job). [Default: \"100\"]")
-parser.add_option("--mass-points-per-job", dest="per_job", type="int", default=10, help="Maximum number of mass points to run per batch job. If more mass points given as arguments the number of jobs will be increased. NOTE: this will be true for each toy. This will increase the number of jobs quadratically. [Default: \"10\"]")
-parser.add_option("--options", dest="opts", default="--observedOnly", type="string", help="Options for limit calculation with limit.py. [Default: \"--observedOnly\"]")
+parser.add_option("--name", dest="name", default="TEST", type="string",
+                  help="Name of the batch submission scripts. [Default: \"TEST\"]")
+parser.add_option("--input", dest="input", default="TEST/INJECT", type="string",
+                  help="Name of the input directory that should be used as starting point for signal injection. [Default: \"TEST/INJECT\"]")
+parser.add_option("--limit-options", dest="opts", default="--observedOnly", type="string",
+                  help="Options for limit calculation with limit.py. [Default: \"--observedOnly\"]")
+parser.add_option("--batch-options", dest="bsub", default="-q 1nd", type="string",
+                  help="Submission arguments for the batch queue. [Default: \"-q 1nd\"]")
+parser.add_option("--toys", dest="njob", default="100", type="string",
+                  help="Number of toys for which to inject signal. (One toy per job). [Default: \"100\"]")
+parser.add_option("--mass-points-per-job", dest="per_job", type="int", default=10,
+                  help="Maximum number of mass points to run per batch job. If more mass points given as arguments the number of jobs will be increased. NOTE: this will be true for each toy. This will increase the number of jobs quadratically. [Default: \"10\"]")
+parser.add_option("--lxq", dest="lxq", default=False, action="store_true",
+                  help="Specify this option when running on lxq instead of lxb. [Default: False]")
+parser.add_option("--condor", dest="condor", default=False, action="store_true",
+                  help="Specify this option when running on condor instead of lxb. [Default: False]")
 ## check number of arguments; in case print usage
 (options, args) = parser.parse_args()
 
@@ -30,7 +39,8 @@ opts     = options.opts
 
 random.seed()
 
-script_template = '''#!/usr/bin/env python
+script_template = '''
+#!/usr/bin/env python
 
 import os
 
@@ -53,6 +63,14 @@ for m in masses :
       os.system("limit.py --asymptotic {OPTS} {TMPDIR}/{USER}/{DIR}_{JOBID}/%s" % m)
       os.system("cp -v {TMPDIR}/{USER}/{DIR}_{JOBID}/%s/higgsCombine-obs.Asymptotic.mH%s.root {PWD}/{PATH}/{DIR}/%s/higgsCombine-obs.Asymptotic.mH%s-{JOBID}.root" % (m, m, m, m))
 os.system("rm -r {TMPDIR}/{USER}/{DIR}_{JOBID}")
+'''
+
+lxq_fragment = '''
+#!/usr/bin/bash
+#$ -l h_cpu=4:00:00
+export SCRAM_ARCH=$scram_arch
+ini cmssw
+ini autoproxy
 '''
 
 condor_sub_template = '''
@@ -94,9 +112,12 @@ os.system("mkdir %s" % name)
 submit_name = '%s_submit.sh' % name
 with open(submit_name, 'w') as submit_script:
     tmpdir = '/tmp'
-    if options.bsub == "condor":
+    if options.condor :
         tmpdir='${_CONDOR_SCRATCH_DIR}'
         submit_script.write(condor_sub_template)
+    if options.lxq :
+        submit_script.write('export scram_arch=$SCRAM_ARCH\n')
+        submit_script.write('export cmssw_base=$CMSSW_BASE\n')
     for idx in range(int(njob)):
         rnd = random.randint(1, 999999)
         log.info("Generating script for limit.py with injected signal for toy %g - %i subjobs will be created", idx, len(mass_groups))
@@ -119,17 +140,22 @@ with open(submit_name, 'w') as submit_script:
                 sh_file.write("#!/bin/bash\n")
                 sh_file.write("cd {PWD}; eval `scram runtime -sh`\n".format(PWD=os.getcwd()))
                 sh_file.write("python %s/%s\n" % (os.getcwd(), script_file_name))
-
+            if options.lxq :
+                sh_file = sh_file.replace('#!/bin/bash\n', lxq_fragment)
             os.system('chmod a+x %s' % script_file_name.replace('.py', '.sh'))
             bsubargs = options.bsub
-            if bsubargs == "condor":
+            if options.condor :
                 submit_script.write("\n")
                 submit_script.write("executable = %s/%s\n" % (os.getcwd(), script_file_name.replace('.py', '.sh')))
                 submit_script.write("output = %s/%s\n" % (os.getcwd(), script_file_name.replace('.py', '.stdout')))
                 submit_script.write("error = %s/%s\n" % (os.getcwd(), script_file_name.replace('.py', '.stderr')))
                 submit_script.write("queue\n")
+            elif options.lxq :
+                submit_script.write('qsub -l site=hh -l h_vmem=4000M %s -v scram_arch -v cmssw_base %s/%s\n'
+                                    % (bsubargs, os.getcwd(), script_file_name.replace('.py', '.sh')))
             else:
-                submit_script.write('bsub %s %s/%s\n' % (bsubargs, os.getcwd(), script_file_name.replace('.py', '.sh')))
+                submit_script.write('bsub {QUEUE} -oo /tmp/{USER}/%J.log {PATH}/{FILE}\n'.format(
+                    QUEUE=bsubargs, USER=os.environ['USER'], PATH=os.getcwd(), FILE=script_file_name.replace('.py', '.sh')))
 ## change mode
 os.system('chmod a+x %s' % submit_name)
 ## execute 
