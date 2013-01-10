28c66cc9baab06f0bb959f43ebf9840e552a2e24
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2013-01-10 17:06:42 +0000

    same logic for submission as for submit.py --asymptotic and --multidim-fit, also fixed log output

--- scripts/lxb-xsec2tanb.py
+++ scripts/lxb-xsec2tanb.py
@@ -2,9 +2,9 @@
 from optparse import OptionParser, OptionGroup
 
 ## set up the option parser
-parser = OptionParser(usage="usage: %prog [options] ARG1 ARG2 ARG3 ...", description="This is a script to submit a set of jobs to set up the datacard structure for direct mA-tanb limit calculation to lxb (lxq). ARGs corresponds to the mass directories or to the parent directories that should contain the masses directories.")
+parser = OptionParser(usage="usage: %prog [options] ARGs", description="This is a script to submit a set of jobs to set up the datacard structure for direct mA-tanb limit calculation to lxb (lxq). ARGs corresponds to the mass directories or to the parent directories.")
 parser.add_option("--name", dest="name", default="submit", type="string",
-                  help="Set the name of the submission. All scripts concerned with the submission will be located in a directory with that name in your working directory. [Default: \"xsec2tanb\"]")
+                  help="Set the name of the submission. All scripts concerned with the submission will be located in a directory with that name in your working directory. [Default: \"submit\"]")
 parser.add_option("--lxq", dest="lxq", default=False, action="store_true",
                   help="Specify this option when running on lxq instead of lxb. [Default: False]")
 
@@ -22,11 +22,14 @@ import logging
 log = logging.getLogger("xsec2tanb")
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
+from HiggsAnalysis.HiggsToTauTau.utils import contained
 from HiggsAnalysis.HiggsToTauTau.utils import is_number
 from HiggsAnalysis.HiggsToTauTau.utils import get_mass
 
 name = options.name
 dirglob = args
+## prepare log file directory
+os.system("mkdir -p log")
 
 ## tamplate for the submission script
 script_template = '''
@@ -52,55 +55,71 @@ ini autoproxy
 if options.lxq :
     script_template = script_template.replace('#!/usr/bin/bash', lxq_fragment)
     
-def submit(name, dirs) :
+def submit(name, key, masses) :
     '''
     prepare the submission script
     '''
     submit_name = '%s_submit.sh' % name
-    if not os.path.exists(name):
-        os.system("mkdir %s" % name)
     with open(submit_name, 'w') as submit_script:
         if options.lxq :
             submit_script.write('export scram_arch=$SCRAM_ARCH\n')
             submit_script.write('export cmssw_base=$CMSSW_BASE\n')
-        for dir in dirs :
-            for i, dir in enumerate(glob.glob(dir+'/*')):
-                if not is_number(dir[dir.rstrip('/').rfind('/')+1:]) :
-                    continue
-                ## do not submit jobs on old LSF output
-                if 'LSFJOB' in dir:
-                    continue
-                log.info(" Generating submision script for %s", dir)
-                script_file_name = '%s/%s_%i.sh' % (name, name, i)
-                with open(script_file_name, 'w') as script:
-                    script.write(script_template.format(
-                        working_dir = os.getcwd(),
-                        directory = dir
-                        ))
-                os.system('chmod a+x %s' % script_file_name)
-                if options.lxq :
-                    submit_script.write('qsub -l site=hh -j y -o /dev/null -l h_vmem=4000M -v scram_arch -v cmssw_base %s\n' % script_file_name)
-                else :
-                    os.system('touch /tmp/{USER}/{LOG}'.format(
-                        USER=os.environ['USER'], LOG=script_file_name[script_file_name.rfind('/')+1:].replace('.sh', '.log')))
-                    submit_script.write('bsub -q 8nh -oo /tmp/{USER}/{LOG} {PWD}/{FILE}\n'.format(
-                        USER=os.environ['USER'], LOG=script_file_name[script_file_name.rfind('/')+1:].replace('.sh', '.log'), PWD=os.getcwd(), FILE=script_file_name))
-                os.system('chmod a+x %s' % submit_name)
+        if not os.path.exists(name):
+            os.system("mkdir -p %s" % name)
+        ##print masses
+        for i, mass in enumerate(masses[key]):
+            dir=key+'/'+mass
+            if not is_number(dir[dir.rstrip('/').rfind('/')+1:]) :
+                continue
+            ## do not submit jobs on old LSF output
+            if 'LSFJOB' in dir:
+                continue
+            log.info(" Generating submision script for %s", dir)
+            script_file_name = '%s/%s_%i.sh' % (name, name, i)
+            with open(script_file_name, 'w') as script:
+                script.write(script_template.format(
+                    working_dir = os.getcwd(),
+                    directory = dir
+                    ))
+            os.system('chmod a+x %s' % script_file_name)
+            if options.lxq :
+                submit_script.write('qsub -l site=hh -j y -o /dev/null -l h_vmem=4000M -v scram_arch -v cmssw_base %s\n' % script_file_name)
+            else :
+                os.system('touch {PWD}/log/{LOG}'.format(
+                    PWD=os.getcwd(), LOG=script_file_name[script_file_name.rfind('/')+1:].replace('.sh', '.log')))
+                submit_script.write('bsub -q 8nh -oo {PWD}/log/{LOG} {PWD}/{FILE}\n'.format(
+                    LOG=script_file_name[script_file_name.rfind('/')+1:].replace('.sh', '.log'), PWD=os.getcwd(), FILE=script_file_name))
+    os.system('chmod a+x %s' % submit_name)
 
-dirs = []
-for dir in args :
-    ## chop off masses directory if present as this will be added automatically by the submission script
-    if is_number(get_mass(dir)) or get_mass(dir) == "common" :
-        dir = dir[:dir.rstrip('/').rfind('/')]
-    if not dir in dirs :
-        dirs.append(dir)
+def directories(args) :
+    ## prepare structure of parent directories
+    dirs = []
+    for dir in args :
+        if is_number(get_mass(dir)) or get_mass(dir) == "common" :
+            dir = dir[:dir.rstrip('/').rfind('/')]
+        if not dir in dirs :
+            dirs.append(dir)
+    ## prepare mapping of masses per parent directory
+    masses = {}
+    for dir in dirs :
+        buffer = []
+        for path in args :
+            if dir+'/' in path :
+                if is_number(get_mass(path)) :
+                    mass = get_mass(path)
+                    if not contained(mass, buffer) :
+                        buffer.append(mass)
+        masses[dir] = list(buffer)
+    return (dirs, masses)
 
-for dir in dirs:
+dirs = directories(args)[0]
+masses = directories(args)[1]
+for dir in dirs :
     ana = dir[:dir.rfind('/')]
     limit = dir[len(ana)+1:]
     jobname = ana[ana.rfind('/')+1:]+'-'+limit+'-'+options.name
     ## create submission scripts
-    submit(jobname, dirs)
+    submit(jobname, dir, masses)
     ## execute
     os.system("./{JOBNAME}_submit.sh".format(JOBNAME=jobname))
     ## shelve
