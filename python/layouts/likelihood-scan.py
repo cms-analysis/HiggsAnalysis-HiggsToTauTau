c7427561fe0ba9e9a99d42c4454540a89de2456e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2013-01-10 23:12:46 +0000

    first useful layouot for ilekihood scan (intermediate step)

---
+++ python/layouts/likelihood-scan.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+layout = cms.PSet(
+    ## dataset
+    dataset = cms.string(", Preliminary, H #rightarrow #tau #tau, L = 17 fb^{-1}"),
+    ## x-axis title
+    xaxis = cms.string("Signal Strength (in multiples of #sigma(SM))"),
+    ## x-axis title
+    yaxis = cms.string("NLL"),
+    ## min for plotting (for fine tuning)
+    min = cms.double(0.),
+    ## min for plotting
+    log = cms.int32(0),
+    ## print to png
+    png = cms.bool(True),
+    ## print to pdf
+    pdf = cms.bool(True),
+    ## print to txt
+    txt = cms.bool(True),
+    ## print to root
+    root = cms.bool(True),
+    ## define verbosity level
+    verbosity = cms.uint32(0),
+    ## define output label
+    outputLabel = cms.string("likelihood-scan"),
+    ## define masspoints for limit plot
+    masspoints = cms.vdouble(
+    110
+   ,115
+   ,120
+   ,125
+   ,130
+   ,135
+   ,140
+   ,145
+    ),
+)
