9e49ba701deb4cd87e432d07dc54075b931807ca
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2013-01-10 22:59:59 +0000

    can be used to plot LIMIT, INJECTED and BESTFIT now

--- src/plottingLimit.cxx
+++ src/plottingLimit.cxx
@@ -6,8 +6,12 @@
 #include "TGraphAsymmErrors.h"
 
 void
-plottingLimit(TCanvas& canv, TGraphAsymmErrors* innerBand, TGraphAsymmErrors* outerBand, TGraph* expected, TGraph* observed, TGraph* unit, std::string& xaxis, std::string& yaxis, double min=0., double max=5., bool log=false, bool injected=false, bool legendOnRight=false)
+plottingLimit(TCanvas& canv, TGraphAsymmErrors* innerBand, TGraphAsymmErrors* outerBand, TGraph* expected, TGraph* observed, TGraph* unit, std::string& xaxis, std::string& yaxis, double min=0., double max=5., bool log=false, std::string PLOT=std::string("LIMIT"), bool legendOnRight=false)
 {
+  // define PLOT type
+  bool injected = (PLOT == std::string("INJECTED"));
+  bool bestfit  = (PLOT == std::string("BESTFIT" ));
+
   // set up styles
   canv.cd();
   canv.SetGridx(1);
@@ -17,7 +21,7 @@ plottingLimit(TCanvas& canv, TGraphAsymmErrors* innerBand, TGraphAsymmErrors* ou
   }
 
   // draw a frame to define the range
-  TH1F* hr=canv.DrawFrame(outerBand->GetX()[0]-.01, min, outerBand->GetX()[outerBand->GetN()-1]+.01, max);
+  TH1F* hr=canv.DrawFrame(expected->GetX()[0]-.01, min, expected->GetX()[expected->GetN()-1]+.01, max);
   // format x axis
   hr->SetXTitle(xaxis.c_str());
   hr->GetXaxis()->SetLabelFont(62);
@@ -34,30 +38,55 @@ plottingLimit(TCanvas& canv, TGraphAsymmErrors* innerBand, TGraphAsymmErrors* ou
   hr->GetYaxis()->SetLabelSize(0.045);
   hr->SetNdivisions(505);
 
-  outerBand->SetLineWidth(1.);
-  outerBand->SetLineColor(kBlack);
-  outerBand->SetFillColor(injected ? kAzure-9 : kYellow);
-  outerBand->Draw("3");
-  
-  innerBand->SetLineWidth(1.);
-  innerBand->SetLineColor(kBlack);
-  innerBand->SetFillColor(injected ? kAzure-4 : kGreen);
-  innerBand->Draw("3same");
-  
-  expected->SetLineColor(kRed);
-  expected->SetLineWidth(3);
-  expected->SetLineStyle(1);
-  expected->Draw("L");
-
+  if(outerBand){
+    outerBand->SetLineWidth(1.);
+    outerBand->SetLineColor(kBlack);
+    outerBand->SetFillColor(injected ? kAzure-9 : kYellow);
+    outerBand->Draw("3");
+  }
+  if(innerBand){
+    if(bestfit){
+      innerBand->SetMarkerColor(kBlack);
+      innerBand->SetMarkerSize(1.0);
+      innerBand->SetMarkerStyle(20);
+      innerBand->SetLineWidth(3.);
+      innerBand->SetLineColor(kBlack);
+      innerBand->SetFillColor(kGreen);
+      innerBand->Draw("3same");
+    }
+    else{
+      innerBand->SetLineWidth(1.);
+      innerBand->SetLineColor(kBlack);
+      innerBand->SetFillColor(injected ? kAzure-4 : kGreen);
+      innerBand->Draw("3same");
+    }
+  }
+  if(expected){
+    if(bestfit){
+      expected->SetLineColor(kBlack);
+      expected->SetLineWidth(3);
+      expected->SetLineStyle(1);
+      expected->SetMarkerColor(kBlack);
+      expected->SetMarkerSize(1.0);
+      expected->SetMarkerStyle(20);
+      expected->Draw("PL");
+    }
+    else{
+      expected->SetLineColor(kRed);
+      expected->SetLineWidth(3);
+      expected->SetLineStyle(1);
+      expected->Draw("L");
+    }
+  }
   if(unit){
     unit->SetLineColor(kBlue);
     unit->SetLineWidth(3.);
     unit->Draw("Lsame");
   }
   if(observed){
-    observed->SetMarkerStyle(20);
-    observed->SetMarkerSize(1.0);
     observed->SetMarkerColor(kBlack);
+    observed->SetMarkerSize(1.0);
+    observed->SetMarkerStyle(20);
     observed->SetLineWidth(3.);
     observed->Draw("PLsame");
   }
@@ -71,14 +100,18 @@ plottingLimit(TCanvas& canv, TGraphAsymmErrors* innerBand, TGraphAsymmErrors* ou
     leg->AddEntry(observed, "observed",  "PL");
   }
   if(injected){
-    leg->AddEntry( expected , "signal injected"      ,  "L" );
-    leg->AddEntry( innerBand, "#pm 1#sigma injected" ,  "F" );
-    leg->AddEntry( outerBand, "#pm 2#sigma injected" ,  "F" );
+    leg->AddEntry( expected , "signal injected",  "L" );
+    if(innerBand){ leg->AddEntry( innerBand, "#pm 1#sigma injected",  "F" ); }
+    if(outerBand){ leg->AddEntry( outerBand, "#pm 2#sigma injected",  "F" ); }
+  }
+  else if(bestfit){
+    leg->AddEntry( expected , "#sigma/#sigma_{SM} (best fit)",  "PL" );
+    if(innerBand){ leg->AddEntry( innerBand, "68% CL" ,  "PLF" ); }
   }
   else{
-    leg->AddEntry( expected , "expected"             ,  "L" );
-    leg->AddEntry( innerBand, "#pm 1#sigma expected" ,  "F" );
-    leg->AddEntry( outerBand, "#pm 2#sigma expected" ,  "F" );
+    leg->AddEntry( expected , "expected",  "L" );
+    if(innerBand){ leg->AddEntry( innerBand, "#pm 1#sigma expected",  "F" ); }
+    if(outerBand){ leg->AddEntry( outerBand, "#pm 2#sigma expected",  "F" ); }
   }
   leg->Draw("same");
   //canv.RedrawAxis("g");
