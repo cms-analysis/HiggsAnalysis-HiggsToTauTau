c2cb24f14c56c0a871f416dc8904d1f68e0d0b13
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2013-01-11 19:48:16 +0000

    file formating

--- scripts/cvs2local.py
+++ scripts/cvs2local.py
@@ -3,34 +3,56 @@
 from optparse import OptionParser, OptionGroup
 
 ## set up the option parser
-parser = OptionParser(usage="usage: %prog [options] ARGS",
-                      description="Script to copy input files and datacards for limit and significance calculation from cvs to local or to the svn server that is used for the combination of all Higgs decay channels. If the output directory is does not already exist a new directory structure will be created (for local copies). ARGS corresponds to a list of integer values resembling the mass points for which you want to copy the datacards. Ranges can be indicated e.g. by: 110-145'. That only any x-th mass point should be taken into account can be indicated e.g. by: 110-145:5. The latter example will pick up the masses 110 115 120 130 135 140 145. Any combination of this syntax is possible.")
-parser.add_option("-i", "--in", dest="input", default="auxiliaries/datacards/", type="string", help="Name of the input directory from where to pick up the datacards. [Default: auxiliaries/datacards/]")
-parser.add_option("-o", "--out", dest="out", default="ichep2012", type="string", help="Name of the output directory to which the datacards should be copied. [Default: ichep2012]")
-parser.add_option("-p", "--periods", dest="periods", default="7TeV 8TeV", type="string", help="List of run periods for which the datacards are to be copied. [Default: \"7TeV 8TeV\"]")
-parser.add_option("-a", "--analysis", dest="analysis", default="sm", type="choice", help="Type of analysis (sm or mssm). Lower case is required. [Default: sm]", choices=["sm", "mssm"])
-parser.add_option("-c", "--channels", dest="channels", default="mm em mt et", type="string", help="List of channels, for whch the datacards should be copied. The list should be embraced by call-ons and separeted by whitespace or comma. Available channels are mm, em, mt, et, tt, vhtt, hmm, hbb. [Default: \"mm em mt et\"]")
-parser.add_option("-u", "--no-update", dest="no_update", default=False, action="store_true", help="If there are already root file in common, do not recopy them. This should be used by other tools only to speed up copy jobs. [Default: False]")
+parser = OptionParser(usage="usage: %prog [options] ARGs",
+                      description="This is  a script to copy input files and datacards for limit and significance calculation from cvs to local or to the svn server that is used for the combination of all Higgs decay channels. If the output directory does not already exist a new directory structure will be created (for local copies). ARGs corresponds to a list of integer values resembling the mass points for which you want to copy the datacards. Ranges can be indicated e.g. by: 110-145'. That only any x-th mass point should be taken into account can be indicated e.g. by: 110-145:5. The latter example will pick up the masses 110 115 120 130 135 140 145. Any combination of this syntax is possible.")
+parser.add_option("-i", "--in", dest="input", default="auxiliaries/datacards/", type="string",
+                  help="Name of the input directory from where to pick up the datacards. [Default: auxiliaries/datacards/]")
+parser.add_option("-o", "--out", dest="out", default="ichep2012", type="string",
+                  help="Name of the output directory to which the datacards should be copied. [Default: ichep2012]")
+parser.add_option("-p", "--periods", dest="periods", default="7TeV 8TeV", type="string",
+                  help="List of run periods for which the datacards are to be copied. [Default: \"7TeV 8TeV\"]")
+parser.add_option("-a", "--analysis", dest="analysis", default="sm", type="choice",
+                  help="Type of analysis (sm or mssm). Lower case is required. [Default: sm]", choices=["sm", "mssm"])
+parser.add_option("-c", "--channels", dest="channels", default="mm em mt et", type="string",
+                  help="List of channels, for which the datacards should be copied. The list should be embraced by call-ons and separeted by whitespace or comma. Available channels are mm, em, mt, et, tt, vhtt, hmm, hbb. [Default: \"mm em mt et\"]")
+parser.add_option("-u", "--no-update", dest="no_update", default=False, action="store_true",
+                  help="If there are already root files in common, do not recopy them. This should be used by other tools only to speed up copy jobs. [Default: False]")
 cats1 = OptionGroup(parser, "SM EVENT CATEGORIES", "Event categories to be picked up for the SM analysis.")
-cats1.add_option("--sm-categories-mm", dest="mm_sm_categories", default="0 1 2 3 5", type="string", help="List mm of event categories. [Default: \"0 1 2 3 5\"]")
-cats1.add_option("--sm-categories-em", dest="em_sm_categories", default="0 1 2 3 5", type="string", help="List em of event categories. [Default: \"0 1 2 3 5\"]")
-cats1.add_option("--sm-categories-mt", dest="mt_sm_categories", default="0 1 2 3 5", type="string", help="List mt of event categories. [Default: \"0 1 2 3 5\"]")
-cats1.add_option("--sm-categories-et", dest="et_sm_categories", default="0 1 2 3 5", type="string", help="List et of event categories. [Default: \"0 1 2 3 5\"]")
-cats1.add_option("--sm-categories-tt", dest="tt_sm_categories", default="0 1", type="string", help="List of tt event categories. [Default: \"0 1\"]")
-cats1.add_option("--sm-categories-vhtt", dest="vhtt_sm_categories", default="0 1", type="string", help="List of vhtt event categories. [Default: \"0 1\"]")
-cats1.add_option("--sm-categories-vhbb", dest="vhbb_sm_categories", default="0 1 2 3 4 5 6 7 8 9", type="string", help="List of tt event categories. [Default: \"0 1 2 3 4 5 6 7 8 9\"]")
+cats1.add_option("--sm-categories-mm", dest="mm_sm_categories", default="0 1 2 3 5", type="string",
+                 help="List mm of event categories. [Default: \"0 1 2 3 5\"]")
+cats1.add_option("--sm-categories-em", dest="em_sm_categories", default="0 1 2 3 5", type="string",
+                 help="List em of event categories. [Default: \"0 1 2 3 5\"]")
+cats1.add_option("--sm-categories-mt", dest="mt_sm_categories", default="0 1 2 3 5", type="string",
+                 help="List mt of event categories. [Default: \"0 1 2 3 5\"]")
+cats1.add_option("--sm-categories-et", dest="et_sm_categories", default="0 1 2 3 5", type="string",
+                 help="List et of event categories. [Default: \"0 1 2 3 5\"]")
+cats1.add_option("--sm-categories-tt", dest="tt_sm_categories", default="0 1", type="string",
+                 help="List of tt event categories. [Default: \"0 1\"]")
+cats1.add_option("--sm-categories-vhtt", dest="vhtt_sm_categories", default="0 1", type="string",
+                 help="List of vhtt event categories. [Default: \"0 1\"]")
+cats1.add_option("--sm-categories-vhbb", dest="vhbb_sm_categories", default="0 1 2 3 4 5 6 7 8 9", type="string",
+                 help="List of tt event categories. [Default: \"0 1 2 3 4 5 6 7 8 9\"]")
 parser.add_option_group(cats1)
 cats2 = OptionGroup(parser, "MSSM EVENT CATEGORIES", "Event categories to be used for the MSSM analysis.")
-cats2.add_option("--mssm-categories-mm", dest="mm_mssm_categories", default="8 9", type="string", help="List mm of event categories. [Default: \"0 1 2 3 6 7\"]")
-cats2.add_option("--mssm-categories-em", dest="em_mssm_categories", default="8 9", type="string", help="List em of event categories. [Default: \"0 1 2 3 6 7\"]")
-cats2.add_option("--mssm-categories-mt", dest="mt_mssm_categories", default="8 9", type="string", help="List mt of event categories. [Default: \"0 1 2 3 6 7\"]")
-cats2.add_option("--mssm-categories-et", dest="et_mssm_categories", default="8 9", type="string", help="List et of event categories. [Default: \"0 1 2 3 6 7\"]")
-cats2.add_option("--mssm-categories-tt", dest="tt_mssm_categories", default="0 1", type="string", help="List of tt event categories. [Default: \"0 1\"]")
-cats2.add_option("--mssm-categories-hmm", dest="hmm_mssm_categories", default="0 1", type="string", help="List of hmm event categories. [Default: \"0 1\"]")
-cats2.add_option("--mssm-categories-hbb", dest="hbb_mssm_categories", default="0 1 2 3 4 5 6", type="string", help="List of hbb event categories. [Default: \"0 1 2 3 4 5 6\"]")
+cats2.add_option("--mssm-categories-mm", dest="mm_mssm_categories", default="8 9", type="string",
+                 help="List mm of event categories. [Default: \"0 1 2 3 6 7\"]")
+cats2.add_option("--mssm-categories-em", dest="em_mssm_categories", default="8 9", type="string",
+                 help="List em of event categories. [Default: \"0 1 2 3 6 7\"]")
+cats2.add_option("--mssm-categories-mt", dest="mt_mssm_categories", default="8 9", type="string",
+                 help="List mt of event categories. [Default: \"0 1 2 3 6 7\"]")
+cats2.add_option("--mssm-categories-et", dest="et_mssm_categories", default="8 9", type="string",
+                 help="List et of event categories. [Default: \"0 1 2 3 6 7\"]")
+cats2.add_option("--mssm-categories-tt", dest="tt_mssm_categories", default="0 1", type="string",
+                 help="List of tt event categories. [Default: \"0 1\"]")
+cats2.add_option("--mssm-categories-hmm", dest="hmm_mssm_categories", default="0 1", type="string",
+                 help="List of hmm event categories. [Default: \"0 1\"]")
+cats2.add_option("--mssm-categories-hbb", dest="hbb_mssm_categories", default="0 1 2 3 4 5 6", type="string",
+                 help="List of hbb event categories. [Default: \"0 1 2 3 4 5 6\"]")
 parser.add_option_group(cats2)
-parser.add_option("-4", "--SM4", dest="sm4", default=False, action="store_true", help="Copy SM4 datacards (will add a prefix SM4_ to each file). [Default: False]")
-parser.add_option("-v", "--verbose", dest="verbose", default=False, action="store_true", help="Run in verbose mode. [Default: False]")
+parser.add_option("-4", "--SM4", dest="sm4", default=False, action="store_true",
+                  help="Copy SM4 datacards (will add a prefix SM4_ to each file). [Default: False]")
+parser.add_option("-v", "--verbose", dest="verbose", default=False, action="store_true",
+                  help="Run in verbose mode. [Default: False]")
 (options, args) = parser.parse_args()
 ## check number of arguments; in case print usage
 if len(args) < 1 :
