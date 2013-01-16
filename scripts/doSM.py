2069ddeb4812b9f63686038f7a44b32a336f0a80
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2013-01-16 10:42:15 +0000

    working version for comparison

--- scripts/doSM.py
+++ scripts/doSM.py
@@ -33,7 +33,7 @@ if not options.skip_cvs :
     for chn, dirs in aux.iteritems() :
         for dir in dirs :
             print "copy files for channel:", chn
-            os.system("cp {CMSSW_BASE}/src/auxiliaries/datacards/collected/{DIR}/htt_{CHN}*.root {CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/setup/{CHN}/".format(
+            os.system("cp {CMSSW_BASE}/src/auxiliaries/datacards/collected/{DIR}/*htt_{CHN}*.root {CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/setup/{CHN}/".format(
                 CMSSW_BASE=cmssw_base,
                 DIR=dir,
                 CHN=chn
@@ -101,7 +101,16 @@ if not options.skip_setup :
 ##     os.system("mv {DIR}/Moriond-2012D/et/htt_et.inputs-sm-8TeV-2012d.root htt_et.inputs-sm-8TeV.root"format(DIR=source))
 ##     os.system("mv {DIR}/Moriond-2012D/mt/htt_mt.inputs-sm-8TeV-2012d.root htt_mt.inputs-sm-8TeV.root"format(DIR=source))
 ##     os.system("mv {DIR}/Moriond-2012D/mm/htt_mm.inputs-sm-8TeV-2012d.root htt_mm.inputs-sm-8TeV.root"format(DIR=source))
-##     os.system("mv {DIR}/Moriond-2012D/tt/htt_tt.inputs-sm-8TeV-2012d.root htt_tt.inputs-sm-8TeV.root"format(DIR=source))
+##     os.system("mv {DIR}/Moriond-2012D/tt/htt_tt.inputs-sm-8TeV-2012d.root htt_tt.inputs-sm-8TeV.root"format(DIR=source)
+    ##setup andrew 2012D    
+    os.system("cp -r {SETUP} {DIR}/andrew-2012D".format(SETUP=setup, DIR=source))
+    os.system("mv {DIR}/andrew-2012D/em/DOnly-htt_em.inputs-sm-8TeV.root htt_em.inputs-sm-8TeV.root".format(SETUP=setup, DIR=source))
+    ##setup andrew HCP
+    os.system("cp -r {SETUP} {DIR}/andrew-HCP".format(SETUP=setup, DIR=source))
+    os.system("mv {DIR}/andrew-HCP/em/DOnly-htt_em.inputs-sm-8TeV.root htt_em.inputs-sm-8TeV.root".format(SETUP=setup, DIR=source))
+    ##setup andrew Moriond
+    os.system("cp -r {SETUP} {DIR}/andrew-Moriond".format(SETUP=setup, DIR=source))
+    os.system("mv {DIR}/andrew-Moriond/em/DOnly-htt_em.inputs-sm-8TeV.root htt_em.inputs-sm-8TeV.root".format(SETUP=setup, DIR=source))
 if not options.skip_datacards :
     ## setup datacards
     datacards = "{CMSSW_BASE}/src/aux".format(CMSSW_BASE=cmssw_base)
@@ -112,16 +121,16 @@ if not options.skip_datacards :
     os.system("mkdir {DIR}/sm".format(DIR=datacards))
     #    os.system("rm -r {DIR}/mssm".format(DIR=datacards))
     #os.system("mkdir {DIR}/mssm".format(DIR=datacards))
-    for ana in ['Moriond']:#, 'Moriond-2012d', 'Moriond-bin-by-bin', 'Moriond-mvis'] :
+    for ana in ['Moriond', 'andrew-2012D', 'andrew-HCP', 'andrew-Moriond']:#, 'Moriond-2012d', 'Moriond-bin-by-bin', 'Moriond-mvis'] :
         print "setup datacards for:", ana, "sm"
-        if '2012d' in ana :
+        if '2012D' in ana :
             os.system("setup-datacards.py -i setups/{ANA} -o {OUTPUT}/sm/{ANA} -a sm -c 'em et mt mm tt' -p 8TeV {MASSES}".format(
             ANA=ana,
             OUTPUT=datacards,
             MASSES=masses
             ))
-        else :
-            os.system("setup-datacards.py -i setups/{ANA} -o {OUTPUT}/sm/{ANA} -a sm -c 'em et mt mm tt' {MASSES}".format(
+        else :  #only 8tev for now
+            os.system("setup-datacards.py -i setups/{ANA} -o {OUTPUT}/sm/{ANA} -a sm -c 'em et mt mm tt' -p 8TeV {MASSES}".format(
                 ANA=ana,
                 OUTPUT=datacards,
                 MASSES=masses
@@ -132,12 +141,12 @@ if not options.skip_limits :
     if not os.path.exists(limits) :
         os.system("mkdir {DIR}".format(DIR=limits))
         os.system("mkdir {DIR}/sm".format(DIR=limits))
-    for ana in ['Moriond']:#, 'Moriond-2012d', 'Moriond-bin-by-bin', 'Moriond-mvis'] :
+    for ana in ['Moriond', 'andrew-2012D', 'andrew-HCP', 'andrew-Moriond']:#, 'Moriond-2012d', 'Moriond-bin-by-bin', 'Moriond-mvis'] :
         label = ""
         if 'mvis' in ana :
             label = "-l "+ana[ana.find('-')+1:]
-        print "setup limits structure for:", ana, "sm"
-        os.system("setup-htt.py -i aux/sm/{ANA} -o {OUTPUT}/sm/{ANA} -a sm -c 'em et mt mm tt' {LABEL} {MASSES}".format(
+        print "setup limits structure for:", ana, "sm" #only 8tev for now
+        os.system("setup-htt.py -i aux/sm/{ANA} -o {OUTPUT}/sm/{ANA} -a sm -c 'em et mt mm tt' -p 8TeV {LABEL} {MASSES}".format(
             ANA=ana,
             LABEL=label,
             OUTPUT=limits,
