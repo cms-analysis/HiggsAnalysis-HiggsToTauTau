8f01d845793a1f7703b4c0a23ae0fc98fc75bf6e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2013-02-07 15:41:35 +0000

    switching default et, mt from Wisconsin to Imperial

--- scripts/doSM.py
+++ scripts/doSM.py
@@ -20,9 +20,9 @@ parser.add_option("--inputs-mm", dest="inputs_mm", default="KIT", type="choice",
                   help="Input files for htt_mm analysis. [Default: \"KIT\"]")
 parser.add_option("--inputs-em", dest="inputs_em", default="MIT", type="choice", choices=['MIT', 'Imperial'],
                   help="Input files for htt_em analysis. [Default: \"MIT\"]")
-parser.add_option("--inputs-et", dest="inputs_et", default="Wisconsin", type="choice", choices=['Wisconsin', 'Imperial', 'CERN'],
+parser.add_option("--inputs-et", dest="inputs_et", default="Imperial", type="choice", choices=['Wisconsin', 'Imperial', 'CERN'],
                   help="Input files for htt_et analysis. [Default: \"Wisconsin\"]")
-parser.add_option("--inputs-mt", dest="inputs_mt", default="Wisconsin", type="choice", choices=['Wisconsin', 'Imperial', 'LLR', 'CERN', 'MIT'],
+parser.add_option("--inputs-mt", dest="inputs_mt", default="Imperial", type="choice", choices=['Wisconsin', 'Imperial', 'LLR', 'CERN', 'MIT'],
                   help="Input files for htt_mt analysis. [Default: \"Wisconsin\"]")
 parser.add_option("--inputs-tt", dest="inputs_tt", default="MIT", type="choice", choices=['CERN', 'MIT'],
                   help="Input files for htt_tt analysis. [Default: \"MIT\"]")
