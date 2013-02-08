e20e1db8fc1899ec7f508e5b568fa09f6b4a4340
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2013-02-08 12:33:50 +0000

    changed lumit to 24.3

--- python/layouts/max-likelihood_sm.py
+++ python/layouts/max-likelihood_sm.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 layout = cms.PSet(
     ## dataset
-    dataset = cms.string(" Preliminary, H #rightarrow #tau #tau, L = 17 fb^{-1}"),
+    dataset = cms.string(" Preliminary, H #rightarrow #tau #tau, L = 24.3 fb^{-1}"),
     ## x-axis title
     xaxis = cms.string("m_{H} [GeV]"),
     ## x-axis title
@@ -18,13 +18,13 @@ layout = cms.PSet(
     ## print to root
     root = cms.bool(True),
     ## min for plotting
-    min = cms.double(-3.),
+    min = cms.double(0.),
     ## max for plotting
     max = cms.double(4.),
     ## min for plotting
     log = cms.int32(0),
     ## define verbosity level
-    verbosity   = cms.uint32(0),
+    verbosity   = cms.uint32(1),
     ## define output label
     outputLabel = cms.string("bestfit"),
     ## define masspoints for limit plot
