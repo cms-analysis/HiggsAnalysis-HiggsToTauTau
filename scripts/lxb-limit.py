3c148170945f77c78cb6b07d57c391d360c276db
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2013-04-04 14:48:46 +0000

    Prevent crashes due to missing CMSSW CVMFS

--- scripts/lxb-limit.py
+++ scripts/lxb-limit.py
@@ -68,7 +68,7 @@ log = condor.log
 notification = never
 getenv = true
 ## make sure AFS is accessible and suppress the default FilesystemDomain requirements of Condor
-requirements = HasAFS_OSG && TARGET.FilesystemDomain =!= UNDEFINED && TARGET.UWCMS_CVMFS_Revision >= 0
+requirements = HasAFS_OSG && TARGET.FilesystemDomain =!= UNDEFINED && TARGET.UWCMS_CVMFS_Revision >= 0 && TARGET.CMS_CVMFS_Revision >= 0
 
 '''
 
