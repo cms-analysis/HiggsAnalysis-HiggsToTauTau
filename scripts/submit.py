49d0e81a1e99c114fb7c7c02eb2d0a977f036e36
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2013-05-02 14:55:41 +0000

    adding option for model independent tanb-ma limits (corresponding to tanb_grid.py changes)

--- scripts/submit.py
+++ scripts/submit.py
@@ -121,6 +121,8 @@ parser.add_option_group(ggroup)
 hgroup = OptionGroup(parser, "TANB+ OPTIONS", "These are the command line options that can be used to configure the submission of tanb+. This option is special in the way that it needs modifications of the directory structure before the limits can be run. Via the script submit.py this setup can only be run interactively using the commend option --setup. Once the directory structure has been set up the limit calculation can be run interactively or in batrch mode.")
 hgroup.add_option("--setup", dest="setup", default=False, action="store_true",
                   help="Use the script to setup the directory structure for direct mA-tanb limits interactively. If false the the script will assume that this has already been done and execute the limit calculation either in batch mode or interactive. [Default: False]")
+parser.add_option_group(hgroup)
+
 ##
 ## FULL CLs
 ##
