8eda5030e527f80720a80f508f42407131a1fefe
Rene Caspart <sha1-117a72c1500b8cb85925aa075b60779d6b80a751@cern.ch>
2013-06-10 07:51:53 +0000

    added tanb as parameter

--- python/MODEL_PARAMS.py
+++ python/MODEL_PARAMS.py
@@ -5,6 +5,7 @@ class MODEL_PARAMS:
         self.masses = {'A' : '120', 'H' : '130', 'h' : '118'}
         self.xsecs  = {'A' : '3'  , 'H' : '2'  , 'h' : '1'  }
         self.brs    = {'A' : '0.1', 'H' : '0.1', 'h' : '0.1'}
+        self.tanb = 15
     def _add_higgs(self,higgs):
         " Adding higgs to list_of_higgs"
         self.list_of_higgses.append(higgs)
