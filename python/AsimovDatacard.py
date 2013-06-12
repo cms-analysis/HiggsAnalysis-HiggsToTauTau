68312205c106a4e8f6d5cd8c5f78371aa9c38091
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2013-06-12 23:17:56 +0000

    added option to update the same file with data_obs_asomiv

--- python/AsimovDatacard.py
+++ python/AsimovDatacard.py
@@ -10,15 +10,17 @@ class AsimovDatacard(DatacardAdaptor) :
     Description:
 
     This class should take all datacards in a given directory and replace all data_obs histograms / entries by the asimov
-    dataset. All data_obs histograms are kept in a file next to the original input files but indicated by an additional
-    postfix _asimov. All datacardss in the given directory are modified accordingly to read the shapes for data from these
-    new input files. The class works for shape analyses and counting experiments. Depending on the configuration all signal
-    processes as given by the datacards can be added to the asimov dataset, multiplied by a scale factor, which is commonly
-    applied to all signal processes. If configured such a parameter can be given to indicate the mass of the signal that
-    should be added to the background processes. If a random seed>=0 is given the asimov dataset is randomized according
-    to a poinsson dirstribution.
+    dataset. There are tow options in introducing the new data_obs: either the it is saved in the the same root input file
+    with name data_obs_asimov, or it is saved with the same name in a different root input file, which is indicated by an
+    additional postfix _asimov. All datacardss in the given directory are modified accordingly. The class works for shape
+    analyses and counting experiments. Depending on the configuration all signal processes as given by the datacards can be
+    added to the asimov dataset, multiplied by a scale factor, which is commonly applied to all signal processes. If
+    configured such a parameter can be given to indicate the mass of the signal that should be added to the background
+    processes. In addition extra templates can be added into the data_obs, which are present in the root input file, but
+    not part of the processes indicated in the datacard. If a random seed>=0 is given the asimov dataset is randomized
+    according to a poinsson dirstribution.
     """
-    def __init__(self, parser_options, seed='-1', add_signal=True, mass='125', signal_scale='1.', extra_templates='') :
+    def __init__(self, parser_options, update_file=False, seed='-1', add_signal=True, mass='125', signal_scale='1.', extra_templates='') :
         ## random seed in case the asimov dataset should be randomized (-1 will indicate that no randomization should be applied) 
         self.seed = seed
         ## should be true if signal should be considered for the asimov dataset
@@ -29,6 +31,9 @@ class AsimovDatacard(DatacardAdaptor) :
         self.signal_scale = signal_scale
         ## in case any other templates should be added to the asimov dataset (e.g. SM signal to MSSM datacards)
         self.extra_templates = extra_templates
+        ## write the asomiv dataset with new histogram name into the same file (i.e. update the existing file) or write the
+        ## asimov dataset with the same histogram name in a new root file?
+        self.update_file = update_file
         ## initialize base class
         super(AsimovDatacard, self).__init__(parser_options)
     
@@ -79,10 +84,11 @@ class AsimovDatacard(DatacardAdaptor) :
     def asimov_shapes(self, dir) :
         """
         Parse all datacards in dir, collect signal and background processes and the paths to the input files and create
-        the asimov dataset including background processes and signal processes if configured such. Write new rootfiles 
+        the asimov dataset including background processes (and signal processes if configured such). Write new rootfiles 
         which contain only the new data_obs histograms. This is done for each bin and each process independently. In a
         final step the individual rootfiles containing the asimov datasets are hadd'ed into one additional file for each
-        inputfile with the extension _asimov. 
+        inputfile with the extension _asimov. If the option self.update_file is chosen the new data_obs histogram(s) will
+        be written as new histograms to the same file with an additional label _asimov.
         """
         ## counter for files with blinded data_obs 
         index = 0
@@ -99,9 +105,9 @@ class AsimovDatacard(DatacardAdaptor) :
             for bin in card.list_of_bins() :
                 for proc in card.list_of_procs() :
                     path = card.path_to_file(bin, proc)
-                    ## determine background list
+                    ## determine background list, add extra contributions to this list if configured such
                     bkg_list = self.list2string(card, bin, card.list_of_procs('b'))
-                    bkg_list = bkg_list + ',' + self.extra_templates.replace(' ','') if self.extra_templates!="" else bkg_list
+                    bkg_list = bkg_list+','+self.extra_templates.replace(' ','') if self.extra_templates!='' else bkg_list
                     ## determine signal list
                     sig_list = self.list2string(card, bin, card.list_of_procs('s')) if self.add_signal else ''
                     if not path == '' :
@@ -117,20 +123,22 @@ class AsimovDatacard(DatacardAdaptor) :
                                 DIR = bin,
                                 SEED = self.seed,
                                 SCALE = self.signal_scale,
-                                IDX = index,
+                                IDX = '' if self.update_file else index,
                                 DATA_OBS = self.list2string(card, bin, ['data_obs']) 
                                 ))
                             index += 1
             file.close()
-        ## combine all individual files into a single file for each used input rootfile
-        for file in processed_files :
-            os.system('hadd {TARGET} {SOURCE}'.format(
-                TARGET=dir+'/'+file.replace('.root', '.root_asimov'),
-                SOURCE=dir+'/'+file.replace('.root', '_*.root')
-                ))
-            os.system('rm {SOURCE}'.format(
-                SOURCE=dir+'/'+file.replace('.root', '_*.root')
-                ))
+        ## combine all individual files into a single file for each used input rootfile. This is only necessary if the
+        ## data_obs histogrtqams have been written in separate files.
+        if not self.update_file :
+            for file in processed_files :
+                os.system('hadd {TARGET} {SOURCE}'.format(
+                    TARGET=dir+'/'+file.replace('.root', '.root_asimov'),
+                    SOURCE=dir+'/'+file.replace('.root', '_*.root')
+                    ))
+                os.system('rm {SOURCE}'.format(
+                    SOURCE=dir+'/'+file.replace('.root', '_*.root')
+                    ))
 
     def make_asimov_datacards(self, dir) :
         """
@@ -147,7 +155,12 @@ class AsimovDatacard(DatacardAdaptor) :
         for card in os.listdir(dir) :
             if not card.endswith('.txt') :
                 continue
-            self.adapt_shapes_lines(dir+'/'+card, 'data_obs', '_asimov')
+            if self.update_file :
+                ## histogram name gets label _asimov, filename remains as is
+                self.adapt_shapes_lines(dir+'/'+card, 'data_obs', '_asimov', '')
+            else:
+                ## filename gets label _asimov, histogram name remains as is
+                self.adapt_shapes_lines(dir+'/'+card, 'data_obs', '', '_asimov')
         print "... adjust observation to modified shapes."
         for card in os.listdir(dir) :
             if not card.endswith('.txt') :
