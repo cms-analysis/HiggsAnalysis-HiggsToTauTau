81f33ceeb8e2f4976a13307a7f203f2e17921fe2
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2013-06-12 23:10:43 +0000

    changed mass for testing

--- python/MODEL_PARAMS.py
+++ python/MODEL_PARAMS.py
@@ -2,7 +2,7 @@ class MODEL_PARAMS:
     """Class for storing parameters of a model"""
     def __init__(self):
         self.list_of_higgses = ['A', 'H', 'h']
-        self.masses = {'A' : '120', 'H' : '130', 'h' : '118'}
+        self.masses = {'A' : '125', 'H' : '130', 'h' : '118'}
         self.xsecs  = {'A' : '3'  , 'H' : '2'  , 'h' : '1'  }
         self.brs    = {'A' : '0.1', 'H' : '0.1', 'h' : '0.1'}
         self.tanb = 15
