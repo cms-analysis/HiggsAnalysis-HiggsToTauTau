a84c9734dc65bc7103c8422868b580c55a8ef8d5
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2013-06-13 22:27:11 +0000

    making life easier in tanb_grid_new.py, the newly added function effective returns the contracted cross section*BR of all Higgses in the list of the model. Whether MOMENT is really needed is still be to be checked. For the moment it is kept for the validation against the old stuff

--- python/MODEL_PARAMS.py
+++ python/MODEL_PARAMS.py
@@ -1,4 +1,6 @@
-class MODEL_PARAMS:
+import math
+
+class MODEL_PARAMS :
     """Class for storing parameters of a model"""
     def __init__(self):
         self.list_of_higgses = ['A', 'H', 'h']
@@ -6,3 +8,12 @@ class MODEL_PARAMS:
         self.xsecs  = {'A' : '3'  , 'H' : '2'  , 'h' : '1'  }
         self.brs    = {'A' : '0.1', 'H' : '0.1', 'h' : '0.1'}
         self.tanb = 15
+        
+    def effective(self, MOMENT=1, higgses=None) :
+        """Return xsec*br for all elements indicated in higgses"""
+        value = 0
+        if not higgses :
+            higgses = self.list_of_higgses
+        for x in higgses :
+            value+=math.pow(float(self.xsecs[x])*float(self.brs[x]), MOMENT)
+        return value if MOMENT == 1 else math.pow(value, 1/MOMENT)
