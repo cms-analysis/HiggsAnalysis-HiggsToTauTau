931cd33c212c45d5bd1b55ba62d8a0f353ca6506
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2013-07-02 12:53:06 +0000

    adding missing file

---
+++ python/summer13_analyses_cfg.py
@@ -0,0 +1,35 @@
+def htt_ee(TYPE='KIT') :
+    dict = {}
+    dict['7TeV'] = 'DESY-KIT'
+    dict['8TeV'] = 'DESY-KIT'
+    return dict
+
+def htt_mm(TYPE='KIT') :
+    dict = {}
+    dict['7TeV'] = 'DESY-KIT'
+    dict['8TeV'] = 'DESY-KIT'
+    return dict
+
+def htt_em(TYPE='MIT') :
+    dict = {}
+    dict['7TeV'] = TYPE
+    dict['8TeV'] = TYPE
+    return dict    
+
+def htt_et(TYPE='Imperial') :
+    dict = {}
+    dict['7TeV'] = TYPE
+    dict['8TeV'] = TYPE
+    return dict
+
+def htt_mt(TYPE='Imperial') :
+    dict = {}
+    dict['7TeV'] = TYPE
+    dict['8TeV'] = TYPE
+    return dict
+
+def htt_tt(TYPE='MIT') :
+    dict = {}
+    dict['7TeV'] = 'None'
+    dict['8TeV'] = TYPE
+    return dict
