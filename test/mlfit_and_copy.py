9904a2438bf969337d90920cfa7525d7d5aeaaae
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2013-07-02 12:29:47 +0000

    adjusting to tanb_grid_new

--- test/mlfit_and_copy.py
+++ test/mlfit_and_copy.py
@@ -40,15 +40,16 @@ if options.analysis == "sm" :
     system("cp -v %s/*.txt ./datacards" % dir)
     system("cp -v %s/../common/*TeV.root ./root" % dir)
     ## for mm override the histograms as used for the limit calculation in favour of something more human readible
-    os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-sm-8TeV-postfit-msv.root ./root/htt_mm.input_8TeV.root")
-    os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-sm-7TeV-postfit-msv.root ./root/htt_mm.input_7TeV.root")
+    ##os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-sm-8TeV-postfit-msv.root ./root/htt_mm.input_8TeV.root")
+    ##os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-sm-7TeV-postfit-msv.root ./root/htt_mm.input_7TeV.root")
 
 if options.analysis == "mssm" :
     system("cp -v %s/out/mlfit.txt ./fitresults/mlfit_mssm.txt" % dir)
     system("cp -v %s/*.txt ./datacards" % dir)
     ##system("cp -v %s/../common/hbb.input_[78]TeV-[01].root ./root" % dir)
-    system("cp -v %s/../common/htt_*.inputs-mssm-[78]TeV-0_%s_%s.00.root ./root" % (dir, options.mA, options.tanb))
+    system("cp -v %s/../common/htt_*.inputs-mssm-[78]TeV-0.root_%s_%s.0 ./root" % (dir, options.mA, options.tanb))
+    system("cp -v %s/../common/htt_*.inputs-mssm-[78]TeV-0.root ./root" % (dir))
     ## for mm override the histograms as used for the limit calculation in favour of something more human readible
-    os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-mssm-8TeV-0_postfit.root ./root/htt_mm.inputs-mssm-8TeV-0.root")
-    os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-mssm-7TeV-0_postfit.root ./root/htt_mm.inputs-mssm-7TeV-0.root")
+    ##os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-mssm-8TeV-0_postfit.root ./root/htt_mm.inputs-mssm-8TeV-0.root")
+    ##os.system("cp -v $CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/setup/mm/htt_mm.inputs-mssm-7TeV-0_postfit.root ./root/htt_mm.inputs-mssm-7TeV-0.root")
 
