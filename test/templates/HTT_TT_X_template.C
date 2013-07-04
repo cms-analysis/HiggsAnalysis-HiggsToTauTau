e54a7fb23c76a93d126a5f45435e18b55b11883c
Felix Frensch <sha1-0a447155b6533bffc74d9189fca33ce0ce7061fc@cern.ch>
2013-07-04 15:07:29 +0000

    changing range of tt for log plots in mssm

--- test/templates/HTT_TT_X_template.C
+++ test/templates/HTT_TT_X_template.C
@@ -281,7 +281,7 @@ HTT_TT_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   canv->cd();
   if(log){ canv->SetLogy(1); }
 #if defined MSSM
-  if(!log){ data->GetXaxis()->SetRange(0, data->FindBin(500)); } else{ data->GetXaxis()->SetRange(0, data->FindBin(500)); };
+  if(!log){ data->GetXaxis()->SetRange(0, data->FindBin(500)); } else{ data->GetXaxis()->SetRange(0, data->FindBin(1000)); };
 #else
   data->GetXaxis()->SetRange(0, data->FindBin(350));
 #endif
