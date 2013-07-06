ac89c3f4180cf20097c06afa9ef3497bcaa17958
Rene Caspart <sha1-117a72c1500b8cb85925aa075b60779d6b80a751@cern.ch>
2013-07-06 22:35:18 +0000

    renamed file and expanded documentation

--- python/lxb-injected-resubmit.py
+++ python/lxb-injected-resubmit.py
@@ -3,8 +3,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='This script is designed to build an resubmitscript for jobs run before. It checks for existing rootfiles with the label "-id_subid" in the output folder and addapts the submitscript accordingly. Please bear in mind ths script does not check whether the rootfiles are created correctly!')
 parser.add_argument('--path', '-p', dest='path', default='LIMITS/cmb/', help='path to result files')
-parser.add_argument('--submitfile', '-sf', dest='submitfile', help='path to submit files')
-parser.add_argument('--sub-jobs', '-sj', dest='subjobs', type=int, default='8', help='number of subjobs')
+parser.add_argument('--submitfile', '-sf', dest='submitfile', help='path to the submitfile for the job')
+parser.add_argument('--sub-jobs', '-sj', dest='subjobs', type=int, default='8', help='number of subjobs in which the toys where submittet. This was determined with the --bunch-masses option.`')
 
 options = parser.parse_args()
 
@@ -16,7 +16,6 @@ subid=0
 for line in source:
     subid = linecount%options.subjobs
     if subid == 0 and linecount != 0: jobid+=1 # jobid is increased if we are through all subid's and only if line is not zero 
-    print glob.glob("%s/*/*-%s_%s*"%(options.path, str(jobid), str(subid)))
     if glob.glob("%s/*/*-%s_%s*"%(options.path, str(jobid), str(subid))) != []:
         if not line.lstrip()[0] == '#':
             line = '#'+line
