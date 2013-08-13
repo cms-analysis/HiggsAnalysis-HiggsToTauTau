81b461b5d254411b1725980bf650d9020dc4aa4b
Roger <roger.wolf@cern.ch>
2013-08-13 19:48:40 +0200

    elevated uncertainty file adaptor script to class

--- scripts/modify_cgs.py
+++ scripts/modify_cgs.py
@@ -1,54 +1,42 @@
+#!/usr/bin/env python
 import argparse
-import os
-import re
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--setup", "-s", nargs="*", default='', dest="setuppath", help="absolute path to the setup directory, in which the operation should take place")
-parser.add_argument("--channel", "-c", nargs="*", dest="channel", help="channels to be considered")
-parser.add_argument("--periods", "-p", nargs="*", dest="period", help="periods to be considered")
-parser.add_argument("--category", "-cat", nargs="*", dest="category", help="categories to be edited")
-parser.add_argument("--add-to-signal", "-as", nargs="*", dest="addsignal", help="processes to be added to the signal")
-parser.add_argument("--add-to-background", "-ab", nargs="*", default='', dest="addbackground", help="processes to be added to the background")
+parser.add_argument("--analysis", "-a", default='sm', dest="analysis", help="Choose here between sm and mssm analysis. [Default: 'sm']")
+parser.add_argument("--setup", "-s", default='', dest="setuppath", help="Absolute path to the setup directory, in which the operation should take place. [Default: '']")
+parser.add_argument("--channels", "-c", nargs="*", dest="channels", help="Channels to be considered.")
+parser.add_argument("--periods", "-p", nargs="*", dest="periods", help="Periods to be considered.")
+parser.add_argument("--categories", "-cat", nargs="*", dest="categories", help="Categories to be edited.")
+parser.add_argument("--add-to-signal", "-as", nargs="*", dest="addsignal", help="Processes to be added to the signal.")
+parser.add_argument("--add-to-background", "-ab", nargs="*", default='', dest="addbackground", help="Processes to be added to the background.")
 opt=parser.parse_args()
 
-pattern = re.compile(r',\s*,')
-## This should not only work on the cantral directory for setup in the package but on dedicated copies of that.
-## I therefore made it configurable. But, Rene, how can I parse a simple string with argparse?!?
-setuppath=opt.setuppath[0]
-for channel in opt.channel:
-    for period in opt.period:
-        for category in opt.category:
-            filename="{SETUP}/{CHANNEL}/cgs-sm-{PERIOD}-{CATEGORY}.conf".format(SETUP=setuppath, CHANNEL=channel, PERIOD=period, CATEGORY=category)
-            infile=open(filename, 'r')
-            outfile=open(filename+"_tmp", 'w')
-            for line in infile:
-                if "signals" in line :
-                    if line[0] == '#' :
-                        line=line.strip('#')
-                if "GROUP signal" in line :
-                    if line[0]=='#':
-                        ## uncomment and remove remnants of what had been commented, in case
-                        ## they exist; furthermore we need to get rid of the leading ',' for
-                        ## the first element. 
-                        line=line[1:line.rfind("GROUP signal")+13]
-                    signalstr=''
-                    for signal in opt.addsignal:
-                        if not signal in line :
-                            signalstr+=","+signal
-                    line=line.rstrip('\n')+signalstr.strip(',')+'\n'
-                    ## remove elements that might be part of signal, in case they should be moved
-                    ## to background.
-                    for signal in opt.addbackground:
-                        line=line.replace(signal, '')
-                        line = re.sub(pattern, '', line)
-                if "GROUP background" in line:
-                    for signal in opt.addsignal:
-                        line=line.replace(signal, '')
-                        line = re.sub(pattern, '', line)
-                    backgroundstr=''
-                    for background in opt.addbackground:
-                        if not background in line :
-                            backgroundstr+=","+background
-                    line=line.rstrip('\n')+backgroundstr+'\n'                        
-                outfile.write(line)
-            os.system("mv -v %s_tmp %s"%(filename, filename))
+import os
+import re
+from HiggsAnalysis.HiggsToTauTau.UncertAdaptor import UncertAdaptor
+
+def main() :
+    print "# --------------------------------------------------------------------------------------"
+    print "# Modify cgs.conf file. "
+    print "# --------------------------------------------------------------------------------------"
+    print "# You are using the following configuration: "
+    print "# --setup             :", opt.setuppath
+    print "# --channels          :", opt.channels
+    print "# --periods           :", opt.periods
+    print "# --categories        :", opt.categories
+    print "# --add-to-signal     :", opt.addsignal
+    print "# --add-to-background :", opt.addbackground
+    print "# Check option --help in case of doubt about the meaning of one or more of these confi-"
+    print "# guration parameters.                           "
+    print "# --------------------------------------------------------------------------------------"
+
+    setuppath=opt.setuppath
+    cgs_shuffle = UncertAdaptor()
+    for channel in opt.channels :
+        for period in opt.periods :
+            for category in opt.categories :
+                filename="{SETUP}/{CHANNEL}/cgs-{ANA}-{PERIOD}-{CATEGORY}.conf".format(SETUP=setuppath, CHANNEL=channel, ANA=opt.analysis, PERIOD=period, CATEGORY=category)
+                print 'processing file:', filename
+                cgs_shuffle.cgs_processes(filename, opt.addsignal, opt.addbackground)
+
+main()
