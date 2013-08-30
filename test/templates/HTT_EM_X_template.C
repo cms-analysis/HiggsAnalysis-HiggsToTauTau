b8ae46e198020be8adbd4a97da2b3a93d1a0d860
Rene Caspart <rcaspart@cern.ch>
2013-08-30 21:15:17 +0200

    aded correct treatment for hww as BG to the em template

--- test/templates/HTT_EM_X_template.C
+++ test/templates/HTT_EM_X_template.C
@@ -23,6 +23,7 @@ static const float SIGNAL_SCALE = 1.;
 $DEFINE_ASIMOV
 $DEFINE_DROP_SIGNAL
 $DEFINE_MSSM
+$DEFINE_HWWBG
 
 /**
    \class   HTT_EM_X_template HTT_EM_X_template.C "HiggsAnalysis/HiggsToTauTau/postfit/tamplates/HTT_EM_X_template.C"
@@ -109,6 +110,12 @@ void rescale(TH1F* hin, unsigned int idx)
   case 7: // VH
   ${SM}VH125
 #endif
+#ifdef HWW_BG
+  case 8: // ggH_hww
+  ${SM}ggH_hww125
+  case 9: // qqH_hww
+  ${SM}qqH_hww125
+#endif
 #endif
   default :
     std::cout << "error histograms not known?!?" << std::endl;
@@ -148,12 +155,21 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   TH1F* ggH  = refill((TH1F*)input2->Get(TString::Format("%s/ggH$MA"     , directory)), "ggH"    ); InitSignal(ggH    ); ggH    ->Scale($TANB);
   TH1F* bbH  = refill((TH1F*)input2->Get(TString::Format("%s/bbH$MA"     , directory)), "bbH"    ); InitSignal(bbH    ); bbH    ->Scale($TANB);
 #else
+#ifdef HWW_BG
+  TH1F* ggH_hww= refill((TH1F*)input->Get(TString::Format("%s/ggH_hww125", directory)), "ggH_hww"); InitHist(ggH_hww  , "", "", kGreen + 2, 1001); ggH_hww->Scale(SIGNAL_SCALE);
+  TH1F* qqH_hww= refill((TH1F*)input->Get(TString::Format("%s/qqH_hww125", directory)), "qqH_hww"); InitHist(qqH_hww  , "", "", kGreen + 2, 1001); qqH_hww->Scale(SIGNAL_SCALE);
+#else
+ TH1F* ggH_hww= refill((TH1F*)input->Get(TString::Format("%s/ggH_hww125", directory)), "ggH_hww"); InitSignal(ggH_hww); ggH_hww->Scale(SIGNAL_SCALE);
+  TH1F* qqH_hww= refill((TH1F*)input->Get(TString::Format("%s/qqH_hww125", directory)), "qqH_hww"); InitSignal(qqH_hww); qqH_hww->Scale(SIGNAL_SCALE);
+#endif
 #ifndef DROP_SIGNAL
   TH1F* ggH    = refill((TH1F*)input->Get(TString::Format("%s/ggH125"    , directory)), "ggH"    ); InitSignal(ggH    ); ggH    ->Scale(SIGNAL_SCALE);
   TH1F* qqH    = refill((TH1F*)input->Get(TString::Format("%s/qqH125"    , directory)), "qqH"    ); InitSignal(qqH    ); qqH    ->Scale(SIGNAL_SCALE);
   TH1F* VH     = refill((TH1F*)input->Get(TString::Format("%s/VH125"     , directory)), "VH"     ); InitSignal(VH     ); VH     ->Scale(SIGNAL_SCALE);
-  TH1F* ggH_hww= refill((TH1F*)input->Get(TString::Format("%s/ggH_hww125", directory)), "ggH_hww"); InitSignal(ggH_hww); ggH_hww->Scale(SIGNAL_SCALE); ggH->Add(ggH_hww);
-  TH1F* qqH_hww= refill((TH1F*)input->Get(TString::Format("%s/qqH_hww125", directory)), "qqH_hww"); InitSignal(qqH_hww); qqH_hww->Scale(SIGNAL_SCALE); qqH->Add(qqH_hww);
+#ifndef HWW_BG
+  ggH->Add(ggH_hww);
+  qqH->Add(qqH_hww);
+#endif
 #endif
 #endif
 #ifdef ASIMOV
@@ -167,8 +183,19 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   ref->Add(EWK  );
   ref->Add(ttbar);
   ref->Add(Ztt  );
+#ifdef HWW_BG
+  ref->Add(qqH_hww);
+  ref->Add(ggH_hww);
+#endif
 
-  double unscaled[7];
+#ifdef HWW_BG
+  const int numhistos = 9;
+  const int offset = 2;
+#else
+  const int numhistos = 7;
+  const int offset = 0;
+#endif
+  double unscaled[numhistos];
   unscaled[0] = Fakes->Integral();
   unscaled[1] = EWK  ->Integral();
   unscaled[2] = ttbar->Integral();
@@ -178,10 +205,14 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   unscaled[5] = bbH  ->Integral();
   unscaled[6] = 0;
 #else
+#ifdef HWW_BG
+  unscaled[4] = ggH_hww  ->Integral();
+  unscaled[5] = qqH_hww  ->Integral();
+#endif
 #ifndef DROP_SIGNAL
-  unscaled[4] = ggH  ->Integral();
-  unscaled[5] = qqH  ->Integral();
-  unscaled[6] = VH   ->Integral();
+  unscaled[4+offset] = ggH  ->Integral();
+  unscaled[5+offset] = qqH  ->Integral();
+  unscaled[6+offset] = VH   ->Integral();
 #endif
 #endif
 
@@ -194,6 +225,10 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
     rescale(ggH,   5);
     rescale(bbH,   6);
 #else
+#ifdef HWW_BG
+    rescale(qqH_hww, 8);
+    rescale(qqH_hww, 9);
+#endif
 #ifndef DROP_SIGNAL
     rescale(ggH,   5);
     rescale(qqH,   6);
@@ -202,36 +237,46 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
 #endif
   }
 
-  TH1F* scales[7];
-  scales[0] = new TH1F("scales-Fakes", "", 7, 0, 7);
+  TH1F* scales[numhistos];
+  scales[0] = new TH1F("scales-Fakes", "", numhistos, 0, numhistos);
   scales[0]->SetBinContent(1, unscaled[0]>0 ? (Fakes->Integral()/unscaled[0]-1.) : 0.);
-  scales[1] = new TH1F("scales-EWK"  , "", 7, 0, 7);
+  scales[1] = new TH1F("scales-EWK"  , "", numhistos, 0, numhistos);
   scales[1]->SetBinContent(2, unscaled[1]>0 ? (EWK  ->Integral()/unscaled[1]-1.) : 0.);
-  scales[2] = new TH1F("scales-ttbar", "", 7, 0, 7);
+  scales[2] = new TH1F("scales-ttbar", "", numhistos, 0, numhistos);
   scales[2]->SetBinContent(3, unscaled[2]>0 ? (ttbar->Integral()/unscaled[2]-1.) : 0.);
-  scales[3] = new TH1F("scales-Ztt"  , "", 7, 0, 7);
+  scales[3] = new TH1F("scales-Ztt"  , "", numhistos, 0, numhistos);
   scales[3]->SetBinContent(4, unscaled[3]>0 ? (Ztt  ->Integral()/unscaled[3]-1.) : 0.);
 #ifdef MSSM
-  scales[4] = new TH1F("scales-ggH"  , "", 7, 0, 7);
+  scales[4] = new TH1F("scales-ggH"  , "", numhistos, 0, numhistos);
   scales[4]->SetBinContent(5, unscaled[4]>0 ? (ggH  ->Integral()/unscaled[4]-1.) : 0.);
-  scales[5] = new TH1F("scales-qqH"  , "", 7, 0, 7);
+  scales[5] = new TH1F("scales-qqH"  , "", numhistos, 0, numhistos);
   scales[5]->SetBinContent(6, unscaled[5]>0 ? (bbH  ->Integral()/unscaled[5]-1.) : 0.);
-  scales[6] = new TH1F("scales-NONE" , "", 7, 0, 7);
-  scales[6]->SetBinContent(7, 0.);
+  scales[6] = new TH1F("scales-NONE" , "", numhistos, 0, numhistos);
+  scales[6]->SetBinContent(numhistos, 0.);
 #else
+#ifdef HWW_BG
+  scales[4] = new TH1F("scales-ggH_hww"  , "", numhistos, 0, numhistos);
+  scales[4]->SetBinContent(5, unscaled[4]>0 ? (ggH_hww  ->Integral()/unscaled[4]-1.) : 0.);
+  scales[5] = new TH1F("scales-qqH_hww"  , "", numhistos, 0, numhistos);
+  scales[5]->SetBinContent(6, unscaled[5]>0 ? (qqH_hww  ->Integral()/unscaled[5]-1.) : 0.);
+#endif  
 #ifndef DROP_SIGNAL
-  scales[4] = new TH1F("scales-ggH"  , "", 7, 0, 7);
-  scales[4]->SetBinContent(5, unscaled[4]>0 ? (ggH  ->Integral()/unscaled[4]-1.) : 0.);
-  scales[5] = new TH1F("scales-qqH"  , "", 7, 0, 7);
-  scales[5]->SetBinContent(6, unscaled[5]>0 ? (qqH  ->Integral()/unscaled[5]-1.) : 0.);
-  scales[6] = new TH1F("scales-VH"   , "", 7, 0, 7);
-  scales[6]->SetBinContent(7, unscaled[6]>0 ? (VH   ->Integral()/unscaled[6]-1.) : 0.);
+  scales[4+offset] = new TH1F("scales-ggH"  , "", numhistos, 0, numhistos);
+  scales[4+offset]->SetBinContent(5+offset, unscaled[4+offset]>0 ? (ggH  ->Integral()/unscaled[4+offset]-1.) : 0.);
+  scales[5+offset] = new TH1F("scales-qqH"  , "", numhistos, 0, numhistos);
+  scales[5+offset]->SetBinContent(6+offset, unscaled[5+offset]>0 ? (qqH  ->Integral()/unscaled[5+offset]-1.) : 0.);
+  scales[6+offset] = new TH1F("scales-VH"   , "", numhistos, 0, numhistos);
+  scales[6+offset]->SetBinContent(7+offset, unscaled[6+offset]>0 ? (VH   ->Integral()/unscaled[6+offset]-1.) : 0.);
 #endif
 #endif
 
   EWK  ->Add(Fakes);
   ttbar->Add(EWK  );
   Ztt  ->Add(ttbar);
+#ifdef HWW_BG
+  qqH_hww->Add(Ztt);
+  ggH_hww->Add(qqH_hww);
+#endif
   if(log){
 #ifdef MSSM
     ggH  ->Add(bbH);
@@ -248,7 +293,11 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
     ggH  ->Add(bbH);
 #else
 #ifndef DROP_SIGNAL
+#ifdef HWW_BG
+    VH   ->Add(qqH_hww);
+#else
     VH   ->Add(Ztt);
+#endif
     qqH  ->Add(VH );
     ggH  ->Add(qqH);
 #endif
@@ -275,7 +324,11 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
 #endif
   data->Draw("e");
 
+#ifdef HWW_BG
+  TH1F* errorBand = (TH1F*)ggH_hww ->Clone("errorBand");
+#else
   TH1F* errorBand = (TH1F*)Ztt ->Clone("errorBand");
+#endif
   errorBand  ->SetMarkerSize(0);
   errorBand  ->SetFillColor(1);
   errorBand  ->SetFillStyle(3013);
@@ -287,6 +340,9 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
     }
   }
   if(log){
+#ifdef HWW_BG
+    ggH_hww->Draw("histsame");
+#endif
     Ztt  ->Draw("histsame");
     ttbar->Draw("histsame");
     EWK  ->Draw("histsame");
@@ -299,6 +355,9 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   else{
 #ifndef DROP_SIGNAL
     ggH  ->Draw("histsame");
+#endif
+#ifdef HWW_BG
+    ggH_hww->Draw("histsame");
 #endif
     Ztt  ->Draw("histsame");
     ttbar->Draw("histsame");
@@ -355,7 +414,12 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
     leg->AddEntry(ggH  , TString::Format("%.0f#timesH(125 GeV)#rightarrow#tau#tau", SIGNAL_SCALE) , "L" );
   }
   else{
+#ifdef HWW_BG
     leg->AddEntry(ggH  , "H(125 GeV)#rightarrow#tau#tau" , "L" );
+#else
+    leg->AddEntry(ggH  , "#splitline{H(125 GeV)#rightarrow#tau#tau}{H(125 GeV)#rightarrow#WW}" , "L" );
+    leg->AddEntry((TObject*)0, "", "");
+#endif
   }
 #endif
 #endif
@@ -363,6 +427,9 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   leg->AddEntry(data , "sum(bkg) + H(125)"              , "LP");
 #else
   leg->AddEntry(data , "observed"                       , "LP");
+#endif
+#ifdef HWW_BG
+  leg->AddEntry(ggH_hww  , "H(125 GeV)#rightarrowWW" , "F" );
 #endif
   leg->AddEntry(Ztt  , "Z#rightarrow#tau#tau"           , "F" );
   leg->AddEntry(ttbar, "t#bar{t}"                       , "F" );
@@ -378,8 +445,11 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   canv0->SetGridx();
   canv0->SetGridy();
   canv0->cd();
-
+#ifdef HWW_BG
+  TH1F* model = (TH1F*)ggH_hww ->Clone("model");
+#else
   TH1F* model = (TH1F*)Ztt ->Clone("model");
+#endif
   TH1F* test1 = (TH1F*)data->Clone("test1"); 
   for(int ibin=0; ibin<test1->GetNbinsX(); ++ibin){
     model->SetBinContent(ibin+1, model->GetBinContent(ibin+1)*model->GetBinWidth(ibin+1));
@@ -448,11 +518,19 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   canv1->cd();
 
   edges.clear();
+#ifdef HWW_BG
+  TH1F* rat2 = (TH1F*) ggH_hww->Clone("rat2");
+  for(int ibin=0; ibin<rat2->GetNbinsX(); ++ibin){
+    rat2->SetBinContent(ibin+1, ref->GetBinContent(ibin+1)>0 ? ggH_hww->GetBinContent(ibin+1)/ref->GetBinContent(ibin+1) : 0);
+    rat2->SetBinError  (ibin+1, ref->GetBinContent(ibin+1)>0 ? ggH_hww->GetBinError  (ibin+1)/ref->GetBinContent(ibin+1) : 0);
+  }
+#else
   TH1F* rat2 = (TH1F*) Ztt->Clone("rat2");
   for(int ibin=0; ibin<rat2->GetNbinsX(); ++ibin){
     rat2->SetBinContent(ibin+1, ref->GetBinContent(ibin+1)>0 ? Ztt->GetBinContent(ibin+1)/ref->GetBinContent(ibin+1) : 0);
     rat2->SetBinError  (ibin+1, ref->GetBinContent(ibin+1)>0 ? Ztt->GetBinError  (ibin+1)/ref->GetBinContent(ibin+1) : 0);
   }
+#endif
   for(int ibin=0; ibin<rat2->GetNbinsX(); ++ibin){
     if(rat2->GetBinContent(ibin+1)>0){
       edges.push_back(TMath::Abs(rat2->GetBinContent(ibin+1)-1.)+TMath::Abs(rat2->GetBinError(ibin+1)));
@@ -500,10 +578,14 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   InitHist  (scales[1], "", "", kRed    + 2, 1001);
   InitHist  (scales[2], "", "", kBlue   - 8, 1001);
   InitHist  (scales[3], "", "", kOrange - 4, 1001);
+#ifdef HWW_BG
+  InitHist(scales[4], "", "", kGreen + 2, 1001);
+  InitHist(scales[5], "", "", kGreen + 2, 1001);
+#endif
 #ifndef DROP_SIGNAL
-  InitSignal(scales[4]);
-  InitSignal(scales[5]);
-  InitSignal(scales[6]);
+  InitSignal(scales[4+offset]);
+  InitSignal(scales[5+offset]);
+  InitSignal(scales[6+offset]);
 #endif
   scales[0]->Draw();
   scales[0]->GetXaxis()->SetBinLabel(1, "#bf{Fakes}");
@@ -515,9 +597,13 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   scales[0]->GetXaxis()->SetBinLabel(6, "#bf{bbH}"  );
   scales[0]->GetXaxis()->SetBinLabel(7, "#bf{NONE}" );
 #else
-  scales[0]->GetXaxis()->SetBinLabel(5, "#bf{ggH}"  );
-  scales[0]->GetXaxis()->SetBinLabel(6, "#bf{qqH}"  );
-  scales[0]->GetXaxis()->SetBinLabel(7, "#bf{VH}"   );
+#ifdef HWW_BG
+  scales[0]->GetXaxis()->SetBinLabel(5, "#bf{ggH_hww}");
+  scales[0]->GetXaxis()->SetBinLabel(6, "#bf{qqH_hww}");
+#endif
+  scales[0]->GetXaxis()->SetBinLabel(5+offset, "#bf{ggH}"  );
+  scales[0]->GetXaxis()->SetBinLabel(6+offset, "#bf{qqH}"  );
+  scales[0]->GetXaxis()->SetBinLabel(7+offset, "#bf{VH}"   );
 #endif
   scales[0]->SetMaximum(+0.5);
   scales[0]->SetMinimum(-0.5);
@@ -526,10 +612,14 @@ HTT_EM_X(bool scaled=true, bool log=true, float min=0.1, float max=-1., string i
   scales[1]->Draw("same");
   scales[2]->Draw("same");
   scales[3]->Draw("same");
-#ifndef DROP_SIGNAL
+#ifdef HWW_BG
   scales[4]->Draw("same");
   scales[5]->Draw("same");
-  scales[6]->Draw("same");
+#endif
+#ifndef DROP_SIGNAL
+  scales[4+offset]->Draw("same");
+  scales[5+offset]->Draw("same");
+  scales[6+offset]->Draw("same");
 #endif
   TH1F* zero_samples = (TH1F*)scales[0]->Clone("zero_samples"); zero_samples->Clear();
   zero_samples->SetBinContent(1,0.);
