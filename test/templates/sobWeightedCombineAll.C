4af2f3e8b5eb6fb25723eeab522739857c0a0015
Rene Caspart <rcaspart@cern.ch>
2013-09-10 19:00:43 +0200

    fixed namings in macro. Currently this macro only works when fitting signal in all categories.

--- test/templates/sobWeightedCombineAll.C
+++ test/templates/sobWeightedCombineAll.C
@@ -27,13 +27,16 @@ root -b /PATH/sobWeightedCombineAll.C
 Authors: Jose Benitez, Lorenzo Bianchini 
 */
 
-#include "HiggsAnalysis/HiggsToTauTau/interface/HttStyles.h"
-#include "HiggsAnalysis/HiggsToTauTau/test/templates/sobWeightedCombine.C"
-#include "HiggsAnalysis/HiggsToTauTau/test/templates/sobWeightedCombineMSSM.C"
+#include "/home/rcaspart/cmssw/mumu_611/CMSSW_6_1_1/src/HiggsAnalysis/HiggsToTauTau/interface/HttStyles.h"
+#include "/home/rcaspart/cmssw/mumu_611/CMSSW_6_1_1/src/HiggsAnalysis/HiggsToTauTau/test/templates/sobWeightedCombine.C"
+#include "/home/rcaspart/cmssw/mumu_611/CMSSW_6_1_1/src/HiggsAnalysis/HiggsToTauTau/test/templates/sobWeightedCombineMSSM.C"
+
+#define NMAXINPUT 65
 
 void sobCombine(TString name, TString* Input,const char* dataset , const char* channel, const char* cat, int weight=1, float muValue=1.0, bool log=false, int mass=125, int tanb=0)
 {
   if(tanb==0){
+    std::cout << "combine" << std::endl;
     sobWeightedCombine(Input,name,weight,muValue);
     /*                                     |-> mu value from the fit to Data
 			 |     |     |->option to apply or not apply weights
@@ -53,112 +56,106 @@ void sobCombine(TString name, TString* Input,const char* dataset , const char* c
   }
 }
 
-void sobWeightedCombineAll(float muValue=1.10, bool log=false, int mass=125, int tanb=0)
+void sobWeightedCombineAll(float muValue=1.02, bool log=false, int mass=125, int tanb=0)
 {
   SetStyle();
 
   TString All_SM[NMAXINPUT]; 
   //vbf
-  All_SM[ 0]="ee_vbf_postfit_7TeV_LIN";
-  All_SM[ 1]="ee_vbf_postfit_8TeV_LIN";
-  All_SM[ 2]="emu_vbf_loose_postfit_7TeV_LIN";
-  All_SM[ 3]="emu_vbf_loose_postfit_8TeV_LIN";
-  All_SM[ 4]="emu_vbf_tight_postfit_8TeV_LIN";
-  All_SM[ 5]="eleTau_vbf_postfit_7TeV_LIN";
-  All_SM[ 6]="eleTau_vbf_loose_postfit_8TeV_LIN";
-  All_SM[ 7]="eleTau_vbf_tight_postfit_8TeV_LIN"; 
-  All_SM[ 8]="mumu_vbf_postfit_7TeV_LIN";
-  All_SM[ 9]="mumu_vbf_postfit_8TeV_LIN";
-  All_SM[10]="muTau_vbf_postfit_7TeV_LIN";
-  All_SM[11]="muTau_vbf_loose_postfit_8TeV_LIN";
-  All_SM[12]="muTau_vbf_tight_postfit_8TeV_LIN"; 
-  All_SM[13]="tauTau_vbf_postfit_8TeV_LIN";
-  All_SM[14]="tauTau_vbf_postfit_8TeV_LIN";
+//  All_SM[ 0]="ee_vbf_postfit_7TeV_LIN";
+//  All_SM[ 1]="ee_vbf_postfit_8TeV_LIN";
+  All_SM[ 0]="emu_vbf_loose_postfit_7TeV_LIN";
+  All_SM[ 1]="emu_vbf_loose_postfit_8TeV_LIN";
+  All_SM[ 2]="emu_vbf_tight_postfit_8TeV_LIN";
+  All_SM[ 3]="eleTau_vbf_loose_postfit_7TeV_LIN";
+  All_SM[ 4]="eleTau_vbf_loose_postfit_8TeV_LIN";
+  All_SM[ 5]="eleTau_vbf_tight_postfit_8TeV_LIN"; 
+  All_SM[ 6]="mumu_vbf_postfit_7TeV_LIN";
+  All_SM[ 7]="mumu_vbf_postfit_8TeV_LIN";
+  All_SM[ 8]="muTau_vbf_loose_postfit_7TeV_LIN";
+  All_SM[ 9]="muTau_vbf_loose_postfit_8TeV_LIN";
+  All_SM[10]="muTau_vbf_tight_postfit_8TeV_LIN"; 
+  All_SM[11]="tauTau_vbf_postfit_8TeV_LIN";
   //1jet
-  All_SM[15]="ee_1jet_low_postfit_7TeV_LIN";
-  All_SM[16]="ee_1jet_low_postfit_8TeV_LIN";
-  All_SM[17]="ee_1jet_high_postfit_7TeV_LIN";
-  All_SM[18]="ee_1jet_high_postfit_8TeV_LIN";
-  All_SM[19]="emu_1jet_low_postfit_7TeV_LIN";
-  All_SM[20]="emu_1jet_low_postfit_8TeV_LIN";
-  All_SM[21]="emu_1jet_high_postfit_7TeV_LIN";
-  All_SM[22]="emu_1jet_high_postfit_8TeV_LIN";
-  All_SM[23]="eleTau_1jet_medium_postfit_7TeV_LIN";
-  All_SM[24]="eleTau_1jet_medium_postfit_8TeV_LIN";
-  All_SM[25]="eleTau_1jet_high_lowhiggs_postfit_7TeV_LIN";
-  All_SM[26]="eleTau_1jet_high_lowhiggs_postfit_8TeV_LIN"; 
-  All_SM[27]="eleTau_1jet_high_mediumhiggs_postfit_7TeV_LIN";
-  All_SM[28]="eleTau_1jet_high_mediumhiggs_postfit_8TeV_LIN"; 
-  All_SM[29]="mumu_1jet_low_postfit_7TeV_LIN";
-  All_SM[30]="mumu_1jet_low_postfit_8TeV_LIN";
-  All_SM[31]="mumu_1jet_high_postfit_7TeV_LIN";
-  All_SM[32]="mumu_1jet_high_postfit_8TeV_LIN";
-  All_SM[33]="muTau_1jet_medium_postfit_7TeV_LIN";
-  All_SM[34]="muTau_1jet_medium_postfit_8TeV_LIN";
-  All_SM[35]="muTau_1jet_high_lowhiggs_postfit_7TeV_LIN";
-  All_SM[36]="muTau_1jet_high_lowhiggs_postfit_8TeV_LIN"; 
-  All_SM[37]="muTau_1jet_high_mediumhiggs_postfit_7TeV_LIN";
-  All_SM[38]="muTau_1jet_high_mediumhiggs_postfit_8TeV_LIN"; 
-  All_SM[39]="tauTau_1jet_high_mediumhiggs_postfit_8TeV_LIN";
-  All_SM[40]="tauTau_1jet_high_highhiggs_postfit_8TeV_LIN";
+//  All_SM[14]="ee_1jet_low_postfit_7TeV_LIN";
+//  All_SM[15]="ee_1jet_low_postfit_8TeV_LIN";
+//  All_SM[16]="ee_1jet_high_postfit_7TeV_LIN";
+//  All_SM[17]="ee_1jet_high_postfit_8TeV_LIN";
+  All_SM[12]="emu_1jet_low_postfit_7TeV_LIN";
+  All_SM[13]="emu_1jet_low_postfit_8TeV_LIN";
+  All_SM[14]="emu_1jet_high_postfit_7TeV_LIN";
+  All_SM[15]="emu_1jet_high_postfit_8TeV_LIN";
+  All_SM[16]="eleTau_1jet_medium_postfit_7TeV_LIN";
+  All_SM[17]="eleTau_1jet_medium_postfit_8TeV_LIN";
+  All_SM[18]="eleTau_1jet_high_mediumhiggs_postfit_7TeV_LIN";
+  All_SM[19]="eleTau_1jet_high_mediumhiggs_postfit_8TeV_LIN"; 
+//  All_SM[26]="mumu_1jet_low_postfit_7TeV_LIN";
+//  All_SM[27]="mumu_1jet_low_postfit_8TeV_LIN";
+//  All_SM[28]="mumu_1jet_high_postfit_7TeV_LIN";
+//  All_SM[29]="mumu_1jet_high_postfit_8TeV_LIN";
+  All_SM[20]="muTau_1jet_medium_postfit_7TeV_LIN";
+  All_SM[21]="muTau_1jet_medium_postfit_8TeV_LIN";
+  All_SM[22]="muTau_1jet_high_lowhiggs_postfit_7TeV_LIN";
+  All_SM[23]="muTau_1jet_high_lowhiggs_postfit_8TeV_LIN"; 
+  All_SM[24]="muTau_1jet_high_mediumhiggs_postfit_7TeV_LIN";
+  All_SM[25]="muTau_1jet_high_mediumhiggs_postfit_8TeV_LIN"; 
+  All_SM[26]="tauTau_1jet_high_mediumhiggs_postfit_8TeV_LIN";
+  All_SM[27]="tauTau_1jet_high_highhiggs_postfit_8TeV_LIN";
   //0jet
-  All_SM[41]="ee_0jet_low_postfit_7TeV_LIN";
-  All_SM[42]="ee_0jet_low_postfit_8TeV_LIN";
-  All_SM[43]="ee_0jet_high_postfit_7TeV_LIN";
-  All_SM[44]="ee_0jet_high_postfit_8TeV_LIN";
-  All_SM[45]="emu_0jet_low_postfit_7TeV_LIN";
-  All_SM[46]="emu_0jet_low_postfit_8TeV_LIN";
-  All_SM[47]="emu_0jet_high_postfit_7TeV_LIN";
-  All_SM[48]="emu_0jet_high_postfit_8TeV_LIN";
-  All_SM[49]="eleTau_0jet_low_postfit_7TeV_LIN";
-  All_SM[50]="eleTau_0jet_low_postfit_8TeV_LIN";
-  All_SM[51]="eleTau_0jet_medium_postfit_7TeV_LIN";
-  All_SM[52]="eleTau_0jet_medium_postfit_8TeV_LIN"; 
-  All_SM[53]="eleTau_0jet_high_postfit_7TeV_LIN";
-  All_SM[54]="eleTau_0jet_high_postfit_8TeV_LIN"; 
-  All_SM[55]="mumu_0jet_low_postfit_7TeV_LIN";
-  All_SM[56]="mumu_0jet_low_postfit_8TeV_LIN";
-  All_SM[57]="mumu_0jet_high_postfit_7TeV_LIN";
-  All_SM[58]="mumu_0jet_high_postfit_8TeV_LIN"; 
-  All_SM[59]="muTau_0jet_low_postfit_7TeV_LIN";
-  All_SM[60]="muTau_0jet_low_postfit_8TeV_LIN";
-  All_SM[61]="muTau_0jet_medium_postfit_7TeV_LIN";
-  All_SM[62]="muTau_0jet_medium_postfit_8TeV_LIN"; 
-  All_SM[63]="muTau_0jet_high_postfit_7TeV_LIN";
-  All_SM[64]="muTau_0jet_high_postfit_8TeV_LIN";  
+//  All_SM[38]="ee_0jet_low_postfit_7TeV_LIN";
+//  All_SM[39]="ee_0jet_low_postfit_8TeV_LIN";
+//  All_SM[40]="ee_0jet_high_postfit_7TeV_LIN";
+//  All_SM[41]="ee_0jet_high_postfit_8TeV_LIN";
+  All_SM[28]="emu_0jet_low_postfit_7TeV_LIN";
+  All_SM[29]="emu_0jet_low_postfit_8TeV_LIN";
+  All_SM[30]="emu_0jet_high_postfit_7TeV_LIN";
+  All_SM[31]="emu_0jet_high_postfit_8TeV_LIN";
+  All_SM[32]="eleTau_0jet_low_postfit_7TeV_LIN";
+  All_SM[33]="eleTau_0jet_low_postfit_8TeV_LIN";
+  All_SM[34]="eleTau_0jet_medium_postfit_7TeV_LIN";
+  All_SM[35]="eleTau_0jet_medium_postfit_8TeV_LIN"; 
+  All_SM[36]="eleTau_0jet_high_postfit_7TeV_LIN";
+  All_SM[37]="eleTau_0jet_high_postfit_8TeV_LIN"; 
+//  All_SM[52]="mumu_0jet_low_postfit_7TeV_LIN";
+//  All_SM[53]="mumu_0jet_low_postfit_8TeV_LIN";
+//  All_SM[54]="mumu_0jet_high_postfit_7TeV_LIN";
+//  All_SM[55]="mumu_0jet_high_postfit_8TeV_LIN"; 
+  All_SM[38]="muTau_0jet_low_postfit_7TeV_LIN";
+  All_SM[39]="muTau_0jet_low_postfit_8TeV_LIN";
+  All_SM[40]="muTau_0jet_medium_postfit_7TeV_LIN";
+  All_SM[41]="muTau_0jet_medium_postfit_8TeV_LIN"; 
+  All_SM[42]="muTau_0jet_high_postfit_7TeV_LIN";
+  All_SM[43]="muTau_0jet_high_postfit_8TeV_LIN";  
   
   TString EMETMTTT_1JET_VBF_SM[NMAXINPUT]; 
   //vbf
   EMETMTTT_1JET_VBF_SM[ 0]="emu_vbf_loose_postfit_7TeV_LIN";
   EMETMTTT_1JET_VBF_SM[ 1]="emu_vbf_loose_postfit_8TeV_LIN";
   EMETMTTT_1JET_VBF_SM[ 2]="emu_vbf_tight_postfit_8TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[ 3]="eleTau_vbf_postfit_7TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[ 3]="eleTau_vbf_loose_postfit_7TeV_LIN";
   EMETMTTT_1JET_VBF_SM[ 4]="eleTau_vbf_loose_postfit_8TeV_LIN";
   EMETMTTT_1JET_VBF_SM[ 5]="eleTau_vbf_tight_postfit_8TeV_LIN"; 
-  EMETMTTT_1JET_VBF_SM[ 6]="muTau_vbf_postfit_7TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[ 6]="muTau_vbf_loose_postfit_7TeV_LIN";
   EMETMTTT_1JET_VBF_SM[ 7]="muTau_vbf_loose_postfit_8TeV_LIN";
   EMETMTTT_1JET_VBF_SM[ 8]="muTau_vbf_tight_postfit_8TeV_LIN"; 
   EMETMTTT_1JET_VBF_SM[ 9]="tauTau_vbf_postfit_8TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[10]="tauTau_vbf_postfit_8TeV_LIN";
   //1jet
-  EMETMTTT_1JET_VBF_SM[11]="emu_1jet_low_postfit_7TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[12]="emu_1jet_low_postfit_8TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[13]="emu_1jet_high_postfit_7TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[14]="emu_1jet_high_postfit_8TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[15]="eleTau_1jet_medium_postfit_7TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[16]="eleTau_1jet_medium_postfit_8TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[17]="eleTau_1jet_high_lowhiggs_postfit_7TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[18]="eleTau_1jet_high_lowhiggs_postfit_8TeV_LIN"; 
-  EMETMTTT_1JET_VBF_SM[19]="eleTau_1jet_high_mediumhiggs_postfit_7TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[20]="eleTau_1jet_high_mediumhiggs_postfit_8TeV_LIN"; 
-  EMETMTTT_1JET_VBF_SM[21]="muTau_1jet_medium_postfit_7TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[22]="muTau_1jet_medium_postfit_8TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[23]="muTau_1jet_high_lowhiggs_postfit_7TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[24]="muTau_1jet_high_lowhiggs_postfit_8TeV_LIN"; 
-  EMETMTTT_1JET_VBF_SM[25]="muTau_1jet_high_mediumhiggs_postfit_7TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[26]="muTau_1jet_high_mediumhiggs_postfit_8TeV_LIN"; 
-  EMETMTTT_1JET_VBF_SM[27]="tauTau_1jet_high_mediumhiggs_postfit_8TeV_LIN";
-  EMETMTTT_1JET_VBF_SM[28]="tauTau_1jet_high_highhiggs_postfit_8TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[10]="emu_1jet_low_postfit_7TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[11]="emu_1jet_low_postfit_8TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[12]="emu_1jet_high_postfit_7TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[13]="emu_1jet_high_postfit_8TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[14]="eleTau_1jet_medium_postfit_7TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[15]="eleTau_1jet_medium_postfit_8TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[16]="eleTau_1jet_high_mediumhiggs_postfit_7TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[17]="eleTau_1jet_high_mediumhiggs_postfit_8TeV_LIN"; 
+  EMETMTTT_1JET_VBF_SM[18]="muTau_1jet_medium_postfit_7TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[19]="muTau_1jet_medium_postfit_8TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[20]="muTau_1jet_high_lowhiggs_postfit_7TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[21]="muTau_1jet_high_lowhiggs_postfit_8TeV_LIN"; 
+  EMETMTTT_1JET_VBF_SM[22]="muTau_1jet_high_mediumhiggs_postfit_7TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[23]="muTau_1jet_high_mediumhiggs_postfit_8TeV_LIN"; 
+  EMETMTTT_1JET_VBF_SM[24]="tauTau_1jet_high_mediumhiggs_postfit_8TeV_LIN";
+  EMETMTTT_1JET_VBF_SM[25]="tauTau_1jet_high_highhiggs_postfit_8TeV_LIN";
 
   TString EE_SM[NMAXINPUT];
   EE_SM[ 0]="ee_vbf_postfit_7TeV_LIN";
@@ -176,31 +173,29 @@ void sobWeightedCombineAll(float muValue=1.10, bool log=false, int mass=125, int
   EMu_SM[ 0]="emu_vbf_loose_postfit_7TeV_LIN";
   EMu_SM[ 1]="emu_vbf_loose_postfit_8TeV_LIN";
   EMu_SM[ 2]="emu_vbf_tight_postfit_8TeV_LIN";
-  EMu_SM[ 3]="emu_0jet_low_postfit_7TeV_LIN";
-  EMu_SM[ 4]="emu_0jet_low_postfit_8TeV_LIN";
-  EMu_SM[ 5]="emu_0jet_high_postfit_7TeV_LIN";
-  EMu_SM[ 6]="emu_0jet_high_postfit_8TeV_LIN";
+  EMu_SM[ 3]="emu_1jet_low_postfit_7TeV_LIN";
+  EMu_SM[ 4]="emu_1jet_low_postfit_8TeV_LIN";
+  EMu_SM[ 5]="emu_1jet_high_postfit_7TeV_LIN";
+  EMu_SM[ 6]="emu_1jet_high_postfit_8TeV_LIN";
   EMu_SM[ 7]="emu_0jet_low_postfit_7TeV_LIN";
   EMu_SM[ 8]="emu_0jet_low_postfit_8TeV_LIN";
   EMu_SM[ 9]="emu_0jet_high_postfit_7TeV_LIN";
   EMu_SM[10]="emu_0jet_high_postfit_8TeV_LIN";
 
   TString ETau_SM[NMAXINPUT];
-  ETau_SM[ 0]="eleTau_vbf_postfit_7TeV_LIN";
+  ETau_SM[ 0]="eleTau_vbf_loose_postfit_7TeV_LIN";
   ETau_SM[ 1]="eleTau_vbf_loose_postfit_8TeV_LIN";
   ETau_SM[ 2]="eleTau_vbf_tight_postfit_8TeV_LIN"; 
   ETau_SM[ 3]="eleTau_1jet_medium_postfit_7TeV_LIN";
   ETau_SM[ 4]="eleTau_1jet_medium_postfit_8TeV_LIN";
-  ETau_SM[ 5]="eleTau_1jet_high_lowhiggs_postfit_7TeV_LIN";
-  ETau_SM[ 6]="eleTau_1jet_high_lowhiggs_postfit_8TeV_LIN"; 
-  ETau_SM[ 7]="eleTau_1jet_high_mediumhiggs_postfit_7TeV_LIN";
-  ETau_SM[ 8]="eleTau_1jet_high_mediumhiggs_postfit_8TeV_LIN"; 
-  ETau_SM[ 9]="eleTau_0jet_low_postfit_7TeV_LIN";
-  ETau_SM[10]="eleTau_0jet_low_postfit_8TeV_LIN";
-  ETau_SM[11]="eleTau_0jet_medium_postfit_7TeV_LIN";
-  ETau_SM[12]="eleTau_0jet_medium_postfit_8TeV_LIN"; 
-  ETau_SM[13]="eleTau_0jet_high_postfit_7TeV_LIN";
-  ETau_SM[14]="eleTau_0jet_high_postfit_8TeV_LIN"; 
+  ETau_SM[ 5]="eleTau_1jet_high_mediumhiggs_postfit_7TeV_LIN";
+  ETau_SM[ 6]="eleTau_1jet_high_mediumhiggs_postfit_8TeV_LIN"; 
+  ETau_SM[ 7]="eleTau_0jet_low_postfit_7TeV_LIN";
+  ETau_SM[ 8]="eleTau_0jet_low_postfit_8TeV_LIN";
+  ETau_SM[ 9]="eleTau_0jet_medium_postfit_7TeV_LIN";
+  ETau_SM[10]="eleTau_0jet_medium_postfit_8TeV_LIN"; 
+  ETau_SM[11]="eleTau_0jet_high_postfit_7TeV_LIN";
+  ETau_SM[12]="eleTau_0jet_high_postfit_8TeV_LIN"; 
 
   TString MuMu_SM[NMAXINPUT];
   MuMu_SM[ 0]="mumu_vbf_postfit_7TeV_LIN";
@@ -215,7 +210,7 @@ void sobWeightedCombineAll(float muValue=1.10, bool log=false, int mass=125, int
   MuMu_SM[ 9]="mumu_0jet_high_postfit_8TeV_LIN"; 
   
   TString MuTau_SM[NMAXINPUT];
-  MuTau_SM[ 0]="muTau_vbf_postfit_7TeV_LIN";
+  MuTau_SM[ 0]="muTau_vbf_loose_postfit_7TeV_LIN";
   MuTau_SM[ 1]="muTau_vbf_loose_postfit_8TeV_LIN";
   MuTau_SM[ 2]="muTau_vbf_tight_postfit_8TeV_LIN"; 
   MuTau_SM[ 3]="muTau_1jet_medium_postfit_7TeV_LIN";
@@ -233,9 +228,8 @@ void sobWeightedCombineAll(float muValue=1.10, bool log=false, int mass=125, int
 
   TString TauTau_SM[NMAXINPUT];
   TauTau_SM[ 0]="tauTau_vbf_postfit_8TeV_LIN";
-  TauTau_SM[ 1]="tauTau_vbf_postfit_8TeV_LIN";
-  TauTau_SM[ 2]="tauTau_1jet_high_mediumhiggs_postfit_8TeV_LIN";
-  TauTau_SM[ 3]="tauTau_1jet_high_highhiggs_postfit_8TeV_LIN";
+  TauTau_SM[ 1]="tauTau_1jet_high_mediumhiggs_postfit_8TeV_LIN";
+  TauTau_SM[ 2]="tauTau_1jet_high_highhiggs_postfit_8TeV_LIN";
   
 
 
@@ -294,17 +288,17 @@ void sobWeightedCombineAll(float muValue=1.10, bool log=false, int mass=125, int
 
   TString dataset="CMS Preliminary,  H#rightarrow#tau#tau,  4.9 fb^{-1} at 7 TeV, 19.8 fb^{-1} at 8 TeV";
 
-  //  sobCombine("All_SM", All_SM, dataset, "ee, e#mu, e#tau_{h}, #mu#mu, #mu#tau_{h}, #tau_{h}#tau_{h}" , "", 1, muValue);
-  //sobCombine("EMETMTTT_1JET_VBF_SM", All_SM, dataset, "e#mu, e#tau_{h}, #mu#tau_{h}, #tau_{h}#tau_{h}" , "", 1, muValue); //no ee, mm, 0jet here - some fixes needed
-  //  sobCombine("EE_SM", All_SM, dataset, "ee" , "", 1, muValue);
-  //  sobCombine("EMu_SM", All_SM, dataset, "e#mu" , "", 1, muValue);
-  //  sobCombine("ETau_SM", All_SM, dataset, "e#tau_{h}" , "", 1, muValue);
-  //  sobCombine("MuMu_SM", All_SM, dataset, " #mu#mu" , "", 1, muValue);
-  //  sobCombine("MuTau_SM", All_SM, dataset, "#mu#tau_{h}" , "", 1, muValue);
-  //  sobCombine("TauTau_SM", All_SM, dataset, "#tau_{h}#tau_{h}" , "", 1, muValue);
+    sobCombine("All_SM", All_SM, dataset, "ee, e#mu, e#tau_{h}, #mu#mu, #mu#tau_{h}, #tau_{h}#tau_{h}" , "", 1, muValue);
+    sobCombine("EMETMTTT_1JET_VBF_SM", EMETMTTT_1JET_VBF_SM, dataset, "e#mu, e#tau_{h}, #mu#tau_{h}, #tau_{h}#tau_{h}" , "", 1, muValue); //no ee, mm, 0jet here - some fixes needed
+  //  sobCombine("EE_SM", EE_SM, dataset, "ee" , "", 1, muValue);
+    sobCombine("EMu_SM", EMu_SM, dataset, "e#mu" , "", 1, muValue);
+    sobCombine("ETau_SM", ETau_SM, dataset, "e#tau_{h}" , "", 1, muValue);
+  //  sobCombine("MuMu_SM", MuMu_SM, dataset, " #mu#mu" , "", 1, muValue);
+    sobCombine("MuTau_SM", MuTau_SM, dataset, "#mu#tau_{h}" , "", 1, muValue);
+    sobCombine("TauTau_SM", TauTau_SM, dataset, "#tau_{h}#tau_{h}" , "", 1, muValue);
 
-  //sobCombine("All_MSSM", All_MSSM, dataset, "#scale[1]e#mu_{h}, #scale[1]e#tau_{h}, #scale[1]{#mu}#mu_{h}, #scale[1]{#mu}#tau_{h}, #tau_{h}#tau_{h}" , "", 1, muValue, log, mass, tanb);
-  sobCombine("All_MSSM", All_MSSM, dataset, "e#mu_{h}, e#tau_{h}, #mu#tau_{h}, #tau_{h}#tau_{h}" , "", 1, muValue, log, mass, tanb); 
+  // sobCombine("All_MSSM", All_MSSM, dataset, "#scale[1]e#mu_{h}, #scale[1]e#tau_{h}, #scale[1]{#mu}#mu_{h}, #scale[1]{#mu}#tau_{h}, #tau_{h}#tau_{h}" , "", 1, muValue, log, mass, tanb);
+  // sobCombine("All_MSSM", All_MSSM, dataset, "e#mu_{h}, e#tau_{h}, #mu#tau_{h}, #tau_{h}#tau_{h}" , "", 1, muValue, log, mass, tanb); 
   // sobCombine("EMu_MSSM", EMu_MSSM, dataset, "e#mu_{h}" , "", 1, muValue, log, mass, tanb);
   // sobCombine("ETau_MSSM", ETau_MSSM, dataset, "e#tau_{h}" , "", 1, muValue, log, mass, tanb);
   // sobCombine("MuMu_MSSM", MuMu_MSSM, dataset, "#mu#mu" , "", 1, muValue, log, mass, tanb);
