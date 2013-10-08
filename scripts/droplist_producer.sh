2953ada0df59ab171b6b4200e40603f5dd05246c
Rene Caspart <rcaspart@cern.ch>
2013-10-08 21:19:25 +0200

    added config for combination on 8th October 2013. Updated droplist_producer

--- scripts/droplist_producer.sh
+++ scripts/droplist_producer.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-python HiggsAnalysis/HiggsToTauTau/scripts/doSM.py --update-all --label _droplist -a "bbb" --add-mutau-soft 125
-for i in ee em et mt mm tt vhtt
+python HiggsAnalysis/HiggsToTauTau/scripts/doSM.py --update-all --label _droplist -a "bbb" 125
+for i in em et mt tt
 do
     submit.py --max-likelihood LIMITS_droplist/bbb/$i/125
 done
 
-## create the droplists per channel and give them individual names
-for i in ee mm
+for i in tt
 do
     while true
     do
         if [ -s "LIMITS_droplist/bbb/$i/125/out/mlfit_largest-constraints.html" ]
         then
-            python HiggsAnalysis/HiggsToTauTau/scripts/prune-uncerts-htt.py --fit-results LIMITS_droplist/bbb/$i/125/out/mlfit.txt --whitelist "_bin_" --shielding 1:0.4 LIMITS_droplist/bbb/$i/125
+            python HiggsAnalysis/HiggsToTauTau/scripts/prune-uncerts-htt.py --threshold 0.05 --fit-results LIMITS_droplist/bbb/$i/125/out/mlfit.txt --whitelist "_bin_" --shielding 125:0.3 LIMITS_droplist/bbb/$i/125
             mv -v uncertainty-pruning-drop.txt uncertainty-pruning-drop_${i}.txt
             break
         fi
         sleep 15
     done
 done
-for i in tt em et mt vhtt
+for i in em et mt
 do
     while true
     do
         if [ -s "LIMITS_droplist/bbb/$i/125/out/mlfit_largest-constraints.html" ]
         then
-            python HiggsAnalysis/HiggsToTauTau/scripts/prune-uncerts-htt.py --fit-results LIMITS_droplist/bbb/$i/125/out/mlfit.txt --whitelist "_bin_" --shielding 125:0.3 LIMITS_droplist/bbb/$i/125
+            python HiggsAnalysis/HiggsToTauTau/scripts/prune-uncerts-htt.py --threshold 0.1 --fit-results LIMITS_droplist/bbb/$i/125/out/mlfit.txt --whitelist "_bin_" --shielding 125:0.3 LIMITS_droplist/bbb/$i/125
             mv -v uncertainty-pruning-drop.txt uncertainty-pruning-drop_${i}.txt
             break
         fi
