25453643394f8e7d1ca13455cbbb484d49e2b883
Rebecca Lane <rcl11@imperial.ac.uk>
2013-11-11 16:03:37 +0100

    adding label change to make CMS label line up with y axis

--- src/Limit.cc
+++ src/Limit.cc
@@ -35,7 +35,7 @@ PlotLimits::plotLimit(TCanvas& canv, TGraphAsymmErrors* innerBand, TGraphAsymmEr
   plottingLimit(canv, innerBand, outerBand, expected, observed, unit, xaxis_, yaxis_, 0, min, max, log_, PLOT, injectedMass_, mssm_, extra_);
   // setup CMS Preliminary
   //CMSPrelim(dataset_.c_str(), "", 0.160, 0.835);
-  CMSPrelim(dataset_.c_str(), "", 0.145, 0.835);
+  CMSPrelim(dataset_.c_str(), "", 0.135, 0.835);
 
   /*
   TPaveText* chan     = new TPaveText(0.60, 0.82, 0.80, 0.92, "NDC");
