7398add44f7fc34f4c807f04e054dfef16048aab
Andrew Gilbert <andrew.gilbert@cern.ch>
2013-11-16 17:46:13 +0100

    Several fixes and improvements for multi-dim fits
     - Implement special combine options for --stable
     - Add instructions in sm-protocol for patching combine and running fits
     - Modify src/MultidimFit.cc to catch some cases where extra added points
       would have been in the wrong place
     - Updates to the plotting layout files

--- python/layouts/multidim-fit-rV-rF.py
+++ python/layouts/multidim-fit-rV-rF.py
@@ -2,11 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 layout = cms.PSet(
     ## dataset
-    dataset = cms.string("Preliminary, #sqrt{s}=7-8 TeV, L = 24.3 fb^{-1}, H #rightarrow #tau #tau"),
+    dataset = cms.string("CMS H#rightarrow#tau#tau, 4.9 fb^{-1} at 7 TeV, 19.7 fb^{-1} at 8 TeV"),
     ## x-axis title
     xaxis = cms.string("r_{V}"),
     ## x-axis title
-    yaxis = cms.string("r_{F}"),
+    yaxis = cms.string("r_{f}"),
     ## min for plotting (for fine tuning)
     min = cms.double(0.),
     ## min for plotting
@@ -27,6 +27,8 @@ layout = cms.PSet(
     masspoints = cms.vdouble(125),
     ## make a temperature plot?
     temp = cms.bool(False),
+    ## smooth plot? 
+    smooth = cms.bool(False),
     ## define physics-model
     model = cms.string("RV-RF"),
     ## max for plotting (for fine tuning)
