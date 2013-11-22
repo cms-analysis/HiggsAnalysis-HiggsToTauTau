fba653ebf4aed5e896969abea8be8ec88b03fa0b
Roger <roger.wolf@cern.ch>
2013-11-22 15:30:03 +0100

    commented debaug print out

--- python/horizontal_morphing.py
+++ python/horizontal_morphing.py
@@ -55,7 +55,7 @@ class Morph:
         """
         name = sample
         hist = self.load_hist(file, directory, name.format(MASS=value))
-        print file, directory, name.format(MASS=value)
+        ##print file, directory, name.format(MASS=value)
         for i in range(hist.GetNbinsX()):
             if hist.GetBinContent(i+1) < 0:
                 print "Warning: setting negativ content of bin {BIN} to zero".format(BIN=str(i))
