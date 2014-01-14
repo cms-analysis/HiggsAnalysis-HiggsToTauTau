5465777f13b2dc0a74498ab455b97bdf4c1b1fc2
Andrew Gilbert <andrew.gilbert@cern.ch>
2014-01-14 15:02:18 +0100

    changing QCDScale_ggH

--- scripts/doSM.py
+++ scripts/doSM.py
@@ -19,8 +19,8 @@ parser.add_option("--label", dest="label", default="", type="string",
 parser.add_option("--blind-datacards", dest="blind_datacards", default=False, action="store_true",
                   help="Option to blind datacards. Also needs to be turned on to inject SM to datacards. [Default: False]")
 parser.add_option("--extra-templates", dest="extra_templates", default="", type="string", help="List of extra background or signal templates which should be injected to the asimov dataset. Needs to be comma seperated list. Here used to inject SM signal into MSSM datacards. [Default: \"\"]")
-parser.add_option("--ignore-during-scaling", dest="do_not_scales", default="mm vhtt", type="string",
-                  help="List of channels, which the scaling by cross seciton times BR should not be applied. The list should be embraced by call-ons and separeted by whitespace or comma. [Default: \"vhtt mm\"]")
+parser.add_option("--ignore-during-scaling", dest="do_not_scales", default="vhtt", type="string",
+                  help="List of channels, which the scaling by cross seciton times BR should not be applied. The list should be embraced by call-ons and separeted by whitespace or comma. [Default: \"vhtt\"]")
 parser.add_option("--hww-mass", dest="hww_mass", default='', type="string",
                   help="specify this option if you want to fix the hww contributions for the channels em and vhtt to a given mass. This configuration applies to hww as part of the signal. When an empty string is given the mass will be scanned. For analysis hhw-bg, where hww signal contributions are defined as background in any case mH=125 GeV willbe chosenindependent from this configuration. [Default: '']")
 parser.add_option("--hww-scale", dest="hww_scale", default='1.', type="string",
@@ -105,10 +105,10 @@ if options.mvis:
         }
 if options.rebin_morph:
     patterns = {
-        'no-bbb'         : '-fbsig',
-        'bbb'            : '-fbsig',
-        'no-bbb:hww-sig' : '-fbsig',
-        'bbb:hww-sig'    : '-fbsig',
+        'no-bbb'         : '-hcg',
+        'bbb'            : '-hcg',
+        'no-bbb:hww-sig' : '-hcg',
+        'bbb:hww-sig'    : '-hcg',
         }
 
 if options.update_all :
