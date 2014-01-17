12ce21aa9ccc6f3da2c3788415c3b8f31cbfa9ef
Felix Frensch <frensch@tcx101.naf.desy.de>
2014-01-17 14:57:36 +0100

    extend to all MSSM benchmark scenarios

--- data/Higgs125/higgs-masses.py
+++ data/Higgs125/higgs-masses.py
@@ -1,13 +1,41 @@
 import os
 from HiggsAnalysis.HiggsToTauTau.tools.mssm_xsec_tools import mssm_xsec_tools
 
-path = "auxiliaries/models/out.mhmax-mu+200-8TeV-tanbHigh-nnlo.root"
-for mA in [90, 100, 120, 130, 140, 160, 180, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000] :
-    file_name = "{CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/data/Higgs125/higgs_{MASS}.dat".format(CMSSW_BASE=os.environ['CMSSW_BASE'],MASS=mA)
-    print "writing file:", file_name
-    file = open(file_name,'w')
-    for tanb in range(600) :
-        mssm_scan = mssm_xsec_tools("{CMSSW_BASE}/src/{PATH}".format(CMSSW_BASE=os.environ['CMSSW_BASE'], PATH=path))
-        htt_query = mssm_scan.query(mA, tanb/10.) ; mh = htt_query['higgses']['h']['mass'] ; mH = htt_query['higgses']['H']['mass']
-        print "%f \t%f \t%f \t%f \n" % (tanb/10., mh, mA, mH) ; file.write("%f \t%f \t%f \t%f \n" % (tanb/10., mh, mA, mH))	
-    file.close()
+from optparse import OptionParser, OptionGroup
+
+## set up the option parser
+parser = OptionParser(usage="usage: %prog [options] ARG1 ARG2 ARG3 ...", description="This is a script to print out txt files containing tanb and the masses of the neutral higgs bosons of a given MSSM Higgs model.")
+cgroup = OptionGroup(parser, "")
+cgroup.add_option("--model", dest="model", default="mhmax-mu+200", type="string",
+                  help="The model for which you will look up the values. [Default: \"mhmax-mu+200\"]")
+cgroup.add_option("--verbosity", dest="verbosity", default=False, action="store_true",
+                  help="Run in verbosity mode. [Default: False]")
+parser.add_option_group(cgroup)
+## check number of arguments; in case print usage
+(options, args) = parser.parse_args()
+
+path = "auxiliaries/models/out.{MODEL}-8TeV-tanbHigh-nnlo.root".format(MODEL=options.model)
+if options.model=="lowmH" :
+    for MUE in range(300, 3101, 20) :
+        file_name = "{CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/data/Higgs125/{MODEL}/higgs_{MASS}.dat".format(CMSSW_BASE=os.environ['CMSSW_BASE'],MODEL=options.model,MASS=MUE)
+        print "writing file:", file_name
+        file = open(file_name,'w')
+        for tanb in range(15, 95) :
+            mssm_scan = mssm_xsec_tools("{CMSSW_BASE}/src/{PATH}".format(CMSSW_BASE=os.environ['CMSSW_BASE'], PATH=path))
+            htt_query = mssm_scan.query(MUE, tanb/10.) ; mh = htt_query['higgses']['h']['mass'] ; mH = htt_query['higgses']['H']['mass'] ; mA = htt_query['higgses']['A']['mass']
+            if options.verbosity :
+               print "%f \t%f \t%f \t%f \n" % (tanb/10., mh, mA, mH) ;
+            file.write("%f \t%f \t%f \t%f \n" % (tanb/10., mh, mA, mH))	
+        file.close()
+else:
+    for mA in [90, 100, 120, 130, 140, 160, 180, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000] :
+        file_name = "{CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/data/Higgs125/{MODEL}/higgs_{MASS}.dat".format(CMSSW_BASE=os.environ['CMSSW_BASE'],MODEL=options.model,MASS=mA)
+        print "writing file:", file_name
+        file = open(file_name,'w')
+        for tanb in range(5, 600) :
+            mssm_scan = mssm_xsec_tools("{CMSSW_BASE}/src/{PATH}".format(CMSSW_BASE=os.environ['CMSSW_BASE'], PATH=path))
+            htt_query = mssm_scan.query(mA, tanb/10.) ; mh = htt_query['higgses']['h']['mass'] ; mH = htt_query['higgses']['H']['mass'] 
+            if options.verbosity :
+                print "%f \t%f \t%f \t%f \n" % (tanb/10., mh, mA, mH) ;
+            file.write("%f \t%f \t%f \t%f \n" % (tanb/10., mh, mA, mH))	
+        file.close()
