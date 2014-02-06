0f43f26779215abdfc25e1ed3325d03f21ba984e
Felix Frensch <ffrensch@nafhh-cms02.desy.de>
2014-02-06 14:59:54 +0100

    build in of 1D injected limits also for MSSM 1D limits with profiling - could someone check if SM is - hopefully - untouched (rene)

--- scripts/lxb-injected.py
+++ scripts/lxb-injected.py
@@ -29,6 +29,10 @@ parser.add_option("--debug", dest="debug", default=False, action="store_true",
                   help="If run in debug mode the script will not submit to the farm, but stop after the creation of the corresponding scripts. [Default: False]")
 parser.add_option("--expected", dest="expected", default=False, action="store_true",
                   help="Save the results as the expected rootfiles. [Default: False]")
+parser.add_option("--MSSM", dest="MSSM", type="string", default="0",
+                  help="Is this MSSM? 0=no, 1=yes. [Default: \"0\"]")
+parser.add_option("--model", dest="model", type="string", default="",
+                  help="Using a physical model, i.e. 'bbH' or 'ggH' for MSSM. [Default:\"\" ]")
 ## check number of arguments; in case print usage
 (options, args) = parser.parse_args()
 
@@ -60,6 +64,9 @@ print "with options {OPTS}"
 print "for directory {PATH}/{DIR}"
 print "for random seed {RND}"
 print "for masses {MASSES}"
+print "for MSSM? 1=yes, 0=no: {MSSM}"
+print "with Model? --model {MODEL}"
+
 os.system("pwd")
 
 masses = "{MASSES}".strip().rstrip().split()
@@ -70,7 +77,7 @@ for m in masses :
     os.system("cp {PWD}/{PATH}/{DIR}/%s/{{vhtt,htt}}_* {TMPDIR}/{USER}/{DIR}_{JOBID}/%s/"%(m,m))
 os.system("cp -r {PWD}/{PATH}/{DIR}/common {TMPDIR}/{USER}/{DIR}_{JOBID}/")
 for m in masses :
-    if m :
+    if m and 0=={MSSM}:
         if {INJECTEDMH}:
             os.system("python {CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/scripts/blindData.py --seed {RND} --injected-mass %s --inject-signal --X-allow-no-signal {TMPDIR}/{USER}/{DIR}_{JOBID}/%s"%(m,m))
         else:
@@ -84,6 +91,11 @@ for m in masses :
                 os.system("cp -v {TMPDIR}/{USER}/{DIR}_{JOBID}/%s/mlfit_result.root {PWD}/{PATH}/{DIR}/%s/higgsCombineMLFIT.Exp.mH%s.root" % (m, m, m))
             else:
                 os.system("cp -v {TMPDIR}/{USER}/{DIR}_{JOBID}/%s/mlfit_result.root {PWD}/{PATH}/{DIR}/%s/higgsCombineMLFIT.mH%s-{OUTPUTLABEL}.root" % (m, m, m))
+    else :
+        os.system("python {CMSSW_BASE}/src/HiggsAnalysis/HiggsToTauTau/scripts/blindData.py --seed {RND} --extra-templates='ggH_SM125,qqH_SM125,VH_SM125' --X-allow-no-signal {TMPDIR}/{USER}/{DIR}_{JOBID}/%s"%m)
+        print "limit.py {MODEL} {METHOD} {OPTS} {TMPDIR}/{USER}/{DIR}_{JOBID}/%s" % m
+        os.system("limit.py {MODEL} {METHOD} {OPTS} {TMPDIR}/{USER}/{DIR}_{JOBID}/%s" % m)
+        os.system("cp -v {TMPDIR}/{USER}/{DIR}_{JOBID}/%s/higgsCombine{EXTENSION}.mH%s.root {PWD}/{PATH}/{DIR}/%s/higgsCombine{EXTENSION}.mH%s-{OUTPUTLABEL}.root" % (m, m, m, m))
 os.system("rm -r {TMPDIR}/{USER}/{DIR}_{JOBID}")
 '''
 
@@ -170,7 +182,9 @@ with open(submit_name, 'w') as submit_script:
                     RND = rnd if not options.expected else '-1',
                     TMPDIR=tmpdir,
                     EXPECTED='True' if options.expected else 'False',
-                    INJECTEDMH='True' if options.injected_mH else 'False'
+                    INJECTEDMH='True' if options.injected_mH else 'False',
+                    MSSM=options.MSSM,
+                    MODEL=options.model
                     ))
             with open(script_file_name.replace('.py', '.sh'), 'w') as sh_file:
                 if options.lxq :
