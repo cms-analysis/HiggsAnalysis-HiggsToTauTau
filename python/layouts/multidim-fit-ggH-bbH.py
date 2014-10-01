434a4a031141a5bb4a6f1f9804b38092bf1bef67
Felix <felix.frensch2@kit.edu>
2014-10-01 14:10:12 +0200

    adding labelling for projections

--- python/layouts/multidim-fit-ggH-bbH.py
+++ python/layouts/multidim-fit-ggH-bbH.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 
 layout = cms.PSet(
     ## dataset
+    #dataset = cms.string("#scale[1.5]{CMS}   Projection       #phi#rightarrow#tau#tau                 300 fb^{-1} (14 TeV)"),    
     dataset = cms.string("#scale[1.5]{CMS}   #phi#rightarrow#tau#tau                                          19.7 fb^{-1} (8 TeV)"),
     #dataset = cms.string("#scale[1.5]{CMS} (unpublished)   #phi#rightarrow#tau#tau                 19.7 fb^{-1} (8 TeV)"),
     ## x-axis title
@@ -70,7 +71,7 @@ layout = cms.PSet(
     ## is this mssm?
     mssm = cms.bool(True),
     ## make a temperature plot?
-    temp = cms.bool(False),
+    temp = cms.bool(True),
     ## draw the SM expectation
     drawSM = cms.bool(False),
     ## apply smoothing before plotting?
