18a0f16992473c8afd1b3d6fa6d37a10c780a799
Felix <felix.frensch2@kit.edu>
2014-10-06 12:43:16 +0200

    14TeV dummy added for projection

--- python/summer13_analyses_cfg.py
+++ python/summer13_analyses_cfg.py
@@ -2,40 +2,47 @@ def htt_ee(TYPE='KIT') :
     dict = {}
     dict['7TeV'] = 'DESY-KIT'
     dict['8TeV'] = 'DESY-KIT'
+    dict['14TeV'] = 'DESY-KIT'
     return dict
 
 def htt_mm(TYPE='KIT') :
     dict = {}
     dict['7TeV'] = 'DESY-KIT'
     dict['8TeV'] = 'DESY-KIT'
+    dict['14TeV'] = 'DESY-KIT'
     return dict
 
 def htt_em(TYPE='MIT') :
     dict = {}
     dict['7TeV'] = TYPE
     dict['8TeV'] = TYPE
+    dict['14TeV'] = TYPE
     return dict    
 
 def htt_et(TYPE='Imperial') :
     dict = {}
     dict['7TeV'] = TYPE
     dict['8TeV'] = TYPE
+    dict['14TeV'] = TYPE
     return dict
 
 def htt_mt(TYPE='Imperial') :
     dict = {}
     dict['7TeV'] = TYPE
     dict['8TeV'] = TYPE
+    dict['14TeV'] = TYPE
     return dict
 
 def htt_tt(TYPE='MIT') :
     dict = {}
     dict['7TeV'] = 'None'
     dict['8TeV'] = TYPE
+    dict['14TeV'] = TYPE
     return dict
 
 def vhtt(TYPE='VHTT') :
     dict = {}
     dict['7TeV'] = 'VHTT'
     dict['8TeV'] = 'VHTT'
+    dict['14TeV'] = 'VHTT'
     return dict
