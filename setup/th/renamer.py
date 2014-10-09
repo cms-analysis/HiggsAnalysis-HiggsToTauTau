583f27a40d3a0c5c4e4b44ed673dfe9431b12481
Riccardo <riccardo.manzoni@cern.ch>
2014-10-09 21:56:02 +0200

    renamer.py now translates Jan's rot files into the limit-compliant input root file, comprising both mmt and emt.

--- setup/th/renamer.py
+++ setup/th/renamer.py
@@ -1,49 +1,47 @@
 import ROOT
 
-file_in = ROOT.TFile.Open('Fisher.root','read')
-file_in.cd()
+def fill_root_file(file_in_name, category, file_out_name, option = 'recreate') :
 
-names = {
-'FisherWZ'		:  'WZ',
-'FisherZZ'		:  'ZZ',
-'Fishertt1l'		:  'tt1l',
-'Fishertt2l'		:  'tt2l',
-'FisherttW'		:  'ttW',
-'FisherttZ'		:  'ttZ',
-'FishertH_YtMinus'	:  'tH_YtMinus125',
-'FisherttH'		:  'ttH',
-'Fisherreducible'	:  'reducible',
-'Fisherdata'		:  'data_obs',
-'FishersumMC'		:  'sumMC',
-        }
+  names = {
+    'FisherWZ'            :  'WZ'           ,
+    'FisherZZ'            :  'ZZ'           ,
+    'Fishertt1l'          :  'tt1l'         ,
+    'Fishertt2l'          :  'tt2l'         ,
+    'FisherttW'           :  'ttW'          ,
+    'FisherttZ'           :  'ttZ'          ,
+    'FishertH_YtMinus'    :  'tH_YtMinus125',
+    'FisherttH'           :  'ttH'          ,
+    'Fisherreducible'     :  'reducible'    ,
+    'Fisherdata'          :  'data_obs'     ,
+    'FishersumMC'         :  'sumMC'        ,
+#     'FishersumMC'         :  'data_obs'     , # this is just a test, as of 9/10/14 FIXME!
+          }
 
-hists = []
-
-for key in file_in.GetListOfKeys() :
-  hist = key.ReadObj()
-  if hist.GetName() in names.keys() :
-    if names[hist.GetName()] in [hh.GetName() for hh in hists] : continue
-    hist.SetName(names[hist.GetName()])
-    hist.SetMarkerStyle(9)
-    hist.SetMarkerSize(1)
-    hist.SetMarkerColor(ROOT.kBlack)
-    hist.SetLineColor(ROOT.kBlack)
-    hists.append(hist)
-
-file_out = ROOT.TFile.Open('htt_th.inputs-sm-8TeV.root','recreate')
-file_out.cd()
-file_out.mkdir('emt')
-file_out.cd('emt')
-for hist in hists :
-  hist.Write()
-
-file_out.cd()
-file_out.mkdir('mmt')
-file_out.cd('mmt')
-## dummy for the moment
-for hist in hists :
-  hist.Scale(0.00000001)
-  hist.Write()
-
-file_out.Close()
-
+  file_in = ROOT.TFile.Open(file_in_name,'read')
+  file_in.cd()
+  
+  hists = []
+  
+  for key in file_in.GetListOfKeys() :
+    hist = key.ReadObj()
+    if hist.GetName() in names.keys() :
+      if names[hist.GetName()] in [hh.GetName() for hh in hists] : continue
+      hist.SetName(names[hist.GetName()])
+      hist.SetMarkerStyle(9)
+      hist.SetMarkerSize(1)
+      hist.SetMarkerColor(ROOT.kBlack)
+      hist.SetLineColor(ROOT.kBlack)
+      hists.append(hist)
+  
+  file_out = ROOT.TFile.Open(file_out_name,option)
+  file_out.cd()
+  file_out.mkdir(category)
+  file_out.cd(category)
+  for hist in hists :
+    hist.Write()
+  file_out.cd()
+  file_out.Close()
+ 
+  
+fill_root_file('Fisher_emt.root','emt','htt_th.inputs-sm-8TeV.root','recreate')
+fill_root_file('Fisher_mmt.root','mmt','htt_th.inputs-sm-8TeV.root','update'  )
